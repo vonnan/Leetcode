5986ce86f6ad7545955d126c9810101f8fccf7d5
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-04-28 23:47:36 -0400

    Time: 32 ms (59.12%), Space: 14.2 MB (67.24%) - LeetHub

---
+++ unique-paths/unique-paths.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        def factorial(x):
+            if x ==1 or x==0:
+                return 1
+            
+            return x*factorial(x-1)
+        return factorial(m+n-2)//factorial(m-1)//factorial(n-1)
