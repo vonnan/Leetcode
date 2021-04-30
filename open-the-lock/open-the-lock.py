18fb23dfd5a40a0aeb139d5074bc3af089d757fd
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-04-30 16:54:04 -0400

    Time: 1892 ms (10.70%), Space: 15.6 MB (40.27%) - LeetHub

--- open-the-lock/open-the-lock.py
+++ open-the-lock/open-the-lock.py
@@ -1,31 +1,33 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         
-        sets = set(deadends+["0000"])
+        sets = set(deadends)
         queue = deque(["0000"])
-        if target =="0000":
-            return 0
         
-        if "0000" in deadends or target in deadends:
+        if "0000" in sets:
             return -1
         
+        sets.add("0000")
         steps = 0
         
         while queue:
             length = len(queue)
-            steps += 1
+            
             for _ in range(length):
                 combo = queue.popleft()
+                if combo == target:
+                    return steps
+                
                 for i in range(4):
                     for move in [-1, 1]:
                         new_move = (int(combo[i]) + move) % 10
                         new_combo = combo[:i] + str(new_move) +combo[i+1:]
                         if new_combo == target:
-                            return steps
+                            return steps + 1
                         if new_combo not in sets and new_combo not in queue:
                             sets.add(new_combo)
                             queue.append(new_combo)
-                            
+            steps += 1            
         return -1
                     
                         
