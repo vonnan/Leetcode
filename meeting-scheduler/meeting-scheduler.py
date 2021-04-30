469278668646dd340619a8d37ba81bab0985f3b9
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-04-29 22:24:11 -0400

    Time: 464 ms (90.94%), Space: 21.6 MB (35.68%) - LeetHub

--- meeting-scheduler/meeting-scheduler.py
+++ meeting-scheduler/meeting-scheduler.py
@@ -1,14 +1,31 @@
+from heapq import heappush
+from heapq import heappop
+
 class Solution:
     def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
-        slots1 = sorted([(s,e) for s,e in slots1 if e-s>= duration])
-        slots2 = sorted([(s,e) for s,e in slots2 if e-s>= duration])
+        heap = []
+        for s1, e1 in slots1:
+            if e1 - s1 >= duration:
+                heappush(heap, (s1, e1))
+           
+        for s2, e2 in slots2:
+            if e2- s2 >= duration:
+                heappush(heap, (s2, e2))   
+        if not heap:
+            return []
         
-        for s1,e1 in slots1:
-            for s2,e2 in slots2:
-                left = max(s1,s2)
-                right = min(e1,e2)
+        s1, e1 = heappop(heap)
+        
+        while heap:
+            while heap and heap[0][0] >= e1:
+                s1, e1 = heappop(heap)
+            if heap: 
+                s, e = heappop(heap)
+                left = max(s1, s)
+                right = min(e1, e)
                 if right - left >= duration:
-                    return [left, left + duration]
-                
+                    return [ left, left + duration]
+                s1, e1 = s, e
         return []
+            
         
