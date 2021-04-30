9b828e433134d73247541ab0757922fca71e2bdd
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-04-30 02:40:55 -0400

    Time: 48 ms (79.23%), Space: 16.1 MB (23.62%) - LeetHub

---
+++ n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        
+        res = []
+        
+        for s in root.children:
+            res += self.postorder(s)
+            
+        res.append(root.val)
+        
+        return res
