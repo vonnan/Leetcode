05359d0e80e679ba15b5488afe630b4695fc99ef
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-03 16:25:27 -0400

    Time: 100 ms (49.65%), Space: 18.2 MB (5.96%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -1,29 +1,31 @@
+from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        dic = defaultdict(list)
+        dic = defaultdict(set)
         
         for course, pre in prerequisites:
-            dic[pre].append(course)
+            dic[pre].add(course)
             
         def cycle(node, tracker, visited):
             tracker[node] = True
             visited[node] = True
+            
             for course in dic[node]:
                 if course not in visited and cycle(course, tracker, visited):
                     return True
                 elif course in tracker:
                     return True
+                
             tracker.pop(node)
+            
             return False
         
         visited = {}
         for course in range(numCourses):
             tracker = {}
+            
             if course not in visited and cycle(course, tracker, visited):
                 return False
             
         return True
-
-       
-                
-            
+        
