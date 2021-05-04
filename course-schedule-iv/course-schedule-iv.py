97a75c1f2299109bce15f8436203a386c9eae224
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-03 23:49:57 -0400

    Time: 2456 ms (23.70%), Space: 17.6 MB (86.98%) - LeetHub

--- course-schedule-iv/course-schedule-iv.py
+++ course-schedule-iv/course-schedule-iv.py
@@ -1,20 +1,18 @@
-from collections import defaultdict
-
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        matrix = [[False]*numCourses for _ in range(numCourses)]
+        dp = [[False]* numCourses for _ in range(numCourses)]
         
         for course, pre in prerequisites:
-            matrix[pre][course] = True
+            dp[pre][course] = True
             
         for k in range(numCourses):
             for i in range(numCourses):
                 for j in range(numCourses):
-                    matrix[i][j] |= matrix[i][k] and matrix[k][j]
+                    dp[i][j] |= dp[i][k] and dp[k][j]
         
         res = []
         
         for course, pre in queries:
-            res.append(matrix[pre][course])
+            res.append(dp[pre][course])
             
         return res
