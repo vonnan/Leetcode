2bf7afde65afdf2ed7b0bd2d6508ac803338c584
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-03 21:24:07 -0400

    Time: 32 ms (92.92%), Space: 14.1 MB (83.54%) - LeetHub

--- shortest-way-to-form-string/shortest-way-to-form-string.py
+++ shortest-way-to-form-string/shortest-way-to-form-string.py
@@ -2,29 +2,32 @@
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
         """
-The find() method finds the first occurrence of the specified value.
+    The find() method finds the first occurrence of the specified value.
 
-The find() method returns -1 if the value is not found.
+    The find() method returns -1 if the value is not found.
 
-The find() method is almost the same as the index() method, the only difference is that the index() method raises an exception if the value is not found. (See example below)
+    The find() method is almost the same as the index() method, the only difference         is that the index() method raises an exception if the value is not found. (See          example below)
 
-Syntax
-string.find(value, start, end)
+    Syntax
+    string.find(value, start, end)
 
         """
-        start, copy = 0, 1
+        
+        start, count = 0, 0
         
         for char in target:
             start = source.find(char, start)
             
-            if start == -1:
+            if start ==-1:
                 start = source.find(char)
-                copy += 1
+                count += 1
                 
-                if start == -1:
+                if start ==-1:
                     return -1
                 
             start += 1
             
-        return copy
+        return count + 1
+            
+
             
