e8fa5393a20f5d41774a7891148e73798c24211e
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-05 01:09:11 -0400

    Time: 1108 ms (28.00%), Space: 33.5 MB (18.00%) - LeetHub

--- jump-game-vi/jump-game-vi.py
+++ jump-game-vi/jump-game-vi.py
@@ -5,7 +5,8 @@ class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
         
         heap =[(0, -k)]
-        
+        # state of heap: first: so far the maximum sum plus the current value; 
+        #second item is the position
         for i, num in enumerate(nums):
             while heap and i - heap[0][1] > k:
                 heappop(heap)
