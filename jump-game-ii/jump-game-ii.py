574be1ecdcc082c63b7dc0256f4f064043d73174
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-05 04:06:16 -0400

    Time: 32 ms (68.42%), Space: 14.1 MB (93.12%) - LeetHub

--- jump-game-ii/jump-game-ii.py
+++ jump-game-ii/jump-game-ii.py
@@ -3,13 +3,14 @@ class Solution:
         n = len(nums)-1
         count = 0
         
-        while n >0:
+        while n > 0:
+            
             for last in range(n):
-                if last + nums[last] >= n:
+                if nums[last] + last >= n:
                     break
+                    
             count += 1
             n = last
-            
+           
         return count 
-            
         
