a353cecef5195097eba45d85d35e6408719ed6cd
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-05 16:29:53 -0400

    Time: 300 ms (56.56%), Space: 20.7 MB (20.03%) - LeetHub

---
+++ odd-even-jump/odd-even-jump.py
@@ -0,0 +1,35 @@
+
+class Solution:
+    def oddEvenJumps(self, arr: List[int]) -> int:
+        stack, n = [], len(arr)
+        next_high, next_low = [0]*n, [0]*n
+        
+        lst_high = sorted([(a,i) for i,a in enumerate(arr)])
+        lst_low = sorted([(-a,i) for i, a in enumerate(arr)])
+        
+        for a, i in lst_high:
+            while stack and stack[-1] < i:
+                next_high[stack.pop()] = i
+            stack.append(i)
+        
+        stack = []    
+        for a, i in lst_low:
+            while stack and stack[-1] < i:
+                next_low[stack.pop()] = i
+            stack.append(i)
+        
+        high, low = [0]*n, [0]*n
+        high[n-1] , low[n-1] = 1, 1
+        for i in range(n-2, -1, -1):
+            high[i] = low[next_high[i]]
+            low[i] = high[next_low[i]]
+            
+        return sum(high)
+            
+        
+            
+                
+        
+            
+            
+        
