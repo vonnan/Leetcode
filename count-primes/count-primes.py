d6ffa594a1e4ec1baf009f030b154fea6c70b5da
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-10 15:12:17 -0400

    Time: 820 ms (57.59%), Space: 103.6 MB (15.58%) - LeetHub

--- count-primes/count-primes.py
+++ count-primes/count-primes.py
@@ -1,21 +1,19 @@
 class Solution:
-    def countPrimes(self, n):
-       
-        if n<2:
+    def countPrimes(self, n: int) -> int:
+        prime = [True] * n
+        if n <=2:
             return 0
-        primes = [True]*n
-        primes[0] = primes[1] = False
+        prime[0] = prime[1] = False
+        if n <= 4:
+            return sum(prime)
         i = 2
-        while i < int(n**0.5)+1:
-            if primes:
-                primes[i*i:n:i]= [False]*((n-1)//i -i+1)
-            i+=1
+        while i <= int(n**0.5):
+            if prime:
+                prime[i*i: n: i] = [False]*((n-1)//i - i + 1)
+            i+= 1
         
-        return sum(primes)
-     
-                        
-                    
-                    
+        return sum(prime)
             
-                   
+                
+        
         
