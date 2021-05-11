a2184647e4fa90a997ca05da7a18752fba3465fc
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-11 15:27:52 -0400

    Time: 144 ms (40.79%), Space: 21 MB (12.46%) - LeetHub

--- h-index-ii/h-index-ii.py
+++ h-index-ii/h-index-ii.py
@@ -1,9 +1,11 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        
         n = len(citations)
+        
+        if citations[-1] <1:
+            return 0
         
         for i, cite in enumerate(citations):
             if cite >= n-i:
                 return n -i
-        return 0
+        
