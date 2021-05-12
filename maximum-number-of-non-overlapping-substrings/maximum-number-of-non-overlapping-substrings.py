714c6825702eaf86019e0494abdf6867d935cbc7
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-12 16:20:39 -0400

    Time: 504 ms (68.49%), Space: 15.9 MB (40.41%) - LeetHub

---
+++ maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxNumOfSubstrings(self, s: str) -> List[str]:
+        
+        char_set = set(s)
+         
+        dic = {c: (s.rindex(c), s.index(c)) for c in char_set}
+        
+        for char in char_set:
+            right, left = dic[char]
+            right_, left_ = -1, -1
+            while not (right_ == right and left_ == left):
+                right_, left_ = right, left
+                left = min(dic[c][1] for c in set(s[left: right + 1]))
+                right = max(dic[c][0] for c in set(s[left: right + 1]))
+            dic[char] = (right, left)
+        
+        curr = 0
+        res = []
+        
+        for r, l in sorted(dic.values()):
+            if l >=curr:
+                res.append(s[l:r+1])
+            curr = r
+         
+        return res
+                
+                
+            
+                
+            
+            
+            
+        
+            
+            
