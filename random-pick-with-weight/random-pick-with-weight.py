5c265c9a63f655cc1d63710165861a86173c2755
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-13 02:51:27 -0400

    Time: 220 ms (85.88%), Space: 18.7 MB (71.32%) - LeetHub

---
+++ random-pick-with-weight/random-pick-with-weight.py
@@ -0,0 +1,18 @@
+from bisect import bisect_left
+from random import randint
+class Solution:
+
+    def __init__(self, w: List[int]):
+        for i in range(1, len(w)):
+            w[i] += w[i-1]
+        self.w = w
+
+    def pickIndex(self) -> int:
+        r = randint(1, self.w[-1])
+        return bisect_left(self.w, r)
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
