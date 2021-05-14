15ae3b263b6b7f5dc13e48439d3549b05a3a26d3
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-14 16:31:44 -0400

    Time: 536 ms (87.71%), Space: 15.6 MB (57.81%) - LeetHub

--- open-the-lock/open-the-lock.py
+++ open-the-lock/open-the-lock.py
@@ -1,34 +1,28 @@
+from collections import deque
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        
-        sets = set(deadends)
-        queue = deque(["0000"])
-        
-        if "0000" in sets:
+        no_go_sets = set(deadends)
+        if "0000" in no_go_sets:
             return -1
+        if target == "0000":
+            return 0
         
-        sets.add("0000")
-        steps = 0
-        
+        step = 0
+        queue = deque(["0000"])
         while queue:
-            length = len(queue)
-            
-            for _ in range(length):
+            n = len(queue)
+            for _ in range(n):
                 combo = queue.popleft()
                 if combo == target:
-                    return steps
-                
-                for i in range(4):
-                    for move in [-1, 1]:
-                        new_move = (int(combo[i]) + move) % 10
-                        new_combo = combo[:i] + str(new_move) +combo[i+1:]
+                    return step + 1
+                for move in [-1,1]:
+                    for i in range(4):
+                        new = str((int(combo[i]) + move)%10)
+                        new_combo = combo[:i] + new + combo[i+1:]
                         if new_combo == target:
-                            return steps + 1
-                        if new_combo not in sets:
-                            sets.add(new_combo)
+                            return step + 1
+                        if new_combo not in no_go_sets:
+                            no_go_sets.add(new_combo)
                             queue.append(new_combo)
-            steps += 1            
+            step += 1
         return -1
-                    
-                        
-            
