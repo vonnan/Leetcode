36e29a119ff66424b27654790fcba20efdb9b8e7
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-18 12:00:13 -0400

    Time: 104 ms (33.69%), Space: 18.2 MB (5.07%) - LeetHub

--- course-schedule-ii/course-schedule-ii.py
+++ course-schedule-ii/course-schedule-ii.py
@@ -1,21 +1,16 @@
-from collections import defaultdict
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        
         dic = defaultdict(set)
-        
         for course, pre in prerequisites:
             dic[pre].add(course)
-            
-        tracker = set()
-        visited = set()
-        stack = []
+        
+        visited, tracker, stack = set(), set(), []
         
         self.cycle = False
         
-        def dfs(node, tracker, visited, stack):
-            tracker.add(node)
+        def dfs(node, visited, tracker, stack):
             visited.add(node)
+            tracker.add(node)
             
             for course in dic[node]:
                 if course in tracker:
@@ -23,16 +18,19 @@ class Solution:
                     break
                     
                 if course not in visited:
-                    dfs(course, tracker, visited, stack)
-                    
+                    dfs(course, visited, tracker, stack)
+                
             tracker.remove(node)
             stack.append(node)
             
         for course in range(numCourses):
             if course not in visited:
-                dfs(course, tracker, visited, stack)
+                dfs(course, visited, tracker, stack)
                 if self.cycle:
                     return []
-                
+        
         return stack[::-1]
+                
+                
                     
+        
