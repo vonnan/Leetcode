dea3099c4925d890dcd7ce726cee0bab411c4a3c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-20 13:50:59 -0400

    Time: 196 ms (45.71%), Space: 14.7 MB (93.25%) - LeetHub

--- minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
+++ minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,17 +1,16 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        res, right = 0, 0
-        # res: tot change of brackets so far; right: right brackets that needs to be changed
+        res, right_goal = 0, 0
         for c in s:
-            if c=="(":
-                right += 2
-                res += right %2
-                right -= right%2
-            if c==")":
-                right -= 1
-                if right <0:
+            if c == "(":
+                right_goal += 2
+                res += right_goal%2
+                right_goal -= right_goal%2
+            
+            if c == ")":
+                right_goal -= 1
+                if right_goal < 0:
                     res += 1
-                    right += 2
-                
-        return res + right
-                
+                    right_goal += 2
+        
+        return res + right_goal
