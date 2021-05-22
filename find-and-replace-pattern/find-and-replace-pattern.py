b2b93b29e36bf062972cdf2b0d8c7ec6d32157ba
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-22 02:02:14 -0400

    Time: 36 ms (35.97%), Space: 14.2 MB (58.99%) - LeetHub

--- find-and-replace-pattern/find-and-replace-pattern.py
+++ find-and-replace-pattern/find-and-replace-pattern.py
@@ -1,27 +1,28 @@
-from collections import Counter
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
         n = len(pattern)
+        
         res = []
         
         for word in words:
             if len(word) != n:
                 continue
+            dic_wp, dic_pw = {}, {}
             
-            dic_wp = {}
-            dic_pw = {}
-            flag = True
+            flag = True    
             for i in range(n):
-                if word[i] not in dic_wp and pattern[i] not in dic_pw:
-                    dic_wp[word[i]] = pattern[i]
-                    dic_pw[pattern[i]] = word[i]
-                elif (word[i] in dic_wp and dic_wp[word[i]] != pattern[i] ) or (pattern[i] in dic_pw and dic_pw[pattern[i]] != word[i]):
+                w, p = word[i], pattern[i]
+                
+                if (w not in dic_wp) and (p not in dic_pw):
+                    dic_wp[w] = p
+                    dic_pw[p] = w
+                    
+                elif (w in dic_wp and dic_wp[w] != p) or (p in dic_pw and dic_pw[p] != w):
                     flag = False
-                    continue
-            if flag:
+                    break
+                
+            if flag:    
                 res.append(word)
-        
-        return res       
-            
-            
+                
+        return res
         
