70bee9a29062180da3f35370ff601bc7384f609d
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-22 02:19:15 -0400

    Time: 1044 ms (68.60%), Space: 14.9 MB (75.73%) - LeetHub

--- max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.py
+++ max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.py
@@ -1,31 +1,33 @@
-from bisect import bisect_left
 from bisect import insort
+from bisect import bisect_left
+
 class Solution:
     def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
-        
         row, col = len(matrix), len(matrix[0])
+        
+        # update matrix with the presum per row
         
         for r in range(row):
             for c in range(1, col):
                 matrix[r][c] += matrix[r][c-1]
                 
         res = -inf
-        print(matrix)
+                
         for c1 in range(col):
             for c2 in range(c1, col):
-                arr = [matrix[r][c2] - (matrix[r][c1-1] if c1>0 else 0) for r in range(row)]
+                arr = [matrix[r][c2] - (matrix[r][c1-1] if c1 > 0 else 0) for r in range(row)]
+                
                 curr = 0
-                presum =[-inf,inf]
+                presum = [-inf, inf]
+                
+                
                 for a in arr:
                     insort(presum, curr)
                     curr += a
-                    idx = bisect_left(presum, curr- k)
+                    idx = bisect_left(presum, curr - k)
                     res = max(res, curr - presum[idx])
-
-        return res 
-
-     
-        
-        
+                
+        return res
                 
                 
+                    
