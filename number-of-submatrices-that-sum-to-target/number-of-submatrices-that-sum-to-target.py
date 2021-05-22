189f82dea8867f0d0e9e24ac14ee044d26f1a671
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-22 02:29:29 -0400

    Time: 884 ms (72.51%), Space: 15.2 MB (66.96%) - LeetHub

--- number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.py
+++ number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.py
@@ -1,27 +1,24 @@
-from collections import defaultdict
 class Solution:
     def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
         row, col = len(matrix), len(matrix[0])
         
         for r in range(row):
-            for c in range(1,col):
+            for c in range(1, col):
                 matrix[r][c] += matrix[r][c-1]
-        
+                
         res = 0
         
         for c1 in range(col):
             for c2 in range(c1, col):
-                presum = defaultdict(int)
-                presum[0] = 1
+                arr = [matrix[r][c2] - (matrix[r][c1 - 1] if c1 >0 else 0) for r in range(row)]
+                
                 curr = 0
-                for r in range(row):
-                    if c1==0:
-                        curr += matrix[r][c2]
-                    else:
-                        curr += matrix[r][c2] - matrix[r][c1-1]
-                    res += presum[curr - target]
+                presum = defaultdict(int)
+                for a in arr:
                     presum[curr] += 1
+                    curr += a
+                    if curr- target in presum:
+                        res += presum[curr - target]
+                        
         return res
                     
-                    
-        
