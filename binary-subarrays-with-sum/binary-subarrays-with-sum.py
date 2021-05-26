3cf8325cd8c8ad1c83c3cf1575d7407abdb0b513
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-25 23:27:25 -0400

    Time: 252 ms (86.46%), Space: 18.3 MB (14.75%) - LeetHub

---
+++ binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -0,0 +1,15 @@
+from itertools import accumulate
+from collections import Counter
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        presum = list(accumulate([0] + nums))
+        counter = Counter(presum)
+        
+        res = 0
+        if goal ==0:
+            return sum(val*(val-1)//2 for _, val in counter.items())
+        for key in sorted(counter.keys()):
+            if key - goal in counter:
+                res += counter[key] * counter[key - goal]
+                
+        return res
