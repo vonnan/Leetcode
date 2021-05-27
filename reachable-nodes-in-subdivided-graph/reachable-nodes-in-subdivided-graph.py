930707009fc9fde8909cb20b07abc37821496381
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-27 00:51:04 -0400

    Time: 444 ms (100.00%), Space: 20.8 MB (77.92%) - LeetHub

---
+++ reachable-nodes-in-subdivided-graph/reachable-nodes-in-subdivided-graph.py
@@ -0,0 +1,42 @@
+from heapq import heappush
+from heapq import heappop
+
+class Solution:
+    def reachableNodes(self, edges: List[List[int]], maxMoves: int, n: int) -> int:
+        graph = defaultdict(dict)
+        
+        for u,v,num in edges:
+            graph[u][v] = num
+            graph[v][u] = num
+            
+        heap = [(0, 0)]
+        steps = 0
+        visited = set()
+        
+        while heap:
+            cost, u = heappop(heap)
+            if u in visited:
+                continue
+            
+            visited.add(u)    
+            steps += 1
+                
+            for v, w in graph[u].items():
+                
+                steps += min(maxMoves - cost, w)
+                    
+                if v not in visited:    
+                    if w + cost  >= maxMoves:
+                        graph[v][u] -= maxMoves - cost
+                    
+                    else:
+                        heappush(heap, (w + cost +1, v))
+                        del graph[v][u]
+            
+        return steps
+                
+                
+                
+                    
+                
+                    
