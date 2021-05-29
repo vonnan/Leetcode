7d150c7debdfea71933c37de57fdf7b8c4ebee58
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-05-29 00:52:46 -0400

    Time: 120 ms (90.40%), Space: 17.4 MB (50.56%) - LeetHub

--- word-ladder/word-ladder.py
+++ word-ladder/word-ladder.py
@@ -1,34 +1,30 @@
-from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
+        dic = defaultdict(list)
+        n = len(beginWord)
         
-        dic, n = defaultdict(list), len(beginWord)
         for word in wordList:
             for i in range(n):
-                dic[word[:i]+"*"+word[i+1:]].append(word)
-        
+                dic[word[:i] + "*" + word[i+1:]].append(word)
+                
         queue = deque([beginWord])
-        visited = set()
+        seen = set()
         step = 1
         
         while queue:
             m = len(queue)
             for _ in range(m):
                 q = queue.popleft()
-                if q == endWord:
-                    return step + 1
                 for i in range(n):
                     mask = q[:i] + "*" + q[i+1:]
                     if mask in dic:
                         for word in dic[mask]:
                             if word == endWord:
                                 return step + 1
-                            if word not in visited:
-                                visited.add(word)
+                            if word not in seen:
+                                seen.add(word)
                                 queue.append(word)
                         del dic[mask]
             step += 1
+        
         return 0
-                
