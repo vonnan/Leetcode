194abc14b3030186e6528b5f2f5584b3c96a54a9
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-03 21:48:50 -0400

    Time: 224 ms (62.39%), Space: 16.6 MB (77.53%) - LeetHub

--- design-browser-history/design-browser-history.py
+++ design-browser-history/design-browser-history.py
@@ -1,24 +1,26 @@
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.browser = deque([])
-        self.browser.appendleft(homepage)
+        self.browser = deque([homepage])
         self.cursor = 0
 
     def visit(self, url: str) -> None:
         for _ in range(self.cursor):
             if self.browser:
                 self.browser.popleft()
+            else:
+                break
         self.browser.appendleft(url)
         self.cursor = 0
         
 
     def back(self, steps: int) -> str:
-        self.cursor = min(self.cursor + steps, len(self.browser)-1)
+        self.cursor = min(self.cursor + steps, len(self.browser) - 1)
         return self.browser[self.cursor]
+        
 
     def forward(self, steps: int) -> str:
-        self.cursor = max(0, self.cursor - steps)
+        self.cursor = max(self.cursor - steps, 0)
         return self.browser[self.cursor]
 
 
