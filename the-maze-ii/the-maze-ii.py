4cf91e81f94ff11658eba48eab806d6e19465f68
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-09 03:17:37 -0400

    Time: 448 ms (20.82%), Space: 15.1 MB (34.98%) - LeetHub

--- the-maze-ii/the-maze-ii.py
+++ the-maze-ii/the-maze-ii.py
@@ -1,34 +1,33 @@
-from collections import defaultdict
-from heapq import heappush
-from heapq import heappop
 class Solution:
     def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
-        m, n = len(maze), len(maze[0])
-        path = [(0,1), (0,-1), (1,0), (-1,0)]
+        row, col = len(maze), len(maze[0])
+        path = [(0,1), (0, -1), (1, 0), (-1,0)]
         heap = [(0, start[0], start[1])]
-        dist = {}
+        visited = set()
         
         while heap:
-            w, x,y = heappop(heap)
-            if (x,y) in dist:
+            cost, x,y = heappop(heap)
+            if (x,y) in visited:
                 continue
-            dist[(x,y)] = w
+            visited.add((x,y))
             if destination ==[x,y]:
-                return w
+                return cost
+            
             for dx, dy in path:
                 nx, ny = x, y
                 nw = 0
-                while 0<= nx < m and 0 <= ny < n and maze[nx][ny] == 0:
+                while 0 <= nx < row and 0 <= ny < col and maze[nx][ny] == 0:
                     nx += dx
                     ny += dy
                     nw += 1
                 nx -= dx
                 ny -= dy
                 nw -= 1
-                if (nx, ny) in dist:
-                    continue
-                heappush(heap, (w + nw, nx, ny))
                 
+                if (nx, ny) not in visited:
+                    heappush(heap, (cost + nw, nx, ny))
+                    
         return -1
-                    
-                    
+                
+            
+            
