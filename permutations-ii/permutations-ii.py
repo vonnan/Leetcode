e7d72569a6e7529c51ae1b9845de7cd8cfd414a0
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-17 19:21:02 -0400

    Time: 328 ms (20.67%), Space: 14.9 MB (8.01%) - LeetHub

---
+++ permutations-ii/permutations-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        path = [0 for _ in range(n)]
+        status = [False for _ in range(n+1)]
+        res = set()
+        
+        def dfs(u):
+            if u == n:
+                res.add(tuple(path[:]))
+                return 
+    
+            for j in range(n):
+                if not status[j]:
+                    status[j] = True
+                    path[u] = nums[j]
+                    dfs(u+1)
+                    status[j] = False
+        
+        dfs(0)
+        
+        return res
