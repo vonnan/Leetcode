b080590e299294ebfa5e9869ed81838156154ee9
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-20 14:33:15 -0400

    Time: 580 ms (49.39%), Space: 15.6 MB (18.79%) - LeetHub

--- island-perimeter/island-perimeter.py
+++ island-perimeter/island-perimeter.py
@@ -1,12 +1,22 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         row, col = len(grid), len(grid[0])
-        land = set([(r,c) for r in range(row) for c in range(col) if grid[r][c] == 1])
-        sets = land.copy()
+        islands = set()
+        path = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j]== 1:
+                    islands.add((i,j))
+        
         res = 0
-        while land:
-            r,c = land.pop()
-            res += 4 - sum((r+dx, c + dy) in sets for dx,dy in ((-1,0), (1,0),(0,1),(0,-1)))
-            
+        for x in range(row):
+            for y in range(col):
+                if (x,y) in islands:
+                    res += 4
+                    for dx, dy in path:
+                        nx, ny = x + dx, y + dy
+                        if (nx, ny) in islands:
+                            res -= 1
+        
         return res
-            
+                    
