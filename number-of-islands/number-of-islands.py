a5e9c248c1a9dc853cc4724dbb3c3637571b9811
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-20 14:58:57 -0400

    Time: 128 ms (93.03%), Space: 19.6 MB (8.57%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,21 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m, n = len(grid), len(grid[0])
-        land =set([(r,c) for r in range(m) for c in range(n) if grid[r][c] == "1"])
+        row, col = len(grid), len(grid[0])
+        land = set([(i,j) for i in range(row) for j in range(col) if grid[i][j] == "1"])
         res = 0
+        path = [(0,1), (1,0), (-1,0), (0, -1)]
         
         while land:
             res += 1
             queue = deque([land.pop()])
             while queue:
-                r, c = queue.popleft()
-                for dx, dy in ((1,0), (-1,0), (0,1), (0,-1)):
-                    nr, nc = r + dx, c + dy
+                r,c = queue.popleft()
+                for dr, dc in path:
+                    nr, nc = r + dr, c +dc
                     if (nr, nc) in land:
-                        land.remove((nr, nc))
                         queue.append((nr, nc))
+                        land.remove((nr, nc))
         return res
-        
-        
-        
-        
+                        
