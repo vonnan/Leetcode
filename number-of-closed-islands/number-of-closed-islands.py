a2c3904d3d8b3cbb59f8cc033b058ea83d8736c8
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-20 19:52:01 -0400

    Time: 140 ms (56.50%), Space: 15.6 MB (9.24%) - LeetHub

--- number-of-closed-islands/number-of-closed-islands.py
+++ number-of-closed-islands/number-of-closed-islands.py
@@ -1,33 +1,28 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        row, col, res = len(grid), len(grid[0]), 0
-        
-        if not grid or not grid[0]:
-            return 0
+        row, col = len(grid), len(grid[0])
+        land = set([(i, j) for i in range(row) for j in range(col) if grid[i][j] ==0])
         
         def dfs(i,j):
-            if 0<=i<row and 0<=j<col and grid[i][j] ==0:
+            if 0 <= i < row and 0<= j < col and grid[i][j] ==0:
                 grid[i][j] = 1
+                dfs(i-1, j)
                 dfs(i+1, j)
-                dfs(i, j+1)
-                dfs(i-1,j)
                 dfs(i, j-1)
-                
-        
+                dfs(i, j+1)
+            
         for i in range(row):
             for j in range(col):
-                if i==0 or i == row-1 or j == 0 or j == col - 1 and grid[i][j]==0:
-                    dfs(i,j)
+                if (i == 0 or i == row-1 or j == 0 or j == col-1) and grid[i][j] ==0:
+                    dfs(i, j)
         
-       
-        
+        res = 0        
         for i in range(row):
             for j in range(col):
                 if grid[i][j] ==0:
-                    dfs(i,j)
+                    dfs(i, j)
                     res += 1
-                
         return res
-                
+            
         
         
