9e87a93779e680f1425ad732324a61e0de2b7df2
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-22 16:06:10 -0400

    Time: 56 ms (85.06%), Space: 14.9 MB (7.98%) - LeetHub

--- permutations-ii/permutations-ii.py
+++ permutations-ii/permutations-ii.py
@@ -2,17 +2,16 @@ class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         res, n = set(), len(nums)
         nums.sort()
+        
         visited = [0] * n
-        def dfs(ans):
+        def dfs(nums, ans, n):
             if len(ans) ==n:
                 res.add(tuple(ans[:]))
                 return
-            for i in range(n):
-                
-                if not visited[i]:
-                    visited[i] = 1
-                    dfs(ans+ [nums[i]] )
-                    visited[i] = 0
+            for i in range(len(nums)):
+                if i > 0 and (nums[i] == nums[i-1]):
+                    continue
+                dfs(nums[:i] + nums[i+1:], ans + [nums[i]], n)
                     
-        dfs([])
+        dfs(nums, [], n)
         return res
