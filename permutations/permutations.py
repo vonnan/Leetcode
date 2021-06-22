e0e44291f7ee96595c3563695c5a6807fa3b0d40
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-22 14:36:28 -0400

    Time: 40 ms (69.58%), Space: 14.4 MB (69.08%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,22 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        path = [0 for _ in range(n)]
-        status = [False for _ in range(n+1)]
+        status = [False] * (n+1)
+        path =[0]* n
         res = set()
         
         def dfs(u):
             if u == n:
                 res.add(tuple(path[:]))
-                return 
-    
+                return
+            
             for j in range(n):
                 if not status[j]:
                     status[j] = True
                     path[u] = nums[j]
                     dfs(u+1)
                     status[j] = False
-        
+                    
         dfs(0)
         
         return res
