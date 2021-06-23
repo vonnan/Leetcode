50da15c83dadceded42b6f7515fd9b52a2a422ce
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-23 02:43:49 -0400

    Time: 1096 ms (91.58%), Space: 33.8 MB (57.92%) - LeetHub

--- decode-xored-permutation/decode-xored-permutation.py
+++ decode-xored-permutation/decode-xored-permutation.py
@@ -1,18 +1,26 @@
 class Solution:
     def decode(self, encoded: List[int]) -> List[int]:
         n = len(encoded) + 1
-        
         ref = 0
+        
         for i in range(1, n+1):
             ref ^= i
             
+        # ref = 1 ^ 2 ^ 3 ^... ^ n
+        
         for j in range(1, n, 2):
             ref ^= encoded[j]
-        
-        res = []
-        res.append(ref)
-        
-        for e in encoded:
-            res.append(res[-1] ^ e)
             
+        res = [ref]
+        #ref: perm[0]
+        
+        for i in range(n-1):
+            res.append(res[-1]^encoded[i])
+        
         return res
+            
+            
+        
+        
+            
+            
