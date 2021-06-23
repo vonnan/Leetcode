d45b20d6e1ecde000d5f13b819089694a85a6188
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-23 00:11:18 -0400

    Time: 88 ms (65.38%), Space: 15 MB (90.38%) - LeetHub

--- find-permutation/find-permutation.py
+++ find-permutation/find-permutation.py
@@ -1,15 +1,12 @@
 class Solution:
     def findPermutation(self, s: str) -> List[int]:
         n = len(s) + 1
-        
-        #if s[i]== "I", do not change order; if s[i] == "D", decreasing order
-        
         res = []
+        
         for i, c in enumerate(s):
-            if c == "I":
-                res.extend(range(i + 1, len(res), -1))
+            if c== "I":
+                res.extend(range(i+1, len(res), -1))
+        
         res.extend(range(n, len(res), -1))
         
         return res
-       
-        
