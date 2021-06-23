3e1be5b809f1f7edf366ef1bc0fee4980fcb4462
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-23 02:14:51 -0400

    Time: 204 ms (73.58%), Space: 14.4 MB (53.63%) - LeetHub

--- count-vowels-permutation/count-vowels-permutation.py
+++ count-vowels-permutation/count-vowels-permutation.py
@@ -1,9 +1,8 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        a, e, i, o, u = 1, 1, 1, 1, 1
-        mod = 10 ** 9 + 7
-        n -= 1
+        a, o, i, e, u = 1, 1, 1, 1, 1
+        n-= 1
         while n:
-            a, e, i, o, u = e, a + i, a + e + o + u, i + u, a
             n -= 1
-        return (a + e + i + o + u) % mod
+            a, o, i, e, u = e, i + u, a + o + e + u, a + i, a
+        return (a+o+i+e+u) % (10**9+7)
