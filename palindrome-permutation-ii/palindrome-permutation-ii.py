177d0844137ce37ff0c976b5e1b8d242e3d5ae68
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-22 20:40:07 -0400

    Time: 84 ms (99.31%), Space: 23.4 MB (18.75%) - LeetHub

--- palindrome-permutation-ii/palindrome-permutation-ii.py
+++ palindrome-permutation-ii/palindrome-permutation-ii.py
@@ -3,25 +3,25 @@ from itertools import permutations
 class Solution:
     def generatePalindromes(self, s: str) -> List[str]:
         def possible(s):
-            n = len(s)
-            ct = sum(x%2 for x in Counter(s).values())
-            return ct -n%2 == 0
+            return len(s)% 2 == sum(x%2 for x in Counter(s).values())
+        
         if not possible(s):
             return []
-        counter = Counter(s)
-        c = ""
-        mid = ""
-        for key, val in counter.items():
-            if val %2:
-                mid = key
-            c+= key * (val//2)
         
+        counter = Counter(s)
+        c, mid = "", ""
         res = set()
         
+        for key, val in counter.items():
+            if val%2:
+                mid = key
+            c += key *(val//2)
+            
         for x in permutations(c):
             x = "".join(x)
-            res.add(x+mid+x[::-1])
+            res.add(x + mid + x[::-1])
             
+        
         return res
             
         
