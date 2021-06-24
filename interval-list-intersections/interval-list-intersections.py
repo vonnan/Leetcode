f053154428c81380929ed4ce3036b03d1f479e2a
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-24 01:18:30 -0400

    Time: 136 ms (98.82%), Space: 15 MB (78.06%) - LeetHub

--- interval-list-intersections/interval-list-intersections.py
+++ interval-list-intersections/interval-list-intersections.py
@@ -1,29 +1,30 @@
+from bisect import bisect
 class Solution:
-    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+    def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        res = []
         i, j = 0, 0
-        res =[]
-        n, m = len(firstList), len(secondList)
+        m, n = len(A), len(B)
         
-        while i < n and j < m:
-            l1,r1 = firstList[i]
-            l2,r2 = secondList[j]
-            left, right = max(l1, l2), min(r1, r2)
+        while i < m and j < n:
+            l1, r1 = A[i]
+            l2, r2 = B[j]
             
-            if l1> r2:
-                j += 1
-                
-            elif l2 > r1:
+            left = max(l1, l2)
+            
+            if r1 < r2:
                 i += 1
-                
+                if left <= r1:
+                    res.append((left, r1))
+                    
             else:
-                res.append((left, right))
-                if i+1 < n and r2 >= firstList[i+1][0]:
-                    i += 1
-                elif j +1 < m and r1 >= secondList[j+1][0]:
-                    j += 1
-                else:
-                    i += 1
-                    j += 1
-                
+                j += 1
+                if left <= r2:
+                    res.append((left, r2))
+                    
         return res
-    
+            
+                
+            
+                    
+        
+        
