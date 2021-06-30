ddf559657dae6fcb210a6c9b4090136b8baff007
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-06-30 16:16:22 -0400

    Time: 36 ms (48.27%), Space: 14.2 MB (73.35%) - LeetHub

---
+++ sort-colors/sort-colors.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        counter = Counter(nums)
+        nums[:] = [0]*counter[0] + [1]* counter[1] + [2]*counter[2]
+        
+        
+        
+        
+        
