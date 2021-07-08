127bb0a822c11aa032709df4eb267ccc4f052cec
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-07 21:42:16 -0400

    Time: 1472 ms (94.20%), Space: 38.5 MB (76.45%) - LeetHub

--- minimum-space-wasted-from-packaging/minimum-space-wasted-from-packaging.py
+++ minimum-space-wasted-from-packaging/minimum-space-wasted-from-packaging.py
@@ -2,18 +2,19 @@ from bisect import bisect
 class Solution:
     def minWastedSpace(self, packages: List[int], boxes: List[List[int]]) -> int:
         packages.sort()
-        tot, maxP = sum(packages), max(packages)
-        res, mod = inf, 10**9 +7
+        tot, res, mod = sum(packages), inf, 10**9 + 7
+        
         for box in boxes:
             box.sort()
-            if box[-1] < maxP:
+            if box[-1] < packages[-1]:
                 continue
-            ct, start = 0, 0
+            start, ct = 0, 0
             for b in box:
                 idx = bisect(packages, b, start)
-                ct += b*(idx - start)
+                ct += (idx - start)*b
                 start = idx
                 if ct > res:
                     break
             res = min(res, ct)
-        return (res - tot)% mod if res != inf else -1
+        return (res- tot) % mod if res != inf else -1
+        
