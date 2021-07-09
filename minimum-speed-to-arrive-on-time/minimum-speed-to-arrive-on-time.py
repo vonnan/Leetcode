8bc05b0961553c57255289e0551a023a5679f4d4
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-09 00:29:37 -0400

    Time: 3940 ms (57.52%), Space: 28.3 MB (79.29%) - LeetHub

--- minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.py
+++ minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.py
@@ -1,24 +1,25 @@
 from math import ceil
+
 class Solution:
     def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
-        n = len(dist)
-        if hour <= n-1:
+        if hour <= len(dist) - 1:
             return -1
         
-        left, right = 1, dist[-1]* 100
+        def check(m):
+            res = 0
+            for d in dist[:-1]:
+                res += ceil(d/m)
+            res += dist[-1]/m
+            print(mid, res)
+            return res <= hour
         
+        left, right = 1, 10**7
         while left < right:
             mid = (left + right)//2
-            res = 0
-            for i, d in enumerate(dist):
-                if i != n-1:
-                    res += ceil(d/mid)
-                else:
-                    res += d/mid
-                if res > hour:
-                    left = mid + 1
-                    break
-            if res <= hour:
+            if check(mid):
                 right = mid
-        
-        return left
+            else:
+                left = mid + 1
+                
+        return right
+            
