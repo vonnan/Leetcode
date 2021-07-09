0ebc7cec567462bcd20263c72081d1f8946cf141
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-09 19:09:04 -0400

    Time: 36 ms (79.22%), Space: 14.4 MB (17.69%) - LeetHub

--- special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,14 +1,12 @@
-from bisect import bisect_left
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        nums.sort()
-        if nums[0] >= n:
-            return n
-
-        for candidate in range(1, n+1):
-            if nums[n-1-candidate] < candidate <= nums[n-candidate]:
-                return candidate
-            
+        left, right = 0, 1000
+        while left < right:
+            mid = (left + right)//2
+            if sum(num >= mid for num in nums) > mid:
+                left = mid + 1
+            else:
+                right = mid
+        if sum(num >= left for num in nums) == left:
+            return left
         return -1
-        
