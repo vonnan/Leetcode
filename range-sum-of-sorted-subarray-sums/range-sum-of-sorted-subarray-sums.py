353651f6d54ef2f60fa5cd96bf3edb80152aad6d
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-09 17:33:05 -0400

    Time: 352 ms (76.71%), Space: 41.7 MB (6.83%) - LeetHub

---
+++ range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        res, mod = [], 10 ** 9 + 7
+        
+        for i in range(n):
+            tot = nums[i]
+            res.append(tot)
+            
+            for j in range(i + 1, n):
+                tot += nums[j]
+                res.append(tot)
+        
+        return sum(sorted(res)[left-1: right])%mod
