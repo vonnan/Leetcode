dece1cde9c2725a08cbec2c44b8b7431000aa8a0
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-09 20:05:32 -0400

    Time: 32 ms (69.14%), Space: 14.2 MB (67.49%) - LeetHub

--- ugly-number-iii/ugly-number-iii.py
+++ ugly-number-iii/ugly-number-iii.py
@@ -1,26 +1,23 @@
 from math import gcd
 class Solution:
     def nthUglyNumber(self, n: int, a: int, b: int, c: int) -> int:
-        def lcm(a,b):
-            return a*b//gcd(a,b)
+        def lcm(a, b):
+            return a * b//gcd(a, b)
         
-        def count_ugly(m, a,b,c,ab, bc, ca,abc):
+        def ct_ugly(m, a,b,c):
             ct = m//a + m//b + m//c
+            ab, bc, ca = lcm(a, b), lcm(b, c), lcm(c, a)
+            abc = lcm(ab, c)
             ct -= m//ab + m//bc + m//ca
-            ct += m//abc
+            ct += m // abc
             return ct
         
-        ab, bc, ca = lcm(a, b), lcm(b, c), lcm(c, a)
-        abc = lcm(ab, c)
-        lo, hi = 1, 2* 10**9
-        while lo < hi:
-            mid = (lo + hi)//2
-            if count_ugly(mid, a, b, c, ab, bc, ca, abc) < n:
-                lo = mid + 1
+        left, right = 1, 2* 10** 9
+        while left < right:
+            mid = (left + right)//2
+            if ct_ugly(mid, a,b,c) >= n:
+                right = mid
             else:
-                hi = mid
-        return lo
-        
-        
+                left = mid + 1
+        return left
             
-        
