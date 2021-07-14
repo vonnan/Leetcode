cb8f4478252b7d0cef1d9f9c1d77b484d900c04f
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-14 03:33:05 -0400

    Time: 332 ms (38.16%), Space: 15.9 MB (69.30%) - LeetHub

---
+++ sliding-window-median/sliding-window-median.py
@@ -0,0 +1,22 @@
+from bisect import insort
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        lst = sorted(nums[:k])
+        
+        res = []
+        m = k//2
+        print(lst, m)
+        def median(lst):
+            if k % 2:
+                res.append(lst[m])
+            else:
+                res.append((lst[m] + lst[m-1])/2)
+        
+        median(lst)
+        for i in range(1, len(nums) - k +1):
+            lst.remove(nums[i - 1])
+            insort(lst, nums[i + k - 1])
+            median(lst)
+        
+        return res
+        
