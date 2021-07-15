018ff75d747ec2f60812ea99355ea16e8967472e
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-15 02:26:12 -0400

    Time: 2076 ms (23.41%), Space: 67.4 MB (27.53%) - LeetHub

--- k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,14 +1,15 @@
+from itertools import combinations
 from heapq import heappush
 from heapq import heappop
-from itertools import combinations
+
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        heap =[]
-        for a,b in combinations(arr,2):
-            heappush(heap, (a/b, a,b))
+        heap = []
+        for x,y in combinations(arr,2):
+            heappush(heap, (x/y,x,y))
+        for _ in range(k-1):
+            heappop(heap)
+        _, x, y = heappop(heap)
+        return [x,y]
         
-        for _ in range(k):
-            ans = heappop(heap)
-        
-        return [ans[1], ans[2]]
-        
+            
