f610075068e50000b04a85a87196d0db760bf7de
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-15 22:07:29 -0400

    Time: 992 ms (74.38%), Space: 19.9 MB (26.25%) - LeetHub

--- maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
+++ maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -1,25 +1,25 @@
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
         row, col = len(mat), len(mat[0])
-        dp = [[0]* (col + 1) for _ in range(row + 1)]
+        presum = [[0] * (col +1) for _ in range(row + 1)]
         
         for r in range(row):
             for c in range(col):
-                dp[r+1][c+1] = mat[r][c] + dp[r+1][c] + dp[r][c + 1] - dp[r][c]
-        
-        def isvalid(size):
-            for r in range(row - size + 1):
-                for c in range(col - size + 1):
-                    if dp[r][c] - dp[r+size][c] - dp[r][c + size] + dp[r + size][c + size] <= threshold:
+                presum[r+1][c+1] = mat[r][c] + presum[r+1][c] + presum[r][c+1] - presum[r][c]
+                
+        def isValid(size):
+            for r in range(row +1 -size):
+                for c in range(col + 1 - size):
+                    if presum[r+size][c+size] + presum[r][c] - presum[r+size][c] - presum[r][c+size] <= threshold:
                         return True
             return False
         
         lo, hi = 0, min(row, col)
         while lo < hi:
             mid = (lo + hi + 1)//2
-            if isvalid(mid):
+            if isValid(mid):
                 lo = mid
             else:
                 hi = mid -1
-        return hi
-            
+        return lo
+        
