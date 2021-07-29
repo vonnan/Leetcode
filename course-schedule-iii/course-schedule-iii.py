f540cf395d92205f5a095ad33b765748deec5aa3
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-29 11:52:03 -0400

    Time: 684 ms (84.25%), Space: 19.4 MB (38.99%) - LeetHub

--- course-schedule-iii/course-schedule-iii.py
+++ course-schedule-iii/course-schedule-iii.py
@@ -1,16 +1,17 @@
 from heapq import heappush
 from heapq import heappop
+
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
         courses.sort(key = lambda x: x[1])
         heap = []
         max_time = 0
-        
-        for duration, end in courses:
-            heappush(heap, -duration)
-            max_time += duration
+    
+            
+        for t, end in courses:
+            heappush(heap, -t)
+            max_time += t
             if max_time > end:
                 max_time += heappop(heap)
-                
         return len(heap)
-             
+            
