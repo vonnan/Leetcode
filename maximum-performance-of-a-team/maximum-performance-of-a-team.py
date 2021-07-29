063336cda1a95d62ed3050636275ae8e81d68387
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-29 13:29:18 -0400

    Time: 380 ms (87.18%), Space: 29.9 MB (79.38%) - LeetHub

--- maximum-performance-of-a-team/maximum-performance-of-a-team.py
+++ maximum-performance-of-a-team/maximum-performance-of-a-team.py
@@ -4,12 +4,12 @@ from heapq import heappop
 class Solution:
     def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
         heap = []
-        speed_sum, res = 0, 0
-        for e,s in sorted(zip(efficiency, speed), reverse = 1):
-            heappush(heap, s)
-            speed_sum += s
-            if len(heap) >k:
-                speed_sum -= heappop(heap)
-            res = max(res, e*speed_sum)
-        return res % (10**9 + 7)
-            
+        res, speed_sofar, mod = 0, 0, 10**9 + 7
+        
+        for e,v in sorted(zip(efficiency, speed), reverse = 1):
+            heappush(heap, v)
+            speed_sofar += v
+            if len(heap) > k:
+                speed_sofar -= heappop(heap)
+            res = max(res, e* speed_sofar)
+        return res % mod
