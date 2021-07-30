785f40a76f907cb56fef6b2f9389d3fe661587f0
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-30 19:11:14 -0400

    Time: 68 ms (95.94%), Space: 14.2 MB (56.50%) - LeetHub

---
+++ split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxUniqueSplit(self, s: str) -> int:
+        seen = set()
+        
+        def dfs(s, seen):
+            if not s:
+                return 0
+            
+            res = 0
+            
+            for i in range(1, len(s) +1):
+                candidate = s[:i]
+                if candidate not in seen and len(s) -i >= res:
+                    seen.add(candidate)
+                    res = max(res, 1 + dfs(s[i:], seen))
+                    seen.remove(candidate)
+            return res
+        
+        return dfs(s, seen)
