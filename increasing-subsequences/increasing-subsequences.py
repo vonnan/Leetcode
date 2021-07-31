8b84029a34fbf19d757fabd75acc3439a06d846f
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-07-31 14:08:15 -0400

    Time: 236 ms (50.62%), Space: 22.8 MB (33.47%) - LeetHub

--- increasing-subsequences/increasing-subsequences.py
+++ increasing-subsequences/increasing-subsequences.py
@@ -1,13 +1,13 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        def dfs(nums, pre):
+        def dfs(nums, prev):
             if nums:
                 for i in range(len(nums)):
-                    if not pre or nums[i] >= pre[-1]:
-                        if len(pre):
-                            result.add(tuple(pre + [nums[i]]))
-                        dfs(nums[i+1:], pre + [nums[i]])
-        
-        result = set()
+                    if not prev or prev[-1] <= nums[i]:
+                        if len(prev):
+                            res.add(tuple(prev + [nums[i]]))
+                        dfs(nums[i+1:], prev + [nums[i]])
+        res = set()
         dfs(nums, [])
-        return result
+        return res
+        
