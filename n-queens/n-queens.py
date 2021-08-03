e6ad020031054382fc21813c5b0b84df219895b1
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-03 00:48:09 -0400

    Time: 56 ms (79.01%), Space: 14.5 MB (87.15%) - LeetHub

--- n-queens/n-queens.py
+++ n-queens/n-queens.py
@@ -1,27 +1,24 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def backtrack(i):
-            if i == n:
-                res.append(list(board))
-            for j in range(n):
-                if j not in cols and j- i not in diag and j + i not in off_diag:
-                    cols.add(j)
-                    diag.add(j-i)
-                    off_diag.add(j+i)
-                    board.append("."* j + "Q" + "."* (n-1-j))
-                    backtrack(i + 1)
-                    board.pop()
-                    off_diag.remove(j+i)
-                    diag.remove(j-i)
-                    cols.remove(j)
-                    
-        res, board = [], []
-        
+        res, board =[], []
         cols, diag, off_diag = set(), set(), set()
-        
-        backtrack(0)
-        
+        def dfs(r):
+            if r == n:
+                res.append(list(board))
+            for c in range(n):
+                if c not in cols and r - c not in diag and r+c not in off_diag:
+                    cols.add(c)
+                    diag.add(r-c)
+                    off_diag.add(r+c)
+                    board.append("."*c + "Q" + "."*(n-1-c))
+                    dfs(r + 1)
+                    board.pop()
+                    off_diag.remove(r + c)
+                    diag.remove(r - c)
+                    cols.remove(c)
+        dfs(0)
         return res
-    
                     
+                    
+            
             
