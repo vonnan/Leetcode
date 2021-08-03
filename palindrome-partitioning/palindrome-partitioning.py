fe3fda7dd7a5229c6acb86bc9beddf7d7a3169ba
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-03 17:34:23 -0400

    Time: 648 ms (87.30%), Space: 30.5 MB (34.89%) - LeetHub

--- palindrome-partitioning/palindrome-partitioning.py
+++ palindrome-partitioning/palindrome-partitioning.py
@@ -1,22 +1,22 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        def isPalindrom(s):
-            return s == s[::-1]
+        def isPalindrom(x):
+            return x == x[::-1]
         
         res = []
         
-        def dfs(sub, path):
-            if not sub:
+        def dfs(x, path):
+            if not x:
                 if path:
                     res.append(list(path))
                 return
             
-            for i in range(1, len(sub) +1):
-                prefix = sub[:i]
+            for i in range(1, len(x) + 1):
+                prefix = x[:i]
                 if isPalindrom(prefix):
                     path.append(prefix)
-                    dfs(sub[i:], path)
+                    dfs(x[i:], path)
                     path.pop()
+        
         dfs(s, [])
         return res
-                
