06c805587f94eb852d04a170195dbbd34405c8fb
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-04 18:11:59 -0400

    Time: 336 ms (85.15%), Space: 14.3 MB (79.41%) - LeetHub

---
+++ android-unlock-patterns/android-unlock-patterns.py
@@ -0,0 +1,36 @@
+
+class Solution:
+    def numberOfPatterns(self, m: int, n: int) -> int:
+        invalid = {}
+        for i in range(1, 9):
+            for j in range( i +1, 10):
+                if (i+j + 1) % 2:
+                    invalid[(i, j)] = (i + j)//2
+                    invalid[(j, i)] = (i + j)//2
+        
+        lst = ((1,5), (2, 4), (2, 6), (3, 5), (5, 7), (5, 9), (4,8), (6, 8))
+        for x, y in lst:
+            del invalid[(x,y)]
+            del invalid[(y, x)]
+        print(sorted(invalid))
+        
+        def dfs(seen, curr, remain):
+            if remain == 0:
+                return 1
+            res = 0
+            seen.add(curr)
+            for i in range(1, 10):
+                if i not in seen and ((curr, i) not in invalid or invalid[(curr, i)] in seen):
+                    res += dfs(seen, i, remain -1)
+            seen.discard(curr)
+            return res
+        seen, ans = set(), 0
+        for i in range(m, n + 1):
+            ans += dfs(seen, 1, i -1)*4
+            ans += dfs(seen, 2, i-1) *4 
+            ans += dfs(seen, 5, i-1) 
+        return ans
+        
+            
+        
+        
