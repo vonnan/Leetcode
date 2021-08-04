79f43b933ad02745fafbe8c6e866af070fffb841
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-04 14:11:35 -0400

    Time: 156 ms (37.48%), Space: 15.5 MB (39.39%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,9 +1,7 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         row, col = len(grid), len(grid[0])
-
-        path = [(0,1), (1,0), (-1,0), (0, -1)]
-        res = 0
+        path = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         
         def dfs(r,c):
             if 0 <= r < row and 0 <= c < col and grid[r][c] == "1":
@@ -14,5 +12,6 @@ class Solution:
                 return 1
             return 0
         
-        return sum(dfs(r,c) for r in range(row) for c in range(col))
-                
+        return sum(dfs(r, c) for r in range(row) for c in range(col))
+            
+            
