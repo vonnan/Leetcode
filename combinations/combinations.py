f54a28236bc3993bb1db900cce5e5229b918a8e9
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-06 18:23:32 -0400

    Time: 652 ms (12.75%), Space: 16 MB (13.48%) - LeetHub

--- combinations/combinations.py
+++ combinations/combinations.py
@@ -1,6 +1,19 @@
-from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        lst = [i for i in range(1, n+1)]
-        return list(combinations(lst, k))
+        res = set([])
         
+        nums = [i for i in range(1, n+1)]
+        
+        def dfs(nums, path):
+            if len(path) > k:
+                return
+            
+            if len(path) == k:
+                res.add(tuple(path))
+                
+            for i in range(len(nums)):
+                dfs(nums[i+1:], path + [nums[i]])
+                
+        dfs(nums, [])
+        
+        return res
