2dac2370d06e2e0aef65d0cf1e3191656c816553
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-06 17:22:27 -0400

    Time: 28 ms (99.78%), Space: 14.1 MB (80.09%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -1,30 +1,33 @@
-from bisect import bisect
-
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        
        
-        n = len(nums)
         
-        if n ==1:
-            return nums
+        i = j = len(nums) - 1
         
-        for i in range(n-1,0,-1):
-            if nums[i-1] >= nums[i]:
-                continue
-            elif i==len(nums)-1:
-                nums[i], nums[i-1] = nums[i-1], nums[i]
-                return nums
-            else:
-                idx = bisect(nums[i:][::-1], nums[i-1])+1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        
+        if i == 0:
+            nums.reverse()
+            return 
+           
+        else:
+            while nums[j] <= nums[i - 1]:
+                j -= 1
                 
-                nums[i-1], nums[-idx] = nums[-idx], nums[i-1]
-               
-                nums[i:] = [n for n in reversed(nums[i:])]
+            nums[i-1], nums[j] = nums[j], nums[i -1]
+            
+            nums[i:] = nums[i:][::-1]
                 
-                return nums
-        nums.reverse()
-        return nums
-      
+        
+            
+            
+        
+                
+          
+            
+        
