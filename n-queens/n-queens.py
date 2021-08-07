053bc73f76e17d1b1caa87602cec4fa596c50b07
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-06 21:16:54 -0400

    Time: 44 ms (98.93%), Space: 14.4 MB (98.03%) - LeetHub

--- n-queens/n-queens.py
+++ n-queens/n-queens.py
@@ -1,24 +1,26 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        res, board =[], []
-        cols, diag, off_diag = set(), set(), set()
+        res, board = [], []
+        
+        col, diag, off_diag = set(), set(), set()
+        
         def dfs(r):
             if r == n:
                 res.append(list(board))
+                
             for c in range(n):
-                if c not in cols and r - c not in diag and r+c not in off_diag:
-                    cols.add(c)
-                    diag.add(r-c)
+                if c not in col and r+ c not in off_diag and r-c not in diag:
+                    col.add(c)
                     off_diag.add(r+c)
+                    diag.add(r -c)
                     board.append("."*c + "Q" + "."*(n-1-c))
                     dfs(r + 1)
                     board.pop()
-                    off_diag.remove(r + c)
+                    col.remove(c)
+                    off_diag.remove(r+c)
                     diag.remove(r - c)
-                    cols.remove(c)
+         
         dfs(0)
+        
         return res
                     
-                    
-            
-            
