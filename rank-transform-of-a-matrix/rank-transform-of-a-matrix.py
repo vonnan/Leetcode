44a3fff4134b1602ac2f1481464c9b75a65c860c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-09 01:11:48 -0400

    Time: 1940 ms (79.31%), Space: 55.4 MB (48.28%) - LeetHub

---
+++ rank-transform-of-a-matrix/rank-transform-of-a-matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def matrixRankTransform(self, A: List[List[int]]) -> List[List[int]]:
+           
+        n, m = len(A), len(A[0])
+        rank = [0] * (m + n)
+        d = collections.defaultdict(list)
+        for i in range(n):
+            for j in range(m):
+                d[A[i][j]].append([i, j])
+
+        def find(i):
+            if p[i] != i:
+                p[i] = find(p[i])
+            return p[i]
+
+        for a in sorted(d):
+            p = list(range(m + n))
+            rank2 = rank[:]
+            for i, j in d[a]:
+                i, j = find(i), find(j + n)
+                p[i] = j
+                rank2[j] = max(rank2[i], rank2[j])
+            for i, j in d[a]:
+                rank[i] = rank[j + n] = A[i][j] = rank2[find(i)] + 1
+        return A
