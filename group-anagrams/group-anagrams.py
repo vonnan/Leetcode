ad15fa60fa4e1e72a649c975b76fbdc056c73e0a
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-12 14:25:29 -0400

    Time: 149 ms (12.75%), Space: 19.4 MB (19.08%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,14 +1,21 @@
+from collections import Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = defaultdict(list)
-        for s in strs:
-            x = "".join(sorted(s)) 
-            if x in dic:
-                dic[x].append(s)
-            else:
-                dic[x] = [s]
-        return [ val for val in dic.values()]
-
-                
+        def converter(s):
+            res = [0]*26
+            for x in s:
+                idx = ord(x) - 97
+                res[idx] +=1
+            return tuple(res)
         
+        res = []
+        dic = {}
+        for s in strs:
+            t = converter(s)
+            if t not in dic:
+                res.append([s])
+                dic[t] = len(res)-1
+            else:
+                res[dic[t]].append(s)
+        return res
         
