c1792ab9b29420dde7d7a3cc77a9d7e41f3f24ea
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-13 01:29:19 -0400

    Time: 3745 ms (22.47%), Space: 538.8 MB (20.22%) - LeetHub

--- maximize-palindrome-length-from-subsequences/maximize-palindrome-length-from-subsequences.py
+++ maximize-palindrome-length-from-subsequences/maximize-palindrome-length-from-subsequences.py
@@ -1,18 +1,18 @@
 class Solution:
     def longestPalindrome(self, word1: str, word2: str) -> int:
         word = word1 + word2
-        n1, n = len(word1), len(word)
+        n1 = len(word1)
         
         @cache
         def dp(i,j):
             if i >= j:
-                return i == j
+                return int(i == j)
             if word[i] == word[j]:
                 return dp(i+1, j-1) + 2
             else:
                 return max(dp(i, j-1), dp(i + 1, j))
         ans = 0   
-        for x in ascii_lowercase:
+        for x in set(word):
             idx = word1.find(x)
             jdx = word2.rfind(x)
             if idx != -1 and jdx != -1:
