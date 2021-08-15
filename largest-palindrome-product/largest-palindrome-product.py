70945c010c0b94d5e02642280ade288984f912af
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-15 03:43:36 -0400

    Time: 508 ms (58.82%), Space: 14.1 MB (82.35%) - LeetHub

--- largest-palindrome-product/largest-palindrome-product.py
+++ largest-palindrome-product/largest-palindrome-product.py
@@ -1,36 +1,17 @@
 class Solution:
-    def largestPalindrome(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        
-        if n==1:
+    def largestPalindrome(self, n: int) -> int:
+        if n == 1:
             return 9
-        high=10**n
-        low=10**(n-1)
-        top = ((high-1)//11)*11
         
-        for i in range(high-1,low-1,-1):
-            w=int(str(i)+str(i)[::-1])
-            for j in range(top,i+1, -11):
-                if w%j==0:
-                    z=w//j
-                    if z<high and z>=low:
-                        return w%1337
-        """            
-        if n == 1: return 9
-        for z in range(2, 2 * (9 * 10**n) - 1):
-            left = 10**n - z
-            right = int(str(left)[::-1])
-            root_1, root_2 = 0, 0
-						
-            if z**2 - 4*right < 0:
-                continue
-            else:
-                root_1 = 1/2 * (z + (z**2-4*right)**0.5)
-                root_2 = 1/2 * (z - (z**2-4*right)**0.5)
-                if root_1.is_integer() or root_2.is_integer():
-                    print(root_1, root_2,10**n*left+right, z)
-                    return (10**n*left+right)%1337
-             """        
+        hi, lo = 10**n -1, 10**(n-1)
+        top = (hi // 11) * 11
+        
+        for left in range(hi, lo-1, -1):
+            res = int(str(left) + str(left)[::-1])
+            
+            for d in range(top, left -1, -11):
+                if res % d == 0:
+                    q = res // d
+                    
+                    if lo <= q <= hi:
+                        return res % 1337
