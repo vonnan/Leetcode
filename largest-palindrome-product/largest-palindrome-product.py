b30f6eb7a0420288d6b4ccca9656191860074f2d
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-16 21:31:39 -0400

    Time: 524 ms (58.65%), Space: 14.2 MB (82.69%) - LeetHub

--- largest-palindrome-product/largest-palindrome-product.py
+++ largest-palindrome-product/largest-palindrome-product.py
@@ -1,17 +1,18 @@
 class Solution:
     def largestPalindrome(self, n: int) -> int:
-        if n == 1:
+        if n ==1:
             return 9
         
-        hi, lo = 10**n -1, 10**(n-1)
-        top = (hi // 11) * 11
+        hi, lo = 10**n -1 , 10**(n-1)
+        top = hi//11*11
         
         for left in range(hi, lo-1, -1):
             res = int(str(left) + str(left)[::-1])
             
             for d in range(top, left -1, -11):
-                if res % d == 0:
-                    q = res // d
-                    
+                if res % d== 0:
+                    q = res//d
                     if lo <= q <= hi:
                         return res % 1337
+        
+            
