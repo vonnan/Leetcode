72c4f713f84601ff28ba556433e257ed8a53ff42
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-24 02:17:01 -0400

    Time: 32 ms (45.96%), Space: 14.2 MB (48.20%) - LeetHub

--- sum-of-two-integers/sum-of-two-integers.py
+++ sum-of-two-integers/sum-of-two-integers.py
@@ -1,8 +1,8 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         mask = 0xFFFFFFFF
-        if b == 0:
+        if b==0:
             return a
-        if b & mask == 0:
+        if b & mask ==0:
             return a & mask
-        return self.getSum(a ^ b, (a & b)<< 1)
+        return self.getSum(a ^ b, (a & b) << 1)
