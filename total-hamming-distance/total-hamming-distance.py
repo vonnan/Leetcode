7d5bc489e5c2e0f2c142cc651dc4f5b89fd69386
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-25 13:22:02 -0400

    Time: 460 ms (79.80%), Space: 16 MB (9.98%) - LeetHub

--- total-hamming-distance/total-hamming-distance.py
+++ total-hamming-distance/total-hamming-distance.py
@@ -1,15 +1,16 @@
 class Solution:
     def totalHammingDistance(self, nums: List[int]) -> int:
-        nums = [bin(num)[2:].zfill(32) for num in nums]
-        
+        nums =[bin(num)[2:].zfill(32) for num in nums]
         res = 0
         for i in range(32):
-            bits =[0, 0]
+            zero, one = 0, 0
             for num in nums:
-                
-                bits[int(num[i])] += 1
-            res += bits[0] * bits[1]
+                if num[i] == "0":
+                    zero += 1
+                else:
+                    one += 1
+            res += one * zero
         return res
             
-            
-        
+                    
+                    
