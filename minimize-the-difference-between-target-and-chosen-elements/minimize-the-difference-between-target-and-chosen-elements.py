e774d8a00c72f3e7616405990affa126bda88278
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-26 16:52:28 -0400

    Time: 4801 ms (64.91%), Space: 14.5 MB (88.13%) - LeetHub

--- minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.py
+++ minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.py
@@ -1,5 +1,8 @@
+from heapq import heappush
+
 class Solution:
     def minimizeTheDifference(self, mat: List[List[int]], target: int) -> int:
+        """
         bits = 1
         
         for row in mat:
@@ -11,3 +14,21 @@ class Solution:
         for x in range(5000):
             if bits >> (target + x) & 1 or x < target and bits >> (target - x) & 1:
                 return x
+                
+        """
+        
+        sets ={0}
+        for row in mat:
+            temp, heap = set(), []
+            for num in row:
+                for prev in sorted(sets):
+                    if num + prev >= target:
+                        heappush(heap, num + prev)
+                        break
+                    else:
+                        temp.add(num + prev)
+            if heap:
+                temp.add(heap[0])
+            sets = temp
+        
+        return min(abs(x - target) for x in sets)
