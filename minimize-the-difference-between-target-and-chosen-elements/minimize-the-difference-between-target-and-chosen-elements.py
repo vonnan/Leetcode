92aa1e7b5f64074554efa6f8d797a202bbe07876
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-26 16:29:50 -0400

    Time: 267 ms (96.16%), Space: 14.6 MB (88.13%) - LeetHub

--- minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.py
+++ minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.py
@@ -1,13 +1,13 @@
 class Solution:
     def minimizeTheDifference(self, mat: List[List[int]], target: int) -> int:
         bits = 1
+        
         for row in mat:
             temp = 0
-            for x in set(row):
-                temp |= bits << x
+            for num in row:
+                temp |= bits << num
             bits = temp
             
         for x in range(5000):
-            if bits >> target + x & 1 or x < target and bits >> target -x & 1:
+            if bits >> (target + x) & 1 or x < target and bits >> (target - x) & 1:
                 return x
-            
