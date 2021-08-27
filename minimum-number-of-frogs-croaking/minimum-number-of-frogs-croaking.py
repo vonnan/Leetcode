a12d0b52f2c235c551515b5076e108ecb9c0bd07
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-27 10:39:42 -0400

    Time: 361 ms (16.30%), Space: 15 MB (35.52%) - LeetHub

--- minimum-number-of-frogs-croaking/minimum-number-of-frogs-croaking.py
+++ minimum-number-of-frogs-croaking/minimum-number-of-frogs-croaking.py
@@ -1,22 +1,19 @@
 class Solution:
     def minNumberOfFrogs(self, A: str) -> int:
         counter = Counter(A)
-        if len(A) % 5 or sum(x not in "croak" for x in counter) > 0 or len([y for y in counter]) != 5 or len(set(y for y in counter.values())) != 1:
+        if len(A) % 5 or len(counter) != 5 or sum(s not in "croak" for s in counter) or len(set(counter.values())) != 1:
             return -1
-        res = 0
-        dp =[0]*5
         
-        for i, s in enumerate(A):
-            
+        dp = [0] * 5
+        res = 0
+        for s in A:
             idx = "croak".index(s)
             dp[idx] += 1
+            for prev in range(idx):
+                if dp[prev] < dp[idx]:
+                    return -1
             if s == "c":
                 res = max(res, dp[0] - dp[-1])
-            
-            for j in range(idx):
-                if dp[idx] > dp[j]:
-                    return -1
         return res
-            
-        
-        
+                                                                                            
+                                                                                            
