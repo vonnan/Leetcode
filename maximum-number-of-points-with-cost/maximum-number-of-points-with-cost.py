b342636aa1cd841e66bb9ea432537f9ea1cd8c68
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-27 11:24:43 -0400

    Time: 2116 ms (89.93%), Space: 48.4 MB (81.72%) - LeetHub

--- maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
+++ maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
@@ -1,11 +1,14 @@
 class Solution:
     def maxPoints(self, A: List[List[int]]) -> int:
-        rows, cols = len(A), len(A[0])
-        for r in range(rows - 1):
-            for c in range(cols-2, -1, -1):
-                A[r][c] = max(A[r][c], A[r][c+1] -1)
-            for c in range(cols):
-                A[r][c] = max(A[r][c], A[r][c-1] -1 if c else 0)
-                A[r+1][c] += A[r][c]
+        row, col = len(A), len(A[0])
+        
+        for r in range(row -1):
+            for c in range(col-2, -1, -1):
+                A[r][c] = max(A[r][c], A[r][c+1] - 1)
                 
+            for c in range(col):
+                A[r][c] = max(A[r][c], A[r][c-1] -1 if c else 0)
+                
+                A[r + 1][c] += A[r][c]
+        
         return max(A[-1])
