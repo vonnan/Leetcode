3663b4bc3c7fca5e918ce81c6fb98925a206d2e7
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-29 00:15:44 -0400

    Time: 75 ms (45.85%), Space: 16.3 MB (5.02%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,7 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = defaultdict(list)
         for i, num in enumerate(nums):
-            if target- num in nums[i+1:]:
-                return [i, nums.index(target-num, i+1)]
+            dic[num].append(i)
+       
+        if target %2 ==0 and (target//2 in dic) and (len(dic[target//2]) ==2):
+            return dic[target//2]
+        else:
+            for num in dic:
+                if target - num in dic and num * 2 != target:
+                    return [dic[num][0], dic[target - num][0]]
             
-        
