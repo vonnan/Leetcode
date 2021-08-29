630165902b96746ef32f5b58d6d83c6fec9e415c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-29 02:15:26 -0400

    Time: 40 ms (37.35%), Space: 14.7 MB (32.53%) - LeetHub

--- merge-sorted-array/merge-sorted-array.py
+++ merge-sorted-array/merge-sorted-array.py
@@ -3,18 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        lst = []
-        i, j = 0, 0
-        while i < m and j < n:
-            if nums1[i] <= nums2[j]:
-                lst.append(nums1[i])
-                i += 1
-            else:
-                lst.append(nums2[j])
-                j += 1
-                
-        lst += nums1[i:m] + nums2[j:n]
-        
-        nums1[:] = lst
-                
-        
+        if m ==0:
+            nums1[:] = nums2
+        if nums2:
+            i = m + n - 1
+            j = m-1
+            print(i, j)
+            while i >=0 and j >=0 and nums2:
+                print(i, j, nums1[j], nums2[-1])
+                if nums1[j] <= nums2[-1]:
+                    nums1[i] = nums2.pop()
+                else:
+                    nums1[i], nums1[j] = nums1[j], nums1[i]
+                    if j > 0:
+                        j -= 1
+                    elif j ==0:
+                        nums1[j] = nums2.pop()
+                i -= 1
