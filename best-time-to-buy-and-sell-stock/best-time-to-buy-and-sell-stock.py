64e8a4f5dec6132d83e90bc678e158a5eda40e3d
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-08-29 16:40:27 -0400

    Time: 1676 ms (5.00%), Space: 25.2 MB (52.49%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,9 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        lst =sorted([(price, i) for i, price in enumerate(prices)])
-        res = 0
-        print(lst)
-        for i, price in enumerate(prices):
-            if price >= lst[-1][0]:
-                continue
-            while lst[-1][1] < i:
-                lst.pop()
-            
-            res = max(res, lst[-1][0] - price)
-        return res
+    def maxProfit(self, prices):
+        profit = 0
+        if len(prices)> 0:
+            buy = prices[0]
+            for p in prices:
+                buy = min(buy, p)
+                profit = max(profit, p - buy)
+        return profit
