9368b62ae06d8b04f7ac6c0f7614fa249f415298
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-02 18:04:52 -0400

    Time: 307 ms (12.62%), Space: 17.1 MB (56.77%) - LeetHub

--- number-of-distinct-islands/number-of-distinct-islands.py
+++ number-of-distinct-islands/number-of-distinct-islands.py
@@ -3,26 +3,24 @@ class Solution:
         row, col = len(grid), len(grid[0])
         
         land = set()
-        global step
-        step = ""
+        self.step = ""
         
         def dfs(r,c, direction):
-            global step
             if 0 <= r < row and 0 <= c <col and grid[r][c]==1:
                 grid[r][c] = 0
-                step += direction
+                self.step += direction
                 dfs(r-1, c, "U")
                 dfs(r+1, c, "D")
                 dfs(r, c-1, "L")
                 dfs(r, c+1, "R")
-                step += "B"
+                self.step += "B"
         
         for r in range(row):
             for c in range(col):
                 if grid[r][c] ==1:
                     dfs(r,c,"O")
-                    land.add(step)
-                    step = ""
-        print(land)
+                    land.add(self.step)
+                    self.step = ""
+        
         return len(land)
                 
