c37901bd2bb316329b83bb10a2698f8e096b42d5
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-03 22:06:25 -0400

    Time: 630 ms (70.96%), Space: 27.9 MB (81.03%) - LeetHub

--- maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.py
+++ maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.py
@@ -1,31 +1,25 @@
 class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
-        lst = []
-        
-        for num in nums:
-            if num < 0:
-                lst.append("n")
-            elif num >0:
-                lst.append("p")
+        lst = [[]]
+        for i, num in enumerate(nums):
+            if num >0:
+                lst[-1].append(1)
+            elif num <0:
+                lst[-1].append(-1)
+            elif lst[-1] !=[]:
+                lst.append([])
+        res = 0
+        print(lst)
+        for num in lst:
+            if not num:
+                continue
+            n = len(num)
+            if num.count(-1) % 2 == 0:
+                res = max(res, n)
             else:
-                lst.append("0")
-        s = "".join(lst)
-                
-        def maxSub(c):
-            ct_n = c.count("n")
-            if ct_n %2 == 0:
-                return len(c)
-            else:
-                res = len(c) - 1 - min(c.index("n"), c[::-1].index("n"))
-                return res
-            
-        if "0" not in s:
-            return maxSub(s)
-        else:
-            ls = s.split("0")
-            res = 0
-            for c in ls:
-                res = max(res, maxSub(c))
+                res = max(res, n - 1 - min(num.index(-1), num[::-1].index(-1)))
                 
         return res
+            
+                
             
