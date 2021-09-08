54732f7d19a0e57a034b95d5dcbc2228ec9a7dc3
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-07 20:56:14 -0400

    Time: 28 ms (80.71%), Space: 14.2 MB (72.92%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -2,5 +2,5 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         dic = {1: 1, 2: 2}
         for i in range(3, n+1):
-            dic[i] = dic[i - 1] + dic[i - 2]
+            dic[i] = dic[i-1] + dic[i-2]
         return dic[n]
