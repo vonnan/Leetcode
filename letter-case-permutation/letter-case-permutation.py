884a9e80e2a06be23dae1acac1651a6bf0aca849
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-07 21:00:59 -0400

    Time: 52 ms (88.21%), Space: 14.7 MB (90.40%) - LeetHub

--- letter-case-permutation/letter-case-permutation.py
+++ letter-case-permutation/letter-case-permutation.py
@@ -3,13 +3,13 @@ class Solution:
         res = [""]
         
         for ch in s:
-            if not ch.isalpha():
+            if ch.isdigit():
                 for i in range(len(res)):
                     res[i] += ch
-                    
             else:
                 for i in range(len(res)):
                     temp = res[i]
                     res[i] += ch.lower()
                     res.append(temp + ch.upper())
         return res
+                    
