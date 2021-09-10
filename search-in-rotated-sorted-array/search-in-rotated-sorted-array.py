df09b2841449475a590554a456977efa29e8b6a2
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-10 14:41:35 -0400

    Time: 44 ms (49.14%), Space: 14.8 MB (22.90%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,31 +1,30 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        if nums[0] == target:
-            return 0
-        if nums[-1] ==target:
+        left, right = 0, len(nums)-1
+        if nums[left] == target:
+            return left
+        if nums[right] == target:
             return right
         
         while left < right:
             mid = (left + right)//2
             if nums[mid] == target:
                 return mid
-            if target > nums[right]:
-                if nums[mid] > nums[right]:
-                    if nums[mid] > target:
-                        right = mid
-                    else:
-                        left = mid + 1
-                else:
+            
+            if nums[mid] > nums[left]:
+                if nums[left] <= target <=nums[mid]:
                     right = mid
+                else:
+                    left = mid + 1
+                    
             else:
-                if nums[mid] > nums[right]:
+                if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
-                    if target > nums[mid]:
-                        left = mid + 1
-                    else:
-                        right = mid
-        return left if nums[left] == target else -1
-                    
-                
+                    right = mid
+            
+        if nums[left] == target:
+            return left
+        else:
+            return -1
+        
