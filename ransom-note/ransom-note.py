8a0ed345b2c83ba64ca055e4d366a0ee12ac26f6
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-15 17:17:25 -0400

    Time: 44 ms (87.87%), Space: 14.3 MB (63.73%) - LeetHub

--- ransom-note/ransom-note.py
+++ ransom-note/ransom-note.py
@@ -1,7 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        counter_r, counter_m = Counter(ransomNote), Counter(magazine)
-        if counter_r & counter_m == counter_r:
-            return True
-        else:
-            return False
+        counter = Counter(magazine)
+        for c in ransomNote:
+            if c not in counter:
+                return False
+            else:
+                counter[c] -= 1
+                if counter[c] == 0:
+                    del counter[c]
+        return True
