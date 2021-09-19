7cef58c3448dd89db96af4904331dc5026020969
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-19 12:48:07 -0400

    Time: 140 ms (66.10%), Space: 15.5 MB (92.83%) - LeetHub

--- surrounded-regions/surrounded-regions.py
+++ surrounded-regions/surrounded-regions.py
@@ -1,34 +1,30 @@
 class Solution:
-    def solve(self, board: List[List[str]]) -> None:
+    def solve(self, A: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        row, col = len(board), len(board[0])
-        queue =deque([(r, c) for r in range(row) for c in range(col) if (r== 0 or r == row -1 or c == 0 or c == col -1 ) and board[r][c] == "O"])
-        path = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        visited = set(queue)
+        row, col = len(A), len(A[0])
+        q = deque([(r,c) for r in range(row) for c in range(col) if (r == 0 or r== row-1 or c == 0 or c == col - 1) and A[r][c] == "O"])
+        path = [(1,0), (-1, 0), (0, 1), (0, -1)]
         
-        while queue:
-            m = len(queue)
+        while q:
+            m = len(q)
             for _ in range(m):
-                r,c = queue.popleft()
+                r,c = q.popleft()
+                if A[r][c] == "O":
+                    A[r][c] = "N"
                 for dr, dc in path:
                     nr, nc = r + dr, c + dc
-                    if (nr, nc ) not in visited:
-                        if 0 <= nr <row and 0 <= nc < col and board[nr][nc] == "O":
-                            visited.add((nr, nc))
-                            queue.append((nr, nc))
+                    if 0 <= nr < row and 0 <= nc < col and A[nr][nc] == "O":
+                        A[nr][nc] = "N"
+                        q.append((nr, nc))
         
-        for r,c in visited:
-            board[r][c] = "N"
-         
         for r in range(row):
             for c in range(col):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                elif board[r][c] == "N":
-                    board[r][c] = "O"
+                if A[r][c] == "O":
+                    A[r][c] = "X"
+                if A[r][c] == "N":
+                    A[r][c] = "O"
         
-        
-            
-        
+        return A
+                        
