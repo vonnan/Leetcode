1fa0f07e6692fa8bb020e43e74a0aac68e54a144
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-22 01:22:57 -0400

    Time: 84 ms (35.41%), Space: 16.2 MB (30.90%) - LeetHub

--- search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -7,7 +7,7 @@
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if not root:
-            return root
+            return
         
         if root.val == val:
             return root
