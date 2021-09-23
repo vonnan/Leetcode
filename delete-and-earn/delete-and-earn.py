551de7e306d15ece70bb7dc2cdd01aa665f6d888
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-23 02:04:25 -0400

    Time: 56 ms (75.29%), Space: 14.3 MB (98.85%) - LeetHub

--- delete-and-earn/delete-and-earn.py
+++ delete-and-earn/delete-and-earn.py
@@ -1,17 +1,16 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        prepre, pre, pre_num =0, 0, -inf
-        
+        prepre, pre, prenum = 0, 0, -inf
+        nums.sort()
         counter = Counter(nums)
         
-        lst = [ num for num in sorted(counter)]
-        
-        for num in lst:
-            if num > pre_num + 1:
+        for num in sorted(counter.keys()):
+            if num > prenum + 1:
                 curr = pre + counter[num] * num
-            else:
-                curr = max(prepre + counter[num] * num, pre)
                 
-            prepre, pre, pre_num = pre, curr, num
-        
+            else:
+                curr = max(pre, prepre + counter[num] * num)
+                
+            prepre, pre, prenum = pre, curr, num
+            
         return curr
