b346d1b835b9d223f8ed364e4df1a6255e2ff442
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-24 21:04:20 -0400

    Time: 36 ms (70.64%), Space: 14.6 MB (66.73%) - LeetHub

--- generate-parentheses/generate-parentheses.py
+++ generate-parentheses/generate-parentheses.py
@@ -3,17 +3,18 @@ class Solution:
         res = set([])
         
         def dfs(path, left, right):
-            if len(path) == 2* n:
-                res.add(path)
+            if len(path) == 2*n:
+                res.add(tuple(path))
                 return
-                
+            
             if left < n:
-                dfs(path + "(", left + 1, right)
+                dfs(path + "(", left +1, right)
+            
             if right < n and left > right:
                 dfs(path + ")", left, right + 1)
                 
         dfs("", 0, 0)
         
-        return res
+        return ["".join(x) for x in res]
                 
                 
