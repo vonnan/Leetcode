d3bb529ef508585939ffd441f509b373cc7115ae
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-09-30 02:00:31 -0400

    Time: 28 ms (91.70%), Space: 14.3 MB (48.01%) - LeetHub

--- integer-break/integer-break.py
+++ integer-break/integer-break.py
@@ -1,14 +1,22 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        max_p = (n-n//2)*(n//2)
-        if n <=4:
-            return max_p
-        for i in range(2, int(n//2)+1):
-            m = n//i
-            k = n % i
-            product = m**(i-k)*(m+1)**k
-            if product > max_p:
-                max_p = product
-        return max_p
-            
+        
+        if n ==2:
+            return 1
+        if n == 3:
+            return 2
+        if n == 4:
+            return 4
+        
+        n1, n2 = n//2, n- n//2
+        res = n1 * n2
+        
+        q,r = divmod(n, 3)
+        if r == 1:
+            return max(res, 3**(q-1) * 4)
+        elif r == 0:
+            return max(res, 3**q)
+        else:
+            return max(res, 3**q * 2)
+        
             
