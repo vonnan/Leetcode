244335760a9f437e9e251bd1f03e14597d653ada
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-10-07 00:55:14 -0400

    Time: 6564 ms (54.42%), Space: 14.4 MB (41.75%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,31 +1,38 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(idx , r, c):
-            
-            if idx == k:
-                self.status = True
-                
-            if self.status:
+        def dfs(idx,r,c):
+            if self.found:
                 return
             
-            if 0 <= r < row and 0 <= c <col and board[r][c] == word[idx]:
-                temp = board[r][c]
+            if idx == k:
+                self.found = True
+                return
+            
+            if 0 <= r < row and 0 <= c < col and board[r][c] ==word[idx]:
+                if idx == k-1:
+                    self.found = True
+                    return
+                
+                tmp = board[r][c]
                 board[r][c] = "*"
                 for dr, dc in path:
                     dfs(idx + 1, r + dr, c + dc)
-                board[r][c] = temp
+                
+                board[r][c] = tmp
                 
             else:
                 return
             
-        self.status = False
+        self.found = False
+        
         row, col, k = len(board), len(board[0]), len(word)
-        path = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        path = [(0,1), (0, -1), (1, 0), (-1, 0)]
         
         for r in range(row):
             for c in range(col):
-                if self.status:
+                if self.found:
                     return True
                 dfs(0, r, c)
-        return self.status
+        return self.found
                 
+        
