3e0410fcebcd83b8d9c18b2ccda0236b5b16c20c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-10-11 17:05:52 -0400

    Time: 80 ms (46.45%), Space: 14.7 MB (9.58%) - LeetHub

--- flood-fill/flood-fill.py
+++ flood-fill/flood-fill.py
@@ -1,20 +1,24 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        path = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
         color = image[sr][sc]
         row, col = len(image), len(image[0])
-        visited = set()
-        def dfs(r, c):
-            if (r,c) not in visited and 0 <= r < row and 0 <= c < col and image[r][c] == color:
+        
+        visited = set([])
+        
+        def dfs(r,c):
+            if 0 <= r < row and 0 <= c < col and (r,c) not in visited and image[r][c] == color:
                 image[r][c] = newColor
-                visited.add((r, c))
-                dfs(r-1, c)
+                visited.add((r,c))
                 dfs(r+1, c)
-                dfs(r, c-1)
-                dfs(r, c+1)
+                dfs(r-1, c)
+                dfs(r, c+ 1)
+                dfs(r, c- 1)
             else:
                 return
-        
+            
         dfs(sr, sc)
-        
         return image
+        
+        
+                
