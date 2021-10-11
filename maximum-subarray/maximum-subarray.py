20db1bc12df137bfe18359aeb819828a0d7fc165
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-10-11 16:10:53 -0400

    Time: 1301 ms (5.00%), Space: 28.6 MB (22.41%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -1,10 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         presum = [0]
-        res, min_sofar = -inf, 0
+        min_, res  = 0,  nums[0]
         for num in nums:
-            x = num + presum[-1]
-            res = max(res, x - min_sofar)
-            min_sofar = min(min_sofar, x)
-            presum.append(x)
+            last = presum[-1] + num
+            presum.append(last)
+            res = max(res, last - min_)
+            min_ = min(min_, last)
         return res
+            
