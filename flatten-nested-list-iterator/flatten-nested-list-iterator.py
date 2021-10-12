e52cc890fbe0bffe73f92c01a66fc8e5227e265b
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-10-12 14:18:28 -0400

    Time: 99 ms (17.80%), Space: 17.8 MB (40.47%) - LeetHub

--- flatten-nested-list-iterator/flatten-nested-list-iterator.py
+++ flatten-nested-list-iterator/flatten-nested-list-iterator.py
@@ -21,28 +21,23 @@
 #        """
 
 class NestedIterator:
-
-    def __init__(self, nestedList):
-        self.stack = [[nestedList, 0]]
-
-    def next(self):
-        self.hasNext()
-        nestedList, i = self.stack[-1]
-        self.stack[-1][1] += 1
-        return nestedList[i].getInteger()
+    def __init__(self, nestedList: [NestedInteger]):
+        self.stack = deque([nl for nl in nestedList])
+    
+    def next(self) -> int:
+        return self.stack.popleft().getInteger()
+    
+    def hasNext(self) -> bool:
+        while self.stack:
+            top = self.stack[0]
+            if top.isInteger():
+                return True
+            self.stack.popleft()
+            for nl in reversed(top.getList()):
+                
+                self.stack.appendleft(nl)
             
-    def hasNext(self):
-        s = self.stack
-        while s:
-            nestedList, i = s[-1]
-            if i == len(nestedList):
-                s.pop()
-            else:
-                x = nestedList[i]
-                if x.isInteger():
-                    return True
-                s[-1][1] += 1
-                s.append([x.getList(), 0])
+                
         return False
          
 
