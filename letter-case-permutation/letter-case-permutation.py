6a8c5ee48b0cac4f5ce775e5e86fc930bb251f7b
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-10-14 00:10:31 -0400

    Time: 72 ms (36.74%), Space: 14.9 MB (57.95%) - LeetHub

--- letter-case-permutation/letter-case-permutation.py
+++ letter-case-permutation/letter-case-permutation.py
@@ -1,15 +1,9 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        res = [""]
-        
-        for ch in s:
-            if ch.isdigit():
-                for i in range(len(res)):
-                    res[i] += ch
+        res = []
+        for c in s:
+            if c.isalpha():
+                res.append([c.upper(), c.lower()])
             else:
-                for i in range(len(res)):
-                    temp = res[i]
-                    res[i] += ch.lower()
-                    res.append(temp + ch.upper())
-        return res
-                    
+                res.append([c])
+        return ["".join(x) for x in product(*res)]
