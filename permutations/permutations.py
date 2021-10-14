c3eff891beda39d7a7b3c8e84cd7830fb5c5ae09
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-10-14 00:04:58 -0400

    Time: 44 ms (51.16%), Space: 14.4 MB (41.96%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -2,13 +2,12 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res, n = set([]), len(nums)
         
-        def dfs(num, path):
+        def dfs(nums, path):
             if len(path) == n:
                 res.add(tuple(path))
-            
-            else:
-                for i in range(len(num)):
-                    dfs(num[:i] + num[i+1:], path + [num[i]])
+                
+            for i, num in enumerate(nums):
+                dfs(nums[:i] + nums[i+1:], path +[num])
         
         dfs(nums, [])
         
