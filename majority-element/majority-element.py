1c4af13df1e59deede41405115f11083b5c4b296
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-10-21 00:40:01 -0400

    Time: 288 ms (11.20%), Space: 15.6 MB (53.30%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,7 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         counter = Counter(nums)
-        max_ = max(counter.values())
-        for key, val in counter.items():
-            if val == max_:
-                return key
+        return max(zip(counter.values(), counter.keys()))[1]
