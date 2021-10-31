7f6f6fa97e948c8d1a7833c1fefbd60255f04710
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-10-31 15:43:03 -0400

    Time: 300 ms (51.15%), Space: 20.7 MB (22.00%) - LeetHub

--- missing-element-in-sorted-array/missing-element-in-sorted-array.py
+++ missing-element-in-sorted-array/missing-element-in-sorted-array.py
@@ -1,17 +1,12 @@
-
+from bisect import bisect
 class Solution:
     def missingElement(self, nums: List[int], k: int) -> int:
-        missing = [num - nums[0] - i for i, num in enumerate(nums)]
-        
-        if k > missing[-1]:
-            return nums[-1] + k - missing[-1]
-        
-        lo, hi = 0, len(nums) - 1
-        while lo < hi:
-            mid = (lo + hi)//2
-            if missing[mid] < k:
-                lo = mid +1
-            else:
-                hi = mid
-        return nums[lo -1] + k - missing[lo - 1]
-                
+        first = nums[0]
+        lst = [num - first - i for i, num in enumerate(nums)]
+        idx = bisect_left(lst, k) - 1
+        if idx < len(nums):
+            return nums[idx] - lst[idx] + k
+        else:
+            return k -lst[-1] + nums[-1]
+          
+            
