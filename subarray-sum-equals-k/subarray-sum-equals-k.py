4fb75d656c3d8bf066e087c617cc83c847f0f139
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-02 01:32:38 -0400

    Time: 264 ms (59.97%), Space: 16.7 MB (85.92%) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.py
+++ subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,15 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         presum = [0]
-        for num in nums:
-            presum.append(presum[-1] + num)
         
         dic = defaultdict(int)
+        dic[0] = 1
+        
         res = 0
-        for i, num in enumerate(presum):
-            if num - k in dic:
-                res += dic[num - k]
-            dic[num] += 1
-            
+        for i, num in enumerate(nums):
+            last = presum[-1] + num
+            presum.append(last)
+            if last - k in dic:
+                res += dic[last - k]
+            dic[last] += 1
         return res
-            
