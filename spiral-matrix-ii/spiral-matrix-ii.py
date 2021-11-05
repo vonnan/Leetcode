a85cdd06cdbc53009e0658c9d22ed75d7ac6a2dc
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-04 20:13:33 -0400

    Time: 20 ms (99.47%), Space: 14.2 MB (76.23%) - LeetHub

--- spiral-matrix-ii/spiral-matrix-ii.py
+++ spiral-matrix-ii/spiral-matrix-ii.py
@@ -1,25 +1,27 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        matrix =[[0] * n for _ in range(n)]
+        matrix = [[1] * n for _ in range(n)]
         
-        rs,re, cs, ce= 0, n-1, 0, n-1
+        rs, cs, re, ce = 0, 0, n-1, n-1
         ct = 1
-        while rs<= re and cs <= ce:
+        
+        while cs <= ce and rs <= re:
             for c in range(cs, ce + 1):
                 matrix[rs][c] = ct
                 ct += 1
-            for r in range(rs + 1, re + 1):
+            for r in range(rs + 1, re):
                 matrix[r][ce] = ct
                 ct += 1
-            for c in range(ce-1, cs-1, -1):
+            for c in range(ce, cs, -1):
                 matrix[re][c] = ct
                 ct += 1
-            for r in range(re-1, rs, -1):
-                matrix[r][cs] = ct
+            for r in range(re, rs, -1):
+                matrix[r][cs] = ct 
                 ct += 1
+            
             rs += 1
-            re -= 1
             cs += 1
+            re -= 1
             ce -= 1
-        
+            
         return matrix
