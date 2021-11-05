92c2b2ac7fe0f01c15f28c8f6939a2c367eaffbb
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-05 15:52:03 -0400

    Time: 1164 ms (69.91%), Space: 45.9 MB (20.51%) - LeetHub

--- find-a-peak-element-ii/find-a-peak-element-ii.py
+++ find-a-peak-element-ii/find-a-peak-element-ii.py
@@ -1,14 +1,10 @@
 class Solution:
     def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
-        row, col = len(mat), len(mat[0])
-        for r in range(row):
-            mat[r] = [-1] + mat[r] + [-1]
-        
-        mat = [[-1]* (col + 2)] + mat + [[-1]* (col + 2)]
-        
-        path = [(-1, 0), (1,0), (0, 1), (0, -1)]
-        
-        for r in range(1, row +1):
-            for c in range(1, col + 1):
-                if all(mat[r][c] > mat[r + dr][c + dc] for dr, dc in path):
-                    return (r - 1,c -1)
+        top, bottom = 0, len(mat) -1
+        while top < bottom:
+            mid = (top + bottom)//2
+            if max(mat[mid]) > max(mat[mid + 1]):
+                bottom = mid
+            else:
+                top = mid + 1
+        return (top, mat[top].index(max(mat[top])))
