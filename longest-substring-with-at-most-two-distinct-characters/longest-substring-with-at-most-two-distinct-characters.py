653cd10e0be8d92f7663088d56288491499a961b
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-05 21:33:02 -0400

    Time: 496 ms (39.72%), Space: 14.8 MB (69.61%) - LeetHub

--- longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
+++ longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
@@ -1,29 +1,26 @@
-from collections import Counter
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
         counter = Counter()
-        res = i = 0
-        seen = set()
-        count = 0
-        for j, ss in enumerate(s):
-            if ss not in seen:
-                count += 1
-                seen.add(ss)
-            counter[ss] += 1
+        ct = 0
+        
+        left, res = 0, 0
+        for right, c in enumerate(s):
+            if c not in counter:
+                ct += 1
+                
             
-            while count > 2:
-                if counter[s[i]] ==1:
-                    count -= 1
-                    seen.remove(s[i])
-                counter[s[i]] -= 1
-                i += 1
+            while ct > 2:
+                if counter[s[left]] == 1:
+                    ct -= 1
+                    counter.pop(s[left])
+                else:
+                    counter[s[left]] -= 1
+                left += 1
+                
+                         
+            counter[c] += 1
             
-            res = max(res, j-i+1)
-            
+            res = max(res, right - left + 1)
+                
         return res
             
-            
-            
-            
-        
-        
