c7dc7c9ce27a956caf90d46f9465469b9f7fd0f8
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-08 15:48:26 -0500

    Time: 32 ms (73.16%), Space: 14.3 MB (57.04%) - LeetHub

--- house-robber-ii/house-robber-ii.py
+++ house-robber-ii/house-robber-ii.py
@@ -3,19 +3,13 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         
-        if len(nums) == 2:
-            return max(nums)
+        prev, curr = 0, nums[0]
+        for num in nums[1:-1]:
+            prev, curr = curr, max(prev + num, curr)
         
-        prev, curr = 0, 0
-        
-        for i, num in enumerate(nums[:-1]):
-            prev, curr = curr, max(curr, prev + num)
-            
         res = curr
+        prev, curr = 0, nums[1]
+        for num in nums[2:]:
+            prev, curr = curr, max(prev + num, curr)
         
-        prev, curr = 0, 0
-        
-        for i, num in enumerate(nums[1:], 1):
-            prev, curr = curr, max(curr, prev + num)
-            
         return max(res, curr)
