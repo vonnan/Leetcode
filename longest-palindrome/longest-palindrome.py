7cc9dc22692f05637003605b7b6d4a9772290fec
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-10 13:04:52 -0500

    Time: 28 ms (92.69%), Space: 14.3 MB (25.43%) - LeetHub

--- longest-palindrome/longest-palindrome.py
+++ longest-palindrome/longest-palindrome.py
@@ -1,10 +1,8 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         counter = Counter(s)
-        flag = False
-        res = 0
-        for val in counter.values():
-            res += val//2 * 2
-            if val % 2:
-                flag = True
-        return res if not flag else res + 1
+        res = sum([x//2*2 for x in counter.values()])
+        if sum([x%2 for x in counter.values()]):
+            res += 1
+        return res
+        
