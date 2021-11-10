50bb64cbd3ec244b32a15b0643a78f69bdcb1ae2
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-10 13:01:07 -0500

    Time: 40 ms (79.74%), Space: 14.6 MB (23.50%) - LeetHub

--- add-strings/add-strings.py
+++ add-strings/add-strings.py
@@ -1,22 +1,19 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        num1, num2 = list(num1), list(num2)
         res, carry = "", 0
-        
+        num1, num2 = list(num1), list(num2)
         while num1 and num2:
-            carry, num = divmod(int(num1.pop()) + int(num2.pop()) + carry, 10)
-            res = str(num) + res
-            
-        if not num1 and not num2:
-            if carry:
-                res = str(carry) + res
+            n1, n2 = int(num1.pop()), int(num2.pop())
+            carry, n = divmod(n1 + n2 + carry, 10)
+            res = str(n) + res
         
-        else:
-            nums = num1 or num2
-            while nums:
-                carry, num = divmod(int(nums.pop()) + carry, 10)
-                res = str(num) + res
-            if carry:
-                res = str(carry) + res
+        num = (num1 or num2)
+        
+        while num:
+            carry, n = divmod(int(num.pop()) + carry, 10)
+            res = str(n) + res
+            
+        if carry:
+            res = str(carry) + res
         
         return res
