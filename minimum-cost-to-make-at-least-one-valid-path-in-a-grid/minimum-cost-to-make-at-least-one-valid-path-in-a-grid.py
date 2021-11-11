e8bf07718a467f98c74f3128b58836b6adf80364
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-11 16:13:25 -0500

    Time: 368 ms (78.23%), Space: 16.1 MB (61.49%) - LeetHub

--- minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -1,23 +1,34 @@
+from heapq import heappush
+from heapq import heappop
+
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
-        moves = {1: (0, 1), 2: (0, -1), 3: (1, 0), 4: (-1, 0)}
-        heap = [(0, 0, 0)]
-        visited = set()
+        moves = {1: (0,1), 2: (0, -1), 3: (1, 0), 4: (-1, 0)}
         row, col = len(grid), len(grid[0])
+        
+        heap = [(0,0,0)]
+        visited = set()
         
         while heap:
             cost, r, c = heappop(heap)
-            if r == row-1 and c == col-1:
+            
+            if r == row-1 and c == col -1:
                 return cost
             
             if (r,c) in visited:
                 continue
-            visited.add((r,c))
-            
-            for move in moves:
-                dr, dc = moves[move]
+                
+            else:
+                visited.add((r, c))
+                
+            for move, val in moves.items():
+                dr, dc = val
                 nr, nc = r + dr, c + dc
-                if (nr, nc) in visited:
-                    continue
-                if 0 <= nr < row and 0 <= nc < col:
+                if (nr, nc) not in visited and 0 <= nr < row and 0 <= nc < col:
                     heappush(heap, (cost + (move != grid[r][c]), nr, nc))
+                    
+                    
+        
+                    
+
+            
