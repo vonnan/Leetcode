1e03e4ef8398976cb7b067d144de5faaec6598e9
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-10 19:54:55 -0500

    Time: 36 ms (90.52%), Space: 14.5 MB (15.50%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -3,12 +3,12 @@ class Solution:
         res, n = set([]), len(nums)
         
         def dfs(nums, path):
-            if len(path) == n:
+            if len(path) ==n:
                 res.add(tuple(path))
                 
             for i, num in enumerate(nums):
-                dfs(nums[:i] + nums[i+1:], path +[num])
+                dfs(nums[:i] + nums[i + 1:], path + [num])
         
         dfs(nums, [])
-        
         return res
+        
