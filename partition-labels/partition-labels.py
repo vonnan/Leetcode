5c7948100f8a1b2764f34b612adace2d9ebca8f5
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-10 23:33:19 -0500

    Time: 44 ms (55.54%), Space: 14.4 MB (25.82%) - LeetHub

--- partition-labels/partition-labels.py
+++ partition-labels/partition-labels.py
@@ -1,30 +1,29 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         sets = set(s)
-        dic = {c : (s.index(c), s.rindex(c)) for c in sets}
+        dic = {c: (s.index(c), s.rindex(c)) for c in sets}
         
-        for char in sets:
+        for char in dic:
             left, right = dic[char]
-            right_, left_ = -1, -1
-            while right_ != right or left_ != left:
-                right_, left_ = right, left
-                right = max(dic[c][1] for c in set(s[left: right + 1]))
-                left = min(dic[c][0] for c in set(s[left:right + 1]))
+            left_, right_ = -1, -1
+            while left_ != left or right_ != right:
+                left_, right_ = left, right
+                right = max(dic[c][1] for c in set(s[left:right + 1]))
+                left = min(dic[c][0] for c in set(s[left: right + 1]))
             dic[char] = (left, right)
+            
+        lst = list(set(dic.values()))
+        lst.sort()
         
-        print(dic)
-        lst = list(dic.values())
-        lst.sort(key = lambda x: (x[0], - x[1]))
-        l, r = lst[0]
-        res = [r -l +1]
-        for left, right in lst[1:]:
-            if left < r:
+        left, right = lst[0]
+        res= [right - left + 1]
+        for l,r in lst[1:]:
+            if l < right:
                 continue
-            else:
-                l, r = left, right
-                res.append(r - l + 1)
-        return res
+            left, right = l, r
+            res.append(right - left + 1)
             
         
-        
+        return res
+            
         
