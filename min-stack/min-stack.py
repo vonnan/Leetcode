4dbaea40e1e4872d02f4a111992b1580e7063237
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-12 13:24:24 -0500

    Time: 64 ms (69.07%), Space: 18.3 MB (36.12%) - LeetHub

--- min-stack/min-stack.py
+++ min-stack/min-stack.py
@@ -1,21 +1,24 @@
+from bisect import insort
 class MinStack:
 
     def __init__(self):
         self.stack = []
-        
+        self.minstack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        insort(self.minstack, val)
         
 
     def pop(self) -> None:
-        self.stack.pop()
+        x = self.stack.pop()
+        self.minstack.remove(x)
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.minstack[0]
 
 
 # Your MinStack object will be instantiated and called as such:
