ddb43e6e1edbf42dd4f5bf1bc1ab37efe3a1078b
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-15 15:01:55 -0500

    Time: 52 ms (90.53%), Space: 14.6 MB (73.40%) - LeetHub

--- n-queens/n-queens.py
+++ n-queens/n-queens.py
@@ -1,26 +1,25 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        res, board = [], []
-        
-        col, diag, off_diag = set(), set(), set()
+        col, diag, off_d = set([]), set([]), set([])
+        board, res = [], []
         
         def dfs(r):
             if r == n:
-                res.append(list(board))
-                
-            for c in range(n):
-                if c not in col and r+ c not in off_diag and r-c not in diag:
-                    col.add(c)
-                    off_diag.add(r+c)
-                    diag.add(r -c)
-                    board.append("."*c + "Q" + "."*(n-1-c))
-                    dfs(r + 1)
-                    board.pop()
-                    col.remove(c)
-                    off_diag.remove(r+c)
-                    diag.remove(r - c)
-         
-        dfs(0)
+                res.append(board[:])
+            
+            else:
+                for c in range(n):
+                    if c not in col and (r-c not in diag) and (r + c not in off_d):
+                        col.add(c)
+                        diag.add(r - c)
+                        off_d.add(r + c)
+                        board.append(c * "." + "Q" + (n-1-c) * ".")
+                        dfs(r + 1)
+                        board.pop()
+                        col.remove(c)
+                        diag.remove(r-c)
+                        off_d.remove(r+c)
         
+        dfs(0)
         return res
-                    
+                        
