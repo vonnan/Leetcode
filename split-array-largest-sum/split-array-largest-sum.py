1fa50952a9bd0a88527e8247883faca321ff2bb5
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-16 13:30:39 -0500

    Time: 36 ms (79.49%), Space: 14.4 MB (33.99%) - LeetHub

--- split-array-largest-sum/split-array-largest-sum.py
+++ split-array-largest-sum/split-array-largest-sum.py
@@ -3,18 +3,19 @@ class Solution:
         left, right = max(nums), sum(nums)
         while left < right:
             mid = (left + right)//2
-            ct, group = 0, 1
+            sum_, ct = 0, 1
             for num in nums:
-                if ct + num > mid:
-                    group += 1
-                    ct = num
-                    if group > m:
-                        left = mid + 1
+                if sum_ + num > mid:
+                    sum_= num
+                    ct += 1
+                    if ct > m:
                         break
                 else:
-                    ct += num
-            if group <= m:
+                    sum_ += num
+            if ct > m:
+                left = mid + 1
+            else:
                 right = mid
+        
         return left
-            
-            
+                
