2ec7f5f8d221e54e0f46006b17c91b96599426eb
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-16 15:30:06 -0500

    Time: 40 ms (90.47%), Space: 16.2 MB (6.00%) - LeetHub

--- regular-expression-matching/regular-expression-matching.py
+++ regular-expression-matching/regular-expression-matching.py
@@ -7,11 +7,9 @@ class Solution:
         if not s:
             return len(p) > 1 and p[1] == "*" and self.isMatch(s, p[2:])
         
-        matched = (s[0] == p[0]) or (p[0] == ".")
+        match = s[0] == p[0] or (p[0] == ".")
         
         if len(p) > 1 and p[1] == "*":
-            return (matched and self.isMatch(s[1:], p)) or self.isMatch(s, p[2:])
-                
-        return matched and self.isMatch(s[1:], p[1:])
+            return (match and self.isMatch(s[1:], p)) or self.isMatch(s, p[2:])
         
-        
+        return match and self.isMatch(s[1:], p[1:])
