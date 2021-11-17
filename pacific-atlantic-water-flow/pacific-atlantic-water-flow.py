1e342a10dfedcd6a99b342f1c5b45687827c57df
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-16 20:19:25 -0500

    Time: 264 ms (96.61%), Space: 17.5 MB (10.31%) - LeetHub

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,31 +1,34 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if not heights:
-            return []
         
         row, col = len(heights), len(heights[0])
+        visited_p = set([])
+        visited_a = set([])
+        
         path = [(0,1), (0, -1), (1, 0), (-1, 0)]
         
-        seen_a, seen_p = set(), set()
-        
-        def dfs(r, c, seen):
-            seen.add((r,c))
-            for dr, dc in path:
+        def dfs(r,c, visited):
+            if (r,c) in visited:
+                return
+            
+            visited.add((r, c))
+            
+            for dr,dc in path:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < row and 0 <= nc < col and (nr, nc) not in seen and heights[nr][nc] >= heights[r][c]:
-                    dfs(nr, nc, seen)
-                    
-        # initialize
+                if 0<= nr< row and 0 <= nc < col and (nr, nc) not in visited:
+                    if heights[nr][nc] >= heights[r][c]:
+                        dfs(nr, nc, visited)
         
         for r in range(row):
-            dfs(r, 0, seen_p)
-            dfs(r, col -1, seen_a)
-            
+            dfs(r, 0, visited_p)
+            dfs(r, col -1, visited_a)
+        
         for c in range(col):
-            dfs(0, c, seen_p)
-            dfs(row - 1, c, seen_a)
-            
-            
-        return [(r,c) for r in range(row) for c in range(col) if (r,c) in seen_a and (r,c) in seen_p]
-                    
-                    
+            dfs(0, c, visited_p)
+            dfs(row -1, c, visited_a)
+        
+        return visited_p & visited_a
+        
+    
+        
+        
