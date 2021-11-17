f4a418aa84f40107f42bebcf8b7419d9fcce0df5
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-16 21:30:13 -0500

    Time: 188 ms (96.61%), Space: 15.6 MB (45.45%) - LeetHub

--- maximal-rectangle/maximal-rectangle.py
+++ maximal-rectangle/maximal-rectangle.py
@@ -2,32 +2,32 @@ class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         if not matrix:
             return 0
+        row, col = len(matrix), len(matrix[0])
         
-        row, col, res = len(matrix), len(matrix[0]), 0
         heights = [0] * col
+        res = 0
         
-        def maxArea(height):
-            maxArea, n = 0, len(height)
+        def maxArea(H):
+            ans, n = 0, len(H)
             stack = []
-            for i, h in enumerate(height):
+            for i, h in enumerate(H):
                 j = i
                 while stack and stack[-1][0] > h:
                     ht, j = stack.pop()
-                    maxArea = max(maxArea, ht * (i - j))
+                    ans = max(ans, ht * (i - j))
                 stack.append((h, j))
+            
             for h, i in stack:
-                maxArea = max(maxArea, h * (n- i))
-            return maxArea
-            
-        for rows in matrix:
-            for i in range(col):
-                if rows[i] == "1":
-                    heights[i] += 1
-                else:
-                    heights[i] = 0
-            res = max(res, maxArea(heights))
-            
-        return res
-            
+                ans = max(ans, (n- i) * h)
+            return ans
         
+        for rows in matrix:
+            for c in range(col):
+                if rows[c] == "1":
+                    heights[c] += 1
+                else:
+                    heights[c] = 0
+            res = max(res, maxArea(heights))
+        
+        return res
             
