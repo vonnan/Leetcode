dfc107633a63df8fad10eae98ab060686c1a76f3
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-17 00:46:11 -0500

    Time: 32 ms (68.74%), Space: 14.2 MB (52.76%) - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.py
+++ pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,11 +1,12 @@
 class Solution:
-    def getRow(self, r: int) -> List[int]:
-        res = [[1] * (i + 1) for i in range(r + 1)]
-        if r <= 1:
+    def getRow(self, n: int) -> List[int]:
+        res = [[1] * (i + 1) for i in range(n + 1)]
+        
+        if n <= 1:
             return res[-1]
         
-        for i in range(1, r + 1):
+        for i in range(2, n + 1):
             for j in range(1, i):
                 res[i][j] = res[i-1][j-1] + res[i-1][j]
-        
+                
         return res[-1]
