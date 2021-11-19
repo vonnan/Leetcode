4950b971369921c3aec650756995ef934b17e9a1
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-19 14:43:43 -0500

    Time: 40 ms (55.54%), Space: 14.5 MB (37.07%) - LeetHub

--- predict-the-winner/predict-the-winner.py
+++ predict-the-winner/predict-the-winner.py
@@ -1,27 +1,23 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         memo = {}
-        
         def dfs(left, right):
+            
             if (left, right) in memo:
                 return memo[(left, right)]
             
             if left > right:
                 return 0
-            
-            if left == right:
+            elif left == right:
                 return nums[left]
             
-            pick_left = nums[left] + min(dfs(left+1, right -1), dfs(left + 2, right))
-            pick_right = nums[right] + min(dfs(left+1, right-1), dfs(left, right -2))
+            else:
+                pick_left = nums[left] + min(dfs(left + 1, right -1), dfs(left + 2, right))
+                pick_right = nums[right] + min(dfs(left, right -2), dfs(left + 1, right - 1))
+
+                score = max(pick_left, pick_right)
+                memo[(left, right)] = score
+                
+                return score
             
-            score = max(pick_left, pick_right)
-            
-            memo[(left, right)] = score
-            
-            return score
-        
-        score = dfs(0, len(nums) - 1)
-        
-        
-        return 2 * score >= sum(nums)
+        return dfs(0, len(nums) - 1) >= (sum(nums)/2)
