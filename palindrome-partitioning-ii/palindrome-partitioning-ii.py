b9265fd40826a329766ad17d6d3353368d7ab308
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-19 16:58:44 -0500

    Time: 1660 ms (34.14%), Space: 14.3 MB (86.54%) - LeetHub

--- palindrome-partitioning-ii/palindrome-partitioning-ii.py
+++ palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -1,8 +1,10 @@
 class Solution:
     def minCut(self, s: str) -> int:
-        cut = [x for x in range(-1,len(s))]
-        for i in range(0,len(s)):
-            for j in range(i,len(s)):
+        n = len(s)
+        cut =[i for i in range(-1, n)]
+        
+        for i in range(n):
+            for j in range(i, n):
                 if s[i:j] == s[j:i:-1]:
-                    cut[j+1] = min(cut[j+1],cut[i]+1)
+                    cut[j + 1] = min(cut[j + 1], cut[i] + 1)
         return cut[-1]
