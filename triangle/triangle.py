9d1357ec986642a0c6ec4d747eb551cdc63ed29d
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-20 17:51:54 -0500

    Time: 56 ms (98.31%), Space: 14.8 MB (98.95%) - LeetHub

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,15 +1,10 @@
 class Solution:
     def minimumTotal(self, A: List[List[int]]) -> int:
         n = len(A)
-        dp = [[0] * i for i in range(1, n+1)]
-        dp[0][0] = A[0][0]
-        
         for r in range(1, n):
-            dp[r][0] = dp[r-1][0] + A[r][0]
-            dp[r][r] = dp[r-1][r-1] + A[r][r]
-            
+            A[r][0] += A[r-1][0]
+            A[r][r] += A[r-1][r-1]
             for c in range(1, r):
-                dp[r][c] = min(dp[r-1][c-1], dp[r-1][c]) + A[r][c]
+                A[r][c] += min(A[r -1][c], A[r-1][c-1])
         
-        return min(dp[-1])
-                
+        return min(A[-1])
