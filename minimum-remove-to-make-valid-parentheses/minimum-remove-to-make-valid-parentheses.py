9224dd68a385c28a6e4b64b0a2e38a60eb38b260
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-21 13:30:36 -0500

    Time: 120 ms (67.14%), Space: 15.9 MB (60.26%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,31 +1,23 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        left, right = 0, 0
+
         stack = []
-        for val in s:
-            stack.append(val)
-            if val == "(":
-                left+= 1
-            elif val == ")":
-                right+= 1
-                
-                if right > left:
-                    stack.pop()
-                    right -= 1
-        if left == right:
-            return "".join(stack)
-        left, right = 0, 0
+        sets = set()
+        for i, c in enumerate(s):
+            if c in "()":
+                if c == "(":
+                    stack.append(i)
+                else:
+                    if not stack:
+                        sets.add(i)
+                    else:
+                        stack.pop()
+        sets |= set(stack)
+        res = ""
+        for i, c in enumerate(s):
+            if i in sets:
+                continue
+            res += c
         
-        stack_r = []
-        for val in stack[::-1]:
-            stack_r.append(val)
-            if val == ")":
-                right += 1
-            elif val == "(":
-                left += 1
-                if left > right:
-                    stack_r.pop()
-                    left -= 1
-        return "".join(stack_r[::-1])
-            
-            
+        return res
+                    
