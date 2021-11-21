cdf05ed38e80bde37e7cd1eb88135d74bf3b6d66
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-20 20:53:32 -0500

    Time: 42 ms (61.52%), Space: 14.1 MB (95.82%) - LeetHub

--- unique-paths-ii/unique-paths-ii.py
+++ unique-paths-ii/unique-paths-ii.py
@@ -6,22 +6,16 @@ class Solution:
         
         row, col = len(A), len(A[0])
         
-        path = [(0,1), (1, 0)]
-        
         dp =[[0] * col for _ in range(row)]
         dp[0][0] = 1
-        for c in range(1, col):
-            dp[0][c] = dp[0][c-1] * (1- A[0][c])  
         
-        for r in range(1, row):
-            dp[r][0] = dp[r-1][0] * (1-A[r][0])
-        
-        for r in range(1,row):
-            for c in range(1,col):
-                if A[r][c] == 1:
-                    dp[r][c] = 0
-                    continue
-                dp[r][c] = dp[r][c-1] + dp[r-1][c]
+        for r in range(row):
+            for c in range(col):
+                if not A[r][c]:
+                    if r > 0:
+                        dp[r][c] += dp[r-1][c]
+                    if c > 0:
+                        dp[r][c] += dp[r][c-1]
                 
         
         return dp[-1][-1]
