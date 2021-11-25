d02ebb4dec43b5ed559f3cbf2b8e2f4d0bb79049
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-25 00:30:42 -0500

    Time: 1784 ms (32.55%), Space: 14.5 MB (64.67%) - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -1,12 +1,14 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        ways = [inf] * (amount + 1)
-        ways[0] = 0
         coins.sort()
+        dp = [inf] * (amount + 1)
+        dp[0] = 0
         
         for amt in range(1, amount + 1):
-            for coin in coins:
-                if coin <= amt:
-                    ways[amt] = min(ways[amt], ways[amt - coin] + 1)
-        
-        return ways[-1] if ways[-1] != inf else -1
+            for coin in coins[::-1]:
+                if coin == amt:
+                    dp[amt] = 1
+                elif coin < amt:
+                    dp[amt] = min(dp[amt], dp[amt - coin] + 1)
+        return dp[-1] if dp[-1] != inf else -1
+                
