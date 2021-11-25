e88804dadab828b94e9e5375f9708a901c8fa94a
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-25 00:57:58 -0500

    Time: 140 ms (83.17%), Space: 14.6 MB (50.73%) - LeetHub

--- coin-change-2/coin-change-2.py
+++ coin-change-2/coin-change-2.py
@@ -1,11 +1,14 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] *(amount + 1)
-        coins =sorted([coin for coin in coins if coin <= amount])
+        n = len(coins)
+        coins.sort()
+        dp = [0] * (amount + 1)
         dp[0] = 1
         
         for coin in coins:
+            if coin > amount:
+                break
             for amt in range(coin, amount + 1):
-                dp[amt] += dp[amt-coin]
-        
+                dp[amt] += dp[amt - coin]
         return dp[-1]
+        
