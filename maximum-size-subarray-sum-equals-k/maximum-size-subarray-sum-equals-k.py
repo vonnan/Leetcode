be6c5899e9f14335e8436ccd7bfb32baaf013bfe
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-26 18:42:18 -0500

    Time: 412 ms (28.93%), Space: 51.2 MB (23.51%) - LeetHub

--- maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
+++ maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
@@ -1,16 +1,16 @@
 class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
         presum = [0]
-        for num in nums:
-            presum.append(presum[-1] + num)
-        
-        dic = {}
         res = 0
-        for i, num in enumerate(presum):
-            if num - k in dic:
-                res = max(res, i - dic[num - k])
-            if num not in dic:
-                dic[num] = i
+        dic = defaultdict(int)
+        dic[0] = -1
+        
+        for i, num in enumerate(nums):
+            nxt = presum[-1] + num
+            if nxt - k in dic:
+                res = max(res, i - dic[nxt - k])
+            if nxt not in dic:
+                dic[nxt] = i
+            presum.append(nxt)
+        
         return res
-                
-            
