0a11b94c460534fc6bd0b320cc7284979a7d61c9
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-25 19:30:15 -0500

    Time: 37 ms (35.25%), Space: 13.9 MB (98.82%) - LeetHub

--- integer-break/integer-break.py
+++ integer-break/integer-break.py
@@ -1,16 +1,15 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        
-        res = (n//2) * (n - n//2)
-        if n <=3:
+        res = n//2 * (n - n//2)
+        if n <= 3:
             return res
-        q, r = divmod(n, 3)
         
-        if r ==0:
-            return max(res, 3 ** q)
+        q,r = divmod(n, 3)
+        if r==0:
+            return max(res, 3** q)
         
-        elif r == 1:
+        elif r ==1:
             return max(res, 3 **(q-1) * 4)
         
-        elif r== 2:
+        elif r == 2:
             return max(res, 3 ** q * 2)
