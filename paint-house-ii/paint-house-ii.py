828c4b24b2d8e8d784e8019c556af4339cd6e8f1
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-29 14:40:04 -0500

    Time: 120 ms (65.94%), Space: 14.5 MB (15.02%) - LeetHub

--- paint-house-ii/paint-house-ii.py
+++ paint-house-ii/paint-house-ii.py
@@ -2,25 +2,13 @@ from heapq import heappush
 from heapq import heappop
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
-        rows, cols = len(costs), len(costs[0])
-        dp = [[0]* cols for _ in range(rows)]
+        row, col = len(costs), len(costs[0])
+        dp = [[0] * col for _ in range(row)]
         dp[0] = costs[0]
-        
-        for r in range(1, rows):
-            heap = []
-            row = dp[r-1]
-            for c in range(cols):
-                heappush(heap, (row[c], c))
-            
-            minc, col = heappop(heap)
-            for c in range(cols):
-                if c!= col:
-                    dp[r][c] = minc + costs[r][c]
-                else:
-                    dp[r][c] = heap[0][0] + costs[r][c]
-            
-        return min(dp[-1])
-        
+        for r , rows in enumerate(costs[1:], 1):
+            for c in range(col):
+                dp[r][c] = min(dp[r-1][:c] + dp[r-1][c+1:]) + costs[r][c]
+        return min(dp[-1])        
                 
                 
         
