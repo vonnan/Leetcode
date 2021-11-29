893759450bdb37e9cd37452d217fd93d8565d9b9
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-11-29 01:29:01 -0500

    Time: 196 ms (87.18%), Space: 15.5 MB (87.98%) - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        i, n = 0, len(arr)
+        while i + 1 < n and arr[i] < arr[i+1]:
+            i += 1
+        
+        if i == 0 or i == n - 1:
+            return False
+        
+        while i + 1 < n and arr[i] > arr[i+ 1]:
+            i += 1
+            
+        return i == n - 1
