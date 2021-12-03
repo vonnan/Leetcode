f5d4ca5d4c17ab5ccbb207d5956f82673d8a3391
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-02 23:02:23 -0500

    Time: 36 ms (65.65%), Space: 14.4 MB (13.93%) - LeetHub

--- wiggle-subsequence/wiggle-subsequence.py
+++ wiggle-subsequence/wiggle-subsequence.py
@@ -1,18 +1,12 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [0] * n
-        
-        if not nums:
-            return 0
-        
         diff = [0] + [nums[i] - nums[i-1] for i in range(1, n)]
         
         res = 1
-        for i, val in enumerate(diff[1:],1):
-            if (val < 0 and diff[i-1] >= 0) or (val > 0 and diff[i-1] <= 0):
+        for i, val in enumerate(diff[1:], 1):
+            if (val > 0 and diff[i-1] <= 0) or (val < 0 and diff[i-1] >= 0):
                 res += 1
-            if val == 0:
+            elif val == 0:
                 diff[i] = diff[i-1]
         return res
-            
