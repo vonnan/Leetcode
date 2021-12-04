dc7d6c685b7c2101c68aabe253345dac31383bc8
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-04 16:42:33 -0500

    Time: 64 ms (61.52%), Space: 14.6 MB (79.68%) - LeetHub

--- shortest-palindrome/shortest-palindrome.py
+++ shortest-palindrome/shortest-palindrome.py
@@ -1,19 +1,9 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        if s==s[::-1]:
+        rev = s[::-1]
+        if not s or s == rev:
             return s
-        flag = False
-        for i in range(len(s)//2, 0, -1):
-            if s[:i] == s[i+1:2*i + 1][::-1] or s[:i] == s[i:2*i][::-1]:
-                flag = True
-                break
-        
-        if not flag:
-            return s[1:][::-1] + s
-        if s[:i] == s[i+1:2*i + 1][::-1]:
-            return s[-(len(s) - (2*i + 1)):][::-1] + s
-        
-        else:
-            return s[-(len(s) - 2*i):][::-1] + s
-            
-            
+        n = len(s)
+        for j in range(n-1, 0, -1):
+            if s[:j] == rev[n-j:]:
+                return s[j:][::-1] + s
