7dbda616fbe1db26e98d61952f72936e8082290d
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-05 02:09:41 -0500

    Time: 80 ms (53.71%), Space: 15.8 MB (29.33%) - LeetHub

--- trapping-rain-water/trapping-rain-water.py
+++ trapping-rain-water/trapping-rain-water.py
@@ -6,14 +6,14 @@ class Solution:
         
         right = [0]
         for h in height[::-1]:
-            right.append(max(h, right [-1]))
-        
+            right.append(max(h, right[-1]))
+            
         left, right = left[1:], right[1:][::-1]
-        
-        n, res = len(height), 0
+        res = 0
         for i, h in enumerate(height):
-            bar = min(left[i], right[i])
+            l,r = left[i], right[i]
+            bar = min(l,r)
             if h < bar:
-                res += bar - h
+                res += (bar - h)
         return res
-                
+            
