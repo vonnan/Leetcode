2c679a3b85eacc0f3a91dd76a25fb46005ad4ddc
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-05 01:00:50 -0500

    Time: 44 ms (90.83%), Space: 16.1 MB (83.62%) - LeetHub

--- house-robber-iii/house-robber-iii.py
+++ house-robber-iii/house-robber-iii.py
@@ -8,12 +8,10 @@ class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
         def dfs(node):
             if not node:
-                return (0,0)
+                return 0, 0
             
             left = dfs(node.left)
             right = dfs(node.right)
             
-            return (left[1] + right[1], max(left[1] + right[1], node.val + left[0] + right[0]))
-        
+            return left[1] + right[1], max(left[1] + right[1], node.val + left[0] + right [0])
         return dfs(root)[1]
-        
