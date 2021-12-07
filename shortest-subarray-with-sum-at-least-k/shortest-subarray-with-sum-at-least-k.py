23dfe5669d760945dedc5e3da48092be19f1bb46
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-07 15:57:49 -0500

    Time: 2432 ms (5.04%), Space: 35.9 MB (5.04%) - LeetHub

--- shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
+++ shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -1,20 +1,21 @@
-from heapq import heappop
 from heapq import heappush
+from heapq import heappop
 
 class Solution:
-    def shortestSubarray(self, nums: List[int], k: int) -> int:
-        heap = [(0, -1)]
-        presum, res  = 0, inf
+    def shortestSubarray(self, A: List[int], K: int) -> int:
         
-        for i, num in enumerate(nums):
-            presum += num
-            target = presum - k
+        presum = 0
+        heap = [(0,-1)]
+        res = len(A) + 1
+        
+        for i,a in enumerate(A):
+            presum += a
+            diff = presum -K
             
-            while heap and heap[0][0] <= target:
-                psum, idx = heappop(heap)
-                res = min(res, i - idx)
+            while heap and heap[0][0] <= diff:
+                psum,idx = heappop(heap)
+                res = min(res, i - idx )
             
             heappush(heap, (presum, i))
-        
-        return res if res != inf else -1
-        
+            
+        return res if res <= len(A) else -1
