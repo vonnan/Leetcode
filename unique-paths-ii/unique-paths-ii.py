78a374b0bb77a0139c1d83a8262115ece525e4de
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-13 01:15:22 -0500

    Time: 36 ms (94.79%), Space: 14.4 MB (63.29%) - LeetHub

--- unique-paths-ii/unique-paths-ii.py
+++ unique-paths-ii/unique-paths-ii.py
@@ -1,21 +1,22 @@
-from collections import defaultdict
 class Solution:
     def uniquePathsWithObstacles(self, A: List[List[int]]) -> int:
-        if A[0][0] ==1:
-            return 0
-        
         row, col = len(A), len(A[0])
         
-        dp =[[0] * col for _ in range(row)]
+        dp = [[0] * col for _ in range(row)]
+        
+        if A[0][0] or A[-1][-1]:
+            return 0
+        
         dp[0][0] = 1
         
         for r in range(row):
             for c in range(col):
-                if not A[r][c]:
-                    if r > 0:
-                        dp[r][c] += dp[r-1][c]
-                    if c > 0:
-                        dp[r][c] += dp[r][c-1]
-                
-        
+                if A[r][c]:
+                    continue
+                if r > 0:
+                    dp[r][c] += dp[r-1][c]
+                if c > 0:
+                    dp[r][c] += dp[r][c-1]
+                    
         return dp[-1][-1]
+            
