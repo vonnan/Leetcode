595995aecbcc370331ea8c3da2c26aff670391fd
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-13 00:19:43 -0500

    Time: 84 ms (98.71%), Space: 14.9 MB (76.47%) - LeetHub

--- factor-combinations/factor-combinations.py
+++ factor-combinations/factor-combinations.py
@@ -1,11 +1,12 @@
 class Solution:
     def getFactors(self, n: int) -> List[List[int]]:
         res = []
+        
         def helper(prev, arr, m):
-            for i in range(prev, int(m ** 0.5) + 1):
-                if m % i== 0:
+            for i in range(prev, int(m**0.5) +1):
+                if m % i == 0:
                     res.append(arr + [i, m//i])
                     helper(i, arr + [i], m//i)
-                    
+        
         helper(2, [], n)
         return res
