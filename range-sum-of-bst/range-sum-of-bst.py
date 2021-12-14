f8c771f5011c264bc36bc1993d880968372d06b9
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-14 01:41:14 -0500

    Time: 260 ms (39.37%), Space: 22.3 MB (16.08%) - LeetHub

--- range-sum-of-bst/range-sum-of-bst.py
+++ range-sum-of-bst/range-sum-of-bst.py
@@ -6,10 +6,10 @@
 #         self.right = right
 from bisect import bisect_left
 from bisect import bisect
+
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         lst = []
-        
         def dfs(node):
             if not node:
                 return
@@ -17,11 +17,10 @@ class Solution:
             dfs(node.left)
             lst.append(node.val)
             dfs(node.right)
-        
-        dfs(root)
-        idx_l = bisect_left(lst, low)
-        idx_r = bisect(lst, high)
-        
-        return sum(lst[idx_l:idx_r])
             
+        dfs(root)
         
+        left = bisect_left(lst, low)
+        right = bisect(lst, high)
+        
+        return sum(lst[left: right])
