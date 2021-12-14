ff4551ac7c110e12156655603ce157e5cecb4892
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-14 16:57:52 -0500

    Time: 116 ms (59.48%), Space: 15.7 MB (18.43%) - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,39 +1,29 @@
 from heapq import heappush
 from heapq import heappop
+
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        price = defaultdict(dict)
-        for u,v,fare in flights:
-            price[u][v] = fare
+        heap = [(0, 0, src)]
+        k += 1
+        graph = defaultdict(set)
+
+        for u,v,cost in flights:
+            graph[u].add((v, cost))
             
-        seen = defaultdict(int)
-        heap =[(0,src,k + 1)]
+        seen = {src: 0}
         
         while heap:
-            cost, u, stop_budget = heappop(heap)
+            cost, stop, node = heappop(heap)
+            seen[node] = stop
             
-            if u == dst:
-                return cost
-            
-            seen[u] = stop_budget
-            
-            if stop_budget >0:
-            
-                for nei in price[u]:
-                    if nei in seen and seen[nei] >= stop_budget -1:
+            if stop <= k :
+                if node == dst:
+                    return cost
+                for v, fare in graph[node]:
+                    if v in seen and seen[v] <= stop + 1:
                         continue
                     
-                    fare = price[u][nei]
-                    heappush(heap, (cost + fare, nei, stop_budget -1))
+                    heappush(heap, (cost + fare, stop + 1, v))
                     
         return -1
                 
-                
-            
-            
-            
-            
-            
-            
-        
-        
