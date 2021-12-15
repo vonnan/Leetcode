b5725d6a8131e9bc6af0edc63fc5edaed4ab6734
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-15 13:59:57 -0500

    Time: 806 ms (15.98%), Space: 34.1 MB (84.97%) - LeetHub

--- number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -1,20 +1,19 @@
 class Solution:
-    def makeConnected(self, n: int, A: List[List[int]]) -> int:
-        if len(A) < n-1:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n-1:
             return -1
-        
         UF = {i: i for i in range(n)}
         def find(x):
             if UF[x] != x:
                 UF[x] = find(UF[x])
             return UF[x]
         
-        def union(x, y):
+        def union(x,y):
             UF.setdefault(x, x)
             UF.setdefault(y, y)
             UF[find(x)] = find(y)
-        
-        for a,b in A:
-            union(a, b)
-        
-        return len(set([find(i) for i in range(n)])) - 1
+            
+        for a,b in connections:
+            union(a,b)
+            
+        return len(set(find(i) for i in range(n))) - 1
