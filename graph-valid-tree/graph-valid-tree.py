afb23dda675ab467ec464e0ad9d7a2f21b7295ae
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-15 00:24:42 -0500

    Time: 165 ms (8.96%), Space: 16.8 MB (19.99%) - LeetHub

--- graph-valid-tree/graph-valid-tree.py
+++ graph-valid-tree/graph-valid-tree.py
@@ -1,28 +1,21 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if not edges:
-            if n ==1:
-                return True 
-            else:
-                return False
-        
-        UF = {}
+        UF = {i: i for i in range(n)}
         
         def find(x):
             if UF[x] != x:
                 UF[x] = find(UF[x])
             return UF[x]
         
-        def union(x, y):
-            UF.setdefault(x,x)
+        def union(x,y):
+            UF.setdefault(x, x)
             UF.setdefault(y, y)
             UF[find(x)] = find(y)
             
         for a,b in edges:
-            if a in UF and b in UF and find(a) == find(b):
+            if find(a) == find(b):
                 return False
-            union(a,b)
-        
-        
-        return len(set(find(i) for i in range(n) if i in UF)) + sum(i for i in range(n) if i not in UF) == 1
-        
+            else:
+                n-= 1
+                union(a,b)
+        return n==1
