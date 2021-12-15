8a5b072b899fa5415edc4d7f588b42878836847c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-15 17:00:52 -0500

    Time: 689 ms (30.51%), Space: 20.9 MB (11.53%) - LeetHub

--- number-of-islands-ii/number-of-islands-ii.py
+++ number-of-islands-ii/number-of-islands-ii.py
@@ -1,44 +1,35 @@
 class Solution:
     def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
-        
-        UF = {}
+        UF= {}
         
         def find(x):
             if UF[x] != x:
                 UF[x] = find(UF[x])
+                
             return UF[x]
         
         def union(x, y):
             UF.setdefault(x, x)
             UF.setdefault(y, y)
             UF[find(x)] = find(y)
-            
-        path =[(0, 1), (0, -1), (1, 0), (-1, 0)]
-        res = [1]
         
-        UF.setdefault((positions[0][0], positions[0][1]), (positions[0][0], positions[0][1]))
-        for r, c in positions[1:]:
+        res = [1]
+        r,c = positions[0]
+        UF[(r,c)] = (r,c)
+        path = [(0,1), (0, -1), (1, 0), (-1, 0)]
+        for r,c in positions[1:]:
             if (r,c) in UF:
                 res.append(res[-1])
                 continue
-    
-            UF.setdefault((r, c), (r, c))
-            sets = set()
+            UF.setdefault((r,c), (r,c))
+            sets = set([])
             for dr, dc in path:
-                nr, nc = r+ dr, c + dc
-                if (nr, nc) in UF:
+                nr, nc = r + dr, c + dc
+                if (nr,nc) in UF:
                     sets.add((nr, nc))
-                          
-            res.append(res[-1] - len(set(find((nr, nc)) for (nr, nc) in sets)) + 1)
-            
-            for (nr, nc) in sets:
-                union((nr, nc), (r, c))
-            
+            res.append(res[-1] + 1 - len(set(find((nr, nc)) for (nr, nc) in sets)))
+            for nr, nc in sets:
+                union((r,c), (nr, nc))
+                
         return res
-                
-                        
-        
-                
-                
-                
                     
