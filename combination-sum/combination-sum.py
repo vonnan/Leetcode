705b4a1a03a0bf688954a40a9db03113c2ea8c68
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-17 01:22:02 -0500

    Time: 96 ms (49.93%), Space: 14.5 MB (23.66%) - LeetHub

--- combination-sum/combination-sum.py
+++ combination-sum/combination-sum.py
@@ -1,19 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        n, res = len(candidates), set([])
         candidates.sort()
+        n, res = len(candidates), set([])
         
         def dfs(remaining, pos, path):
             if remaining == 0:
                 res.add(tuple(path))
                 
             else:
-                for i in range(pos, n):
-                    if candidates[i] > remaining:
+                for i, num in enumerate(candidates[pos:], pos):
+                    if num <= remaining:
+                        dfs(remaining - num, i, path + [num])
+                    else:
                         break
-                    dfs(remaining - candidates[i], i, path + [candidates[i]])
-        
         dfs(target, 0, [])
         return res
-            
-            
