8773005df981b6fe02561d301b4b01faf3d0ded5
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-17 00:14:46 -0500

    Time: 179 ms (14.33%), Space: 16.9 MB (29.67%) - LeetHub

--- number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,24 +1,15 @@
-from collections import Counter
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        UF = [i for i in range(n)]
+        UF = {i:i for i in range(n)}
         
         def find(x):
             if UF[x] != x:
                 UF[x] = find(UF[x])
             return UF[x]
         
-        def union(x,y):
-            UF[find(x)] = find(y)
+        for a, b in edges:
+            UF[find(a)] = find(b)
             
-        for a,b in edges:
-            union(a, b)
-        
-        for i in range(n):
-            find(i)
-        
-        return len(set(UF))
-        
-            
-                        
+        return len(set(find(i) for i in range(n)))
+                
         
