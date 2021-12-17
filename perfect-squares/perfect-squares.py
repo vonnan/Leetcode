221618dd3e732f706f9ffb2323d823acdd34b2c4
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-17 01:39:14 -0500

    Time: 254 ms (80.81%), Space: 14.8 MB (36.70%) - LeetHub

--- perfect-squares/perfect-squares.py
+++ perfect-squares/perfect-squares.py
@@ -2,24 +2,26 @@ class Solution:
     def numSquares(self, n: int) -> int:
         q = deque([0])
         visited = set([])
-        sqrt_n = int(n ** 0.5)
+        
+        sqrt = int(n ** 0.5) + 1
         step = 0
         
         while q:
             m = len(q)
             step += 1
             for _ in range(m):
-                num = q.popleft()
-                for val in range(1, sqrt_n + 1):
-                    new = num + val * val
+                x = q.popleft()
+                for i in range(1, sqrt):
+                    new = i * i + x
+                    if new in visited:
+                        continue
                     if new > n:
                         break
+                    if new < n:
+                        q.append(new)
+                        visited.add(new)
                     elif new == n:
                         return step
-                    else:
-                        if new not in visited:
-                            visited.add(new)
-                            q.append(new)
-        
             
-            
+                    
+                
