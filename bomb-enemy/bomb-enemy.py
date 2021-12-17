547923ebfe1bfc2686eb10ad336a3f139cc2a9a4
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-17 18:58:07 -0500

    Time: 1764 ms (7.65%), Space: 164.9 MB (5.20%) - LeetHub

--- bomb-enemy/bomb-enemy.py
+++ bomb-enemy/bomb-enemy.py
@@ -1,44 +1,16 @@
 class Solution:
     def maxKilledEnemies(self, grid: List[List[str]]) -> int:
-        row, col = len(grid), len(grid[0])
-        res = 0
-        for r in range(row):
-            rows = grid[r]
-            for c in range(col):
-                if grid[r][c] != "0":
-                    continue
-                
-                ct = 0
-                
-                #count left
-                
-                left = c-1
-                
-                while left >= 0 and rows[left] != "W":
-                    
-                    if rows[left] == "E":
-                        ct += 1
-                        
-                    left -= 1
-                
-                right = c + 1
-                while right < col and rows[right] != "W":
-                    if rows[right] == "E":
-                        ct += 1
-                    right += 1
-                    
-                up = r -1
-                while up >= 0 and grid[up][c] != "W":
-                    if grid[up][c] == "E":
-                        ct += 1
-                    up -= 1
-                
-                down = r + 1
-                while down < row and grid[down][c] != "W":
-                    if grid[down][c] == "E":
-                        ct += 1
-                    down += 1
-                   
-                res = max(res, ct)
-                
-        return res
+        m, n = len(grid), len(grid[0]) if grid else 0
+        up, dw, lt, rt = tuple(collections.defaultdict(int) for _ in range(4))
+        zeroes = set()
+        for r in range(m):
+            for c in range(n):
+                x, y = m - r - 1, n - c - 1
+                E, W = (grid[r][c] == 'E'), (grid[r][c] != 'W')
+                G, K = (grid[x][y] == 'E'), (grid[x][y] != 'W')
+                up[r,c] = (up[r-1,c] + E) * W
+                lt[r,c] = (lt[r,c-1] + E) * W
+                dw[x,y] = (dw[x+1,y] + G) * K
+                rt[x,y] = (rt[x,y+1] + G) * K
+                if grid[r][c] == "0": zeroes.add((r, c))
+        return max([up[r,c] + dw[r,c] + lt[r,c] + rt[r,c] for r, c in zeroes] or [0])
