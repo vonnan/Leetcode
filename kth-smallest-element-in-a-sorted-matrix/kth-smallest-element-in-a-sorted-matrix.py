5b093a222323b31d6d20de175dbf68f631238d2b
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-19 14:17:05 -0500

    Time: 152 ms (98.72%), Space: 18.9 MB (29.01%) - LeetHub

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,12 +1,14 @@
 from bisect import bisect
 class Solution:
-    def kthSmallest(self, A: List[List[int]], k: int) -> int:
-        lo, hi = A[0][0], A[-1][-1]
-        while lo < hi:
-            mid = (lo + hi)//2
-            if sum(bisect(row, mid) for row in A) >= k:
-                hi = mid
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        l,r = matrix[0][0], matrix[-1][-1]
+        
+        while l < r:
+            mid = (l + r)//2
+            tot = sum(bisect(row, mid) for row in matrix)
+            if tot < k:
+                l = mid + 1
             else:
-                lo = mid + 1
-        return lo
-            
+                r = mid
+        return l
+        
