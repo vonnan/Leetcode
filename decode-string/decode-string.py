36df7e39ae32ac024cb27582a5995bf85b567d6d
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-19 18:05:42 -0500

    Time: 28 ms (83.65%), Space: 14.4 MB (19.74%) - LeetHub

--- decode-string/decode-string.py
+++ decode-string/decode-string.py
@@ -4,12 +4,16 @@ class Solution:
         for c in s:
             if c.isdigit():
                 num += c
+                
             elif c.isalpha():
                 strs += c
+                
             elif c == "[":
                 stack.append((num, strs))
                 num, strs = "", ""
+                
             else:
                 pre_num, pre_str = stack.pop()
                 strs = pre_str + int(pre_num) * strs
         return strs
+                
