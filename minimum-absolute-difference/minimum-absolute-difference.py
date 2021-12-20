918911b9a15feecf1d41452e3e4aa0378b9f3055
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-20 02:21:03 -0500

    Time: 340 ms (69.62%), Space: 28.3 MB (18.02%) - LeetHub

---
+++ minimum-absolute-difference/minimum-absolute-difference.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        
+        min_ = min( [b-a for a, b in zip(arr, arr[1:])])
+        
+        return [(a,b) for a, b in zip(arr, arr[1:]) if b-a == min_]
