719e1f6b6d62b032c968774f1ee6a576d0aad69a
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-23 01:24:20 -0500

    Time: 112 ms (83.86%), Space: 18.9 MB (85.16%) - LeetHub

--- the-skyline-problem/the-skyline-problem.py
+++ the-skyline-problem/the-skyline-problem.py
@@ -5,7 +5,7 @@ class Solution:
     def getSkyline(self, A: List[List[int]]) -> List[List[int]]:
         lst = sorted([(l, -h, r) for l, r, h in A] + list(set((r, 0, 0) for _, r, _ in A)))
         res, heap =[(0,0)], [(0, inf)]
-        
+        print(lst)
         for l, nH, r in lst:
             while l >= heap[0][1]:
                 heappop(heap)
