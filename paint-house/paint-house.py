28b8d5f35771c45b0ba565c2560f2534d652ec31
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-24 17:08:29 -0500

    Time: 60 ms (65.80%), Space: 14.5 MB (31.28%) - LeetHub

--- paint-house/paint-house.py
+++ paint-house/paint-house.py
@@ -1,9 +1,10 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
         row, col = len(costs), len(costs[0])
-        dp = [[0] * col for _ in range(row)]
-        dp[0] = costs[0]
-        for r , rows in enumerate(costs[1:], 1):
-            for c in range(col):
-                dp[r][c] = min(dp[r-1][:c] + dp[r-1][c+1:]) + costs[r][c]
-        return min(dp[-1])
+        
+        dp = costs[0]
+        for i, cost in enumerate(costs[1:], 1):
+            dp = [cost[c] + min(dp[:c] + dp[c+1:]) for c in range(3)]
+            
+        
+        return min(dp)     
