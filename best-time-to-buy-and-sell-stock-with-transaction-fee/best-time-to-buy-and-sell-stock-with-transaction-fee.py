a5817c21bc4ddf0f6a0b8756cfd7dca7a1fc5a04
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-28 02:12:42 -0500

    Time: 804 ms (41.36%), Space: 21.4 MB (59.13%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,13 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
-        rest, hold, sell = [0] * n, [0] * n, [0] * n
-        
+        sell, hold, rest = [0] * n, [0] * n, [0] * n
         hold[0] = -prices[0]
-        
         for i, price in enumerate(prices[1:], 1):
-            sell[i] = hold[i-1] + price - fee
+            sell[i] = hold[i-1] + price -fee
             rest[i] = max(rest[i-1], sell[i])
             hold[i] = max(hold[i-1], rest[i] - price)
+        return max(sell[-1], rest[-1])
         
-        return max(rest[-1], sell[-1])
