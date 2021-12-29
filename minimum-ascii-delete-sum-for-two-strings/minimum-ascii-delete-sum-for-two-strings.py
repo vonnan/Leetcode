4b8511fb3cb74b0d98650d44d84d91524e2af435
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-29 02:14:38 -0500

    Time: 536 ms (92.61%), Space: 15.2 MB (81.64%) - LeetHub

--- minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -2,14 +2,13 @@ class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         row, col = len(s1), len(s2)
         
-        dp = [[0] * (col + 1) for _ in range(row + 1)]
+        dp = [[0]* (col + 1) for _ in range(row + 1)]
         
         for r in range(row):
             for c in range(col):
                 if s1[r] == s2[c]:
                     dp[r+1][c+1] = dp[r][c] + ord(s1[r])
                 else:
-                    dp[r+1][c+1] = max(dp[r+1][c], dp[r][c+1])
-                    
-        return sum(map(ord, s1+s2)) - dp[-1][-1] * 2
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+                    dp[r+1][c+1] = max(dp[r][c+1] , dp[r+1][c])
+        
+        return sum(map(ord,s1+ s2)) - dp[-1][-1] * 2
