930e37b89965ae8201c5a51ffb0f11db9e72e202
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-28 19:03:22 -0500

    Time: 76 ms (28.92%), Space: 15.3 MB (10.51%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.py
+++ maximum-product-subarray/maximum-product-subarray.py
@@ -1,8 +1,9 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         back = nums[::-1]
+        
         for i in range(1, len(nums)):
             nums[i] *= nums[i-1] or 1
-            back[i] *= back[i-1]  or 1
-        #print(nums, back)
+            back[i] *= back[i-1] or 1
+            
         return max(max(nums), max(back))
