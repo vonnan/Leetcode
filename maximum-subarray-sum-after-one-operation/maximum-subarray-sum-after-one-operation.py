ceba2cb952b06b9be02de9d1deda320ccdd8f988
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-29 01:39:20 -0500

    Time: 2154 ms (5.11%), Space: 35.9 MB (10.58%) - LeetHub

--- maximum-subarray-sum-after-one-operation/maximum-subarray-sum-after-one-operation.py
+++ maximum-subarray-sum-after-one-operation/maximum-subarray-sum-after-one-operation.py
@@ -1,14 +1,12 @@
 class Solution:
     def maxSumAfterOperation(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[0]* 2 for _ in range(n)]
-        dp[0] = [ nums[0], nums[0] ** 2]
-        res = dp[0][1]
-        
-        for i, num in enumerate(nums[1:] ,1):
-            dp[i][0] = max(dp[i-1][0] + num, num)
-            dp[i][1] = max(dp[i-1][0] + num * num, dp[i-1][1] + num, num * num)
-        
-            res = max(res, dp[i][1])
-            
+        dp = [[0] * 2 for _ in range(n)]
+        dp[0] = [nums[0], nums[0] ** 2]
+        res = max(dp[0])
+        for i, num in enumerate(nums[1:], 1):
+            dp[i][0] = max(dp[i-1][0], 0) + num
+            dp[i][1] = max(dp[i-1][0] + num * num, num * num, dp[i-1][1] + num)
+            res = max(res, max(dp[i]))
         return res
+            
