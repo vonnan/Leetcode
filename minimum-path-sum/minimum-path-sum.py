c2135aa2c0c15516fada41c7e459aaffb5541d28
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-30 15:48:13 -0500

    Time: 96 ms (78.93%), Space: 15.7 MB (62.46%) - LeetHub

--- minimum-path-sum/minimum-path-sum.py
+++ minimum-path-sum/minimum-path-sum.py
@@ -6,7 +6,7 @@ class Solution:
         
         for r in range(row):
             for c in range(col):
-                if r ==0 and c == 0:
+                if r == 0 and c == 0:
                     dp[r][c] = grid[r][c]
                 elif r == 0:
                     dp[r][c] = dp[r][c-1] + grid[r][c]
@@ -14,5 +14,5 @@ class Solution:
                     dp[r][c] = dp[r-1][c] + grid[r][c]
                 else:
                     dp[r][c] = min(dp[r-1][c], dp[r][c-1]) + grid[r][c]
-                    
+        
         return dp[-1][-1]
