06701238c8dc89456b7447965de2caa3e4d5de52
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-31 00:21:00 -0500

    Time: 499 ms (58.30%), Space: 17.9 MB (39.19%) - LeetHub

--- longest-line-of-consecutive-one-in-matrix/longest-line-of-consecutive-one-in-matrix.py
+++ longest-line-of-consecutive-one-in-matrix/longest-line-of-consecutive-one-in-matrix.py
@@ -2,32 +2,33 @@ class Solution:
     def longestLine(self, mat: List[List[int]]) -> int:
         row, col = len(mat), len(mat[0])
         
-        dp = [[[0,0,0,0]] * (col+1) for _ in range(row)]
+        dp= [[[0, 0, 0, 0]] *(col+ 1) for _ in range(row)]
+        
         res = 0
         for r in range(row):
             for c in range(col):
                 if mat[r][c]:
-                   
+                    dp[r][c] = [1] * 4
+                    if r == 0 and c == 0:
+                        res = max(res, 1)
                     
-                    res = max(res, 1)
-                    if r ==0 and c ==0:
-                        dp[r][c] = [1] * 4
-                        continue
-                        
                     elif r == 0:
-                        dp[r][c] = [1] * 4
                         dp[r][c][0] += dp[r][c-1][0]
                     
                     elif c == 0:
-                        dp[r][c] = [1] * 4
                         dp[r][c][1] += dp[r-1][c][1]
-                        dp[r][c][3] += dp[r-1][c+1][3]
-                    
-                   
+                        dp[r][c][2] += dp[r-1][c+1][2]
+                        
+                        
                     else:
-                        dp[r][c] = [ dp[r][c-1][0] + 1, dp[r-1][c][1] + 1,dp[r-1][c-1][2] + 1, dp[r-1][c+1][3] + 1]
-                    
+                        dp[r][c][0] += dp[r][c-1][0]
+                        dp[r][c][1] += dp[r-1][c][1]
+                        dp[r][c][2] += dp[r-1][c+1][2]
+                        dp[r][c][3] += dp[r-1][c-1][3]
                     res = max(res, max(dp[r][c]))
-        #print(dp)
+        
         return res
+                        
+                        
+                        
                         
