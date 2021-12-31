bae3bdbf947247ffa58552b8e1908e345fd11fdb
Nan Feng <56846412+vonnan@users.noreply.github.com>
2021-12-31 00:46:54 -0500

    Time: 2484 ms (15.90%), Space: 37.3 MB (57.36%) - LeetHub

--- shortest-distance-to-target-color/shortest-distance-to-target-color.py
+++ shortest-distance-to-target-color/shortest-distance-to-target-color.py
@@ -2,8 +2,10 @@ from bisect import bisect_left
 class Solution:
     def shortestDistanceColor(self, colors: List[int], queries: List[List[int]]) -> List[int]:
         dp = [[], [], []]
+        
         for i, c in enumerate(colors):
             dp[c-1].append(i)
+        
         res = []
         for i, c in queries:
             lst = dp[c-1]
@@ -11,12 +13,11 @@ class Solution:
                 res.append(-1)
                 continue
             idx = bisect_left(lst, i)
-            
             if idx == 0:
-                res.append(lst[0] - i)
+                res.append(lst[idx] - i)
             elif idx == len(lst):
-                res.append(i- lst[-1])
+                res.append(i - lst[-1])
             else:
-                res.append(min(lst[idx] -i, i - lst[idx-1]))
-        return res
+                res.append(min(lst[idx] - i, i - lst[idx-1]))
         
+        return res
