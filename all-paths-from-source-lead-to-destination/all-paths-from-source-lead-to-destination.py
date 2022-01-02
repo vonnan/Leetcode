86018b0a63cc0ca35b78ed5663fc19ed9d487fa7
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-01 19:27:04 -0500

    Time: 280 ms (88.82%), Space: 20.9 MB (17.28%) - LeetHub

--- all-paths-from-source-lead-to-destination/all-paths-from-source-lead-to-destination.py
+++ all-paths-from-source-lead-to-destination/all-paths-from-source-lead-to-destination.py
@@ -1,18 +1,18 @@
 class Solution:
     def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = defaultdict(set)
+        
+        for u, v in edges:
+            graph[u].add(v)
+        
+        seen = set([])
         def dfs(node):
             seen.add(node)
             for nei in graph[node]:
                 if nei == node or nei in seen or not dfs(nei):
                     return False
-            seen.discard(node)
+            seen.discard(node) 
+            #print(graph[node], node==destination)
             return graph[node] or node == destination
         
-        seen = set()
-        graph = defaultdict(list)
-        
-        for u, v in edges:
-            graph[u].append(v)
-        
         return dfs(source)
-            
