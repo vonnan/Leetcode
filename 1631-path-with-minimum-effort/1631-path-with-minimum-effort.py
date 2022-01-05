50ec7fa927f606df6625bb545132370b9fb6b6b5
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-05 00:32:53 -0500

    Time: 1190 ms (36.15%), Space: 16.5 MB (40.79%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,27 +1,26 @@
-from heapq import heappush
-from heapq import heappop
 class Solution:
-    def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        path = ((-1,0), (1,0), (0,-1), (0, 1))
-        row, col = len(heights), len(heights[0])
-        heap =[(0,0,0)]
-        visited = set()
+    def minimumEffortPath(self, A: List[List[int]]) -> int:
+        row, col = len(A), len(A[0])
+        path = [(0,1), (0, -1), (1, 0), (-1, 0)]
+        
+        seen = set([])
+        
+        heap = [(0, 0, 0)]
         
         while heap:
-            effort, r, c = heappop(heap)
-            if (r,c) in visited:
+            val, r,c = heappop(heap)
+            #print(val, r, c)
+            if (r,c) in seen:
                 continue
-            if r== row-1 and c == col -1:
-                return effort
-            visited.add((r,c))
-            
-            for dr,dc in path:
-                nr, nc = r+dr, c +dc
-                if (nr, nc) in visited:
-                    continue
-                if 0<=nr<row and 0<=nc<col:
-                    eft = max(effort, abs(heights[nr][nc] - heights[r][c]))
-                    
-                    heappush(heap, (eft, nr, nc))
-                        
+                
+            if r == row-1 and c == col - 1:
+                return val
+            seen.add((r, c))
+            a = A[r][c]
+            for dr, dc in path:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < row and 0 <= nc < col and (nr, nc) not in seen:
+                    tmp = max(val, abs(A[nr][nc] - a))
+                    heappush(heap, (tmp, nr, nc))
+        
         
