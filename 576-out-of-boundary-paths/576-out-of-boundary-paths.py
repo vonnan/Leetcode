5356c1d225b9754fdab735a51e6e693de8ece194
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-06 17:44:59 -0500

    Time: 156 ms (60.12%), Space: 14.6 MB (80.64%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        
+        dp =[[0] * n for _ in range(m)]
+        mod = 10 ** 9 + 7
+        
+        for _ in range(maxMove):
+            dp = [[(r==0 or dp[r-1][c]) +(c==0 or dp[r][c-1]) + (r == m -1 or dp[r+1][c]) + (c== n-1 or dp[r][c+1]) for c in range(n)] for r in range(m) ]
+        
+        return dp[startRow][startColumn] % mod
