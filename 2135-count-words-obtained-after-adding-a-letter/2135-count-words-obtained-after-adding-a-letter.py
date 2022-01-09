b3ec66a398f4c3854d6f3b1cedeb9222663c5633
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-09 16:45:31 -0500

    Time: 2022 ms (25.00%), Space: 34.4 MB (25.00%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
@@ -1,23 +1,20 @@
 class Solution:
     def wordCount(self, s: List[str], t: List[str]) -> int:
-        seen = set([])
+        start = set([])
         a = ord("a")
         for word in s:
-            m = 0
+            m= 0
             for ch in word:
-                m ^= 1 << (ord(ch) -a)
-            seen.add(m)
+                m ^= 1 << (ord(ch) - a)
+            start.add(m)
             
         res = 0
-        
         for word in t:
             m = 0
             for ch in word:
                 m ^= 1 << (ord(ch) - a)
-            
             for ch in word:
-                if m ^ 1<<(ord(ch) - a) in seen:
+                if m ^ (1 << (ord(ch) - a)) in start:
                     res += 1
                     break
-        
         return res
