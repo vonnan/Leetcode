5dbec52ab1883de8edeab8c34b13e8ddfcadcf45
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-09 17:02:33 -0500

    Time: 312 ms (48.11%), Space: 48.1 MB (9.34%) - LeetHub

---
+++ 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.py
@@ -0,0 +1,11 @@
+class Solution:
+    def differByOne(self, dict: List[str]) -> bool:
+        sets = set()
+        for word in dict:
+            for i, ch in enumerate(word):
+                x = (i, word[:i] + word[i+1:])
+                if x in sets:
+                    return True
+                sets.add(x)
+        return False
+                
