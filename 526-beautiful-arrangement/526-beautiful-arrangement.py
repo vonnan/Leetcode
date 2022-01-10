394dcd8ad255108f2f81153265c0d426efe36c13
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-10 17:03:40 -0500

    Time: 1044 ms (66.99%), Space: 14.2 MB (70.28%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.py
+++ 526-beautiful-arrangement/526-beautiful-arrangement.py
@@ -3,13 +3,13 @@ class Solution:
         nums = list(range(1, n + 1))
         self.res = 0
         
-        def helper(nums, tmp):
+        def helper(nums, pos):
             if not nums:
                 self.res += 1
             else:
                 for i, num in enumerate(nums):
-                    if num % tmp == 0 or (tmp % num == 0):
-                        helper(nums[:i] + nums[i+1:], tmp + 1)
+                    if num % pos == 0 or (pos % num == 0):
+                        helper(nums[:i] + nums[i+1:], pos + 1)
         
         helper(nums, 1)
         return self.res
