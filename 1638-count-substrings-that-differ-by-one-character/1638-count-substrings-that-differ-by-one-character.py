6b9b66a541bc7e3f500471113d49f3feaf72b7d6
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-09 23:21:21 -0500

    Time: 44 ms (97.61%), Space: 14.4 MB (39.71%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.py
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.py
@@ -2,15 +2,13 @@ class Solution:
     def countSubstrings(self, s: str, t: str) -> int:
         row, col = len(s), len(t)
         
-        def helper(r,c):
-            prev, curr, res = 0, 0, 0
-            
-            for d in range(min(row-r, col - c)):
+        def helper(r, c):
+            prev = curr = res = 0
+            for k in range(min(row - r, col - c)):
                 curr += 1
-                if s[r+d] != t[c+d]:
+                if s[r+k] != t[c+k]:
                     prev, curr = curr, 0
                 res += prev
             return res
         
-        return sum(helper(r, 0) for r in range(row)) + sum(helper(0, c) for c in range(1, col))
-                    
+        return sum(helper(r,0) for r in range(row)) + sum(helper(0,c) for c in range(1, col))
