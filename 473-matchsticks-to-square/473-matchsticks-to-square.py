e629c2f7c8a2c1cc03dbd279190cad0bbb7af360
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-12 17:12:41 -0500

    Time: 212 ms (81.11%), Space: 14.3 MB (76.70%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def makesquare(self, nums):
+        if not nums:
+            return False
+        
+        tot = sum(nums)
+        if tot % 4:
+            return False
+        
+        nums.sort(reverse = 1)
+        
+        target, n = tot//4, len(nums)
+        
+        def helper(mask, sides, t):
+            if sides == 0 and mask == 0:
+                return True
+            
+            for i in range(n):
+                if mask & (1 << i) != 0:
+                    if nums[i] > t:
+                        break
+                    elif nums[i] == t:
+                        if helper(mask ^ (1 <<i), sides - 1, target):
+                            return True
+                    else:
+                        if helper(mask ^ (1 <<i), sides, t - nums[i]):
+                            return True
+            return False
+        
+        return helper((1<<n)-1, 4, target)
+                        
+    
