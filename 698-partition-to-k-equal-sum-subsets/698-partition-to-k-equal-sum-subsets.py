6d4ef03b1c4a880242d1f9f9403af053549cdaf0
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-12 23:51:30 -0500

    Time: 3562 ms (5.00%), Space: 60.6 MB (5.21%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -8,25 +8,16 @@ class Solution:
         
         target = tot//k
         
-        def helper(mask, sides, t):
-            if sides == 0 and mask == 0:
-                return True
+        @lru_cache(None)
+        def dfs(mask):
+            if mask == 0:
+                return 0
             
             for i in range(n):
-                if mask & (1 << i):
-                 
-                    if nums[i] > t:
-                        #print(i, t, nums[i])
-                        break
-                    
-                    elif nums[i] == t:
-                        #print(i, t, nums[i], mask ^ (1 <<i), sides - 1, target)
-                        if helper(mask ^ (1 <<i), sides - 1, target):
-                            return True
-                    elif nums[i] < t:
-                        #print(i, t, nums[i], mask ^ (1 <<i), sides - 1, target)
-                        if helper(mask ^ (1 <<i), sides, t - nums[i]):
-                            return True
-            return False
+                if mask & (1<< i):
+                    nxt = dfs(mask ^ (1 << i))
+                    if nxt >= 0 and (nxt + nums[i] <= target):
+                        return (nxt + nums[i]) % target
+            return -1
         
-        return helper((1<<n)-1, k, target)
+        return dfs((1<<n)-1) == 0
