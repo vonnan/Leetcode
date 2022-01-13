ac50fcd8af8f5b3cbe1acd4df6e9847ab70061eb
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-12 22:53:26 -0500

    Time: 2775 ms (9.58%), Space: 14.4 MB (55.54%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.py
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -1,37 +1,26 @@
-class Solution(object):
-    def makesquare(self, nums):
-        if not nums:
-            return False
-        
-        tot = sum(nums)
+class Solution:
+    def makesquare(self, nums: List[int]) -> bool:
+        n, tot = len(nums), sum(nums)
         nums.sort(reverse = 1)
         
-        target, n = tot//4, len(nums)
+        target = tot//4
         
-        if tot % 4 or nums[0] > target:
+        if tot % 4 or nums[0] > target or n < 4:
             return False
-         
-        def helper(mask, sides, t):
-            if sides == 0 and mask == 0:
+        
+        t = [target] * 4
+        
+        def dfs(pos):
+            if pos == n:
                 return True
             
-            for i in range(n):
-                if mask & (1 << i):
-                 
-                    if nums[i] > t:
-                        #print(i, t, nums[i])
-                        break
-                    
-                    elif nums[i] == t:
-                        #print(i, t, nums[i], mask ^ (1 <<i), sides - 1, target)
-                        if helper(mask ^ (1 <<i), sides - 1, target):
-                            return True
-                    elif nums[i] < t:
-                        #print(i, t, nums[i], mask ^ (1 <<i), sides - 1, target)
-                        if helper(mask ^ (1 <<i), sides, t - nums[i]):
-                            return True
+            for i in range(4):
+                if t[i] >= nums[pos]:
+                    t[i] -= nums[pos] 
+                    if dfs(pos + 1):
+                        return True
+                    t[i] += nums[pos]
             return False
         
-        return helper((1<<n)-1, 4, target)
-                        
-    
+        return dfs(0)
+                    
