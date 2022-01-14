abb49aa1beadbe19b48746f69102d794fc40ef7e
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-14 00:18:11 -0500

    Time: 202 ms (81.80%), Space: 18.4 MB (13.15%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.py
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -2,26 +2,26 @@ class Solution:
     def makesquare(self, nums: List[int]) -> bool:
         n, tot = len(nums), sum(nums)
         nums.sort(reverse = 1)
-        
         target = tot//4
-        
-        if tot % 4 or nums[0] > target or n < 4:
+        if tot%4 or nums[0] > target:
             return False
         
-        t = [target] * 4
-        
-        #@lru_cache(None)
-        def dfs(pos):
-            if pos == n:
+        @lru_cache(None)
+        def dfs(mask, sides, t):
+            if mask == 0 and sides ==0:
                 return True
             
-            for i in range(4):
-                if t[i] >= nums[pos]:
-                    t[i] -= nums[pos] 
-                    if dfs(pos + 1):
+            for i in range(n):
+                if mask & (1 << i):
+                    if t < nums[i]:
+                        break
+                    mask_ = mask ^(1 <<i)
+                    if t == nums[i]:
+                        if dfs(mask_, sides -1, target):
+                            return True
+                    elif dfs(mask_, sides, t - nums[i]):
                         return True
-                    t[i] += nums[pos]
+            
             return False
         
-        return dfs(0)
-                    
+        return dfs((1<<n)-1, 4, target)
