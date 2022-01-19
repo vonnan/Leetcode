de1dbc9d75dde019fe82aab1e5a4584311884796
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-19 17:12:41 -0500

    Time: 1865 ms (84.50%), Space: 21 MB (66.50%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -4,21 +4,21 @@ from itertools import combinations
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
         n = len(nums)
+        
         gcds = {(i, j): gcd(nums[i], nums[j]) for (i,j) in combinations(range(n), 2)}
         
         @lru_cache(None)
-        def dfs(mask):
-            if mask == 0 :
+        def dp(mask):
+            if mask == 0:
                 return 0
             
-            no_group = [i for i in range(n) if mask & (1 << i)]
             res = 0
             
-            for i, j in combinations(no_group, 2):
-                mask_nxt = mask ^ (1 <<i) ^ (1 <<j)
-                res = max(res, dfs(mask_nxt) + (n - len(no_group) + 2)//2 * gcds[i, j])
+            not_assigned =[i for i in range(n) if mask &( 1<<i)]
             
+            for j, k in combinations(not_assigned, 2):
+                mask_nxt = mask ^ (1<<j) ^( 1 <<k)
+                res = max(res, dp(mask_nxt) + (n - len(not_assigned) + 2)//2 * gcds[j,k])
             return res
         
-        return dfs((1 << n) - 1)
-                
+        return dp((1<<n) - 1)
