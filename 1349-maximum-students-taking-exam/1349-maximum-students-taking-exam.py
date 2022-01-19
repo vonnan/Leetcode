8ecd66ddb14326d1b146dfcb862ac82799da8214
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-19 14:55:11 -0500

    Time: 68 ms (80.17%), Space: 14.7 MB (20.66%) - LeetHub

--- 1349-maximum-students-taking-exam/1349-maximum-students-taking-exam.py
+++ 1349-maximum-students-taking-exam/1349-maximum-students-taking-exam.py
@@ -1,22 +1,24 @@
 class Solution:
     def maxStudents(self, seats: List[List[str]]) -> int:
         rows, cols = len(seats), len(seats[0])
+        
         masks = []
         
         for row in seats:
-            mask = sum((1<<c) * (val == ".") for c, val in enumerate(row) )
-            masks.append([c for c in range(1<<cols) if ((c & mask) == c) and c & (c >> 1) == 0 ])
-        
+            mask = sum(1<< j for j in range(cols) if row[j] == ".")
+            masks.append([c for c in range(1 << cols) if c & mask == c and c & (c >>1) ==0])
+            
         @lru_cache(None)
-        def dfs(curr, r):
+        def dp(mask, r):
             if r == -1:
                 return 0
             
             res = 0
             for prev in masks[r-1]:
-                if (curr >>1 ) & prev == 0 and (curr & (prev >> 1) == 0):
-                    res = max(res, dfs(prev, r -1))
-            
-            return res + bin(curr).count("1")
+                if mask & (prev >>1) ==0 and prev & (mask >>1) == 0:
+                    res = max(res, dp(prev, r -1))
+            return res + bin(mask).count("1")
         
-        return max(dfs(mask, rows-1) for mask in masks[-1])
+        return max(dp(mask, rows - 1) for mask in masks[-1])
+            
+            
