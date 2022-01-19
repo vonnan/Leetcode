162577cab9f00485cd7864cb823f9ba393abdbe0
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-19 15:51:21 -0500

    Time: 339 ms (70.21%), Space: 22.2 MB (64.89%) - LeetHub

---
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
@@ -0,0 +1,23 @@
+class Solution:
+    def smallestSufficientTeam(self, req_skills: List[str], people: List[List[str]]) -> List[int]:
+        dic = {val:i for i, val in enumerate(req_skills)}
+        n = len(dic)
+        
+        masks = []
+        dp = defaultdict(list)
+        dp[0] = []
+        for i, p in enumerate(people):
+            p_skill = 0
+            for val in p:
+                p_skill |= 1 <<dic[val]
+            for skillset, need in list(dp.items()):
+                with_p = p_skill | skillset
+                if skillset == with_p:
+                    continue
+                if with_p not in dp or len(dp[with_p]) > len(need) + 1:
+                    dp[with_p] = need + [i]
+        return dp[(1<<n) -1]
+            
+        
+                
+            
