0b74992b62ddc03ecfa27701f356eacbebdeebcc
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-20 16:28:07 -0500

    Time: 1594 ms (30.25%), Space: 72.9 MB (20.37%) - LeetHub

--- 943-find-the-shortest-superstring/943-find-the-shortest-superstring.py
+++ 943-find-the-shortest-superstring/943-find-the-shortest-superstring.py
@@ -1,19 +1,19 @@
 class Solution:
     def shortestSuperstring(self, words: List[str]) -> str:
         @lru_cache(None)
-        def suffix(w1, w2):
+        def suff(w1,w2):
             m = min(len(w1), len(w2))
             for i in range(m, -1, -1):
-                if w1[-i:] == w2[:i]:
+                if w2[:i] == w1[-i:]:
                     return w2[i:]
             return w2
         
         n = len(words)
         @lru_cache(None)
         def dp(mask, i):
-            if mask == (1 <<n) -1:
+            if mask == (1<<n) - 1:
                 return ""
             
-            return min([suffix(words[i], words[j] + dp(mask | 1 <<j, j)) for j in range(n) if not mask & (1<<j)], key = len)
+            return min([suff(words[i], words[j]) + dp(mask | 1<<j,  j) for j in range(n) if not mask & (1 <<j)], key = len)
         
         return min([words[i] + dp(1<<i, i) for i in range(n)], key = len)
