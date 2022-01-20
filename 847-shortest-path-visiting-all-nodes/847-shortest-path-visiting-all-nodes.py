6ed4472cf02c558b2e5493d74cba267582b5ea41
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-20 01:13:53 -0500

    Time: 399 ms (40.14%), Space: 19.3 MB (38.32%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.py
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.py
@@ -1,23 +1,21 @@
 from heapq import heappush
 from heapq import heappop
+
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
         n = len(graph)
-        final = (1 <<n) -1
-        memo,heap= set(), [(0, node, 1 << node) for node in range(n)]
+        memo = set()
+        heap = [(0, node, 1 << node) for node in range(n)]
+        
+        final = (1<<n) - 1
         
         while heap:
             step, node, state = heappop(heap)
             if state == final:
-                return step
+                return step 
             for v in graph[node]:
-                nxt = (1<<v) | state
+                nxt = state | (1 << v)
                 if (nxt, v) not in memo:
-                    heappush(heap, (step + 1, v, nxt))
                     memo.add((nxt, v))
-        
-        
-                
-                
-            
+                    heappush(heap, (step + 1, v, nxt))
             
