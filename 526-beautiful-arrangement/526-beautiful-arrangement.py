58c348b63261c3b029c8dbcc48f768733ba9f2ae
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-20 00:35:12 -0500

    Time: 85 ms (92.88%), Space: 15.3 MB (17.00%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.py
+++ 526-beautiful-arrangement/526-beautiful-arrangement.py
@@ -1,20 +1,16 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
-        
         @lru_cache(None)
-        def dfs(mask, pos):
+        def dp(mask, pos):
             if pos == 0:
                 return 1
             
             res = 0
-            
             for i in range(n):
-                j = i + 1
-                if  (mask & 1<<i) and (pos % j == 0 or (j % pos ==0)):
-                    #print(bin(mask)[2:], i, res)
-                    res += dfs(mask ^ ( 1 << i), pos - 1)
+                if mask & ( 1<< i) and (not pos % (i + 1) or not (i + 1) % pos):
+                    res += dp(mask ^ (1 <<i), pos -1)
             
             return res
         
-        return dfs((1 <<n) - 1, n)
- 
+        return dp((1<<n) -1, n)
+                    
