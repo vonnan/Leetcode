33aae6bda8cbce1962c1fe6afbef1071de84d7ac
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-21 15:13:27 -0500

    Time: 51 ms (57.61%), Space: 14.3 MB (63.59%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoEggDrop(self, n: int) -> int:
+        y = x = int((2 * n)**0.5)
+        if y * (y + 1) == 2 * n:
+            return y
+        
+        def dp(a):
+            m, b = n, a
+            ct = 0
+            while a > 1:
+                m -= a
+                a -= 1
+                ct += 1
+            return max(b, ct + m)
+        
+        return min(dp(y), dp(y + 1))
+                
+        return 
