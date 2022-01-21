f2c1c0d42d73da25ce77e899310d3f6e8e790881
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-21 00:21:17 -0500

    Time: 3802 ms (15.25%), Space: 14.7 MB (86.25%) - LeetHub

--- 1494-parallel-courses-ii/1494-parallel-courses-ii.py
+++ 1494-parallel-courses-ii/1494-parallel-courses-ii.py
@@ -1,36 +1,25 @@
 from itertools import combinations
 
 class Solution:
-    def minNumberOfSemesters(self, n: int, A: List[List[int]], k: int) -> int:
-        
-        prereq = [0] * n
-        for u,v in A:
-            prereq[v-1] |= 1 << (u - 1)
-            
-        dp = [n] * (1 << n)
+    def minNumberOfSemesters(self, n: int, relations: List[List[int]], k: int) -> int:
+        dp = [n] * (1<<n)
         dp[0] = 0
+        prereq = [0] * n
         
+        for u, v in relations:
+            prereq[v-1] |= 1 <<(u-1)
+            
         for mask in range(1 << n):
             available = []
+            for u in range(n):
+                if mask & ( 1<< u) == 0 and (prereq[u] & mask == prereq[u]):
+                    available.append(u)
             
-            for v in range(n):
-                if mask & (1 <<v) == 0 and (mask & prereq[v] == prereq[v]):
-                    available.append(v)
-                    
-            
-            
-            for choices in combinations(available, min(k, len(available))):
+            m = len(available)
+            for choice in combinations(available, min(k, m)):
                 mask2 = mask
-                for v in choices:
-                    mask2 |= 1<< v
-            
-                dp[mask2] = min(dp[mask2], 1+ dp[mask])
+                for v in choice:
+                    mask2 |= (1 <<v)
+                dp[mask2] = min(dp[mask2], 1 + dp[mask])
             
         return dp[-1]
-                
-                
-        
-        
-            
-        
-            
