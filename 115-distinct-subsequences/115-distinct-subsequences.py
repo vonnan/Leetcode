74939a0a586be86e964cf7e5d56cd8704c77ce70
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-01-26 16:24:45 -0500

    Time: 1641 ms (5.01%), Space: 185.1 MB (5.62%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        @lru_cache(None)
+        def dp(i, j):
+            if i == -1: return j == -1
+            if j == -1: return j == -1
+            return dp(i-1, j) + (s[i] == t[j]) * dp(i-1, j-1)
+            
+        return dp(len(s) - 1, len(t) - 1)
