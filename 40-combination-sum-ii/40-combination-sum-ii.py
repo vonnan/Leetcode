bf7c746e75ae407713bb21b0f284957cfb25b6b5
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-02-04 01:15:58 -0500

    Time: 103 ms (43.71%), Space: 14.1 MB (82.86%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -2,20 +2,19 @@ class Solution:
     def combinationSum2(self, A: List[int], target: int) -> List[List[int]]:
         n, res = len(A), set()
         A.sort()
-       
         def dfs(pos, path, target):
             if target == 0:
                 res.add(tuple(path))
             
-            if target < 0:
-                return
-            
             for i, num in enumerate(A[pos:], pos):
                 if num > target:
                     break
+                
                 if i > pos and A[i-1] == num:
                     continue
+                
                 dfs(i + 1, path + [num], target - num)
                 
         dfs(0, [], target)
+        
         return res
