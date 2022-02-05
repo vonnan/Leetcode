3fb13cc712f7729d187ae0c154f0ac76a3fac9d4
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-02-05 01:39:01 -0500

    Time: 106 ms (51.89%), Space: 15.3 MB (20.16%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -1,31 +1,30 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         n, tot = len(nums), sum(nums)
-        if tot %k:
+        
+        if tot % k:
             return False
         
-        nums.sort(reverse = 1)
-        
         target = tot//k
+        nums.sort(reverse = 1)
         
         @lru_cache(None)
         def helper(mask, side, t):
-            if mask == 0 and side == 0:
+            if side == 0 and mask == 0:
                 return True
             
             for i in range(n):
-                if mask & ( 1<< i):
-                    
-                    if nums[i] > t:
+
+                if mask & (1<<i):
+                    if nums[i] >t:
                         break
-                    mask_nxt = mask ^ (1 <<i)
-                    if nums[i] == t:
-                        if helper(mask_nxt, side -1, target):
+                    if t == nums[i]:
+                        if helper(mask ^(1<<i), side -1, target):
                             return True
                     else:
-                        if helper(mask_nxt, side, t - nums[i]):
+                        if helper(mask^(1<<i), side, t - nums[i]):
                             return True
+            
             return False
         
         return helper((1<<n)-1, k, target)
-                        
