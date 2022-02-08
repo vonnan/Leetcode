f5f3d47ba527280c4b46d07d323e15757397bb53
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-02-08 02:38:54 -0500

    Time: 1020 ms (59.24%), Space: 14.7 MB (60.59%) - LeetHub

---
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numSubmat(self, mat: List[List[int]]) -> int:
+        row, col = len(mat), len(mat[0])
+        for r in range(row):
+            for c in range(1, col):
+                if mat[r][c]:
+                    mat[r][c] = mat[r][c-1] + 1
+        
+        res = 0
+        
+        for r in range(row):
+            for c in range(col):
+                if mat[r][c]:
+                    prev = inf
+                    for t in range(r, -1, -1):
+                        if not mat[t][c]:
+                            break
+                        prev = min(prev, mat[t][c])
+                        res += prev
+        
+        return res
+                        
+                
