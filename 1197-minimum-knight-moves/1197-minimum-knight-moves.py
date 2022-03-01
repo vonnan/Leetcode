25acd6d73e46f3cb041a108679d729941a0cf703
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-02-28 23:30:34 -0500

    Time: 99 ms (80.30%), Space: 15.6 MB (83.83%) - LeetHub

---
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minKnightMoves(self, x: int, y: int) -> int:
+        @lru_cache(None)
+        def bfs(x,y):
+            if x + y == 0:
+                return 0
+            if x+y ==2:
+                return 2
+            return min(bfs(abs(x-1), abs(y-2)), bfs(abs(x-2), abs(y-1))) + 1
+        x, y = abs(x), abs(y)
+        return bfs(x,y)
+            
+            
