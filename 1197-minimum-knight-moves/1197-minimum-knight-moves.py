1c0dc902a714e6a6d60d84149c0357ea591e4fa0
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-02-28 23:30:56 -0500

    Time: 56 ms (94.46%), Space: 15.4 MB (88.69%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -4,7 +4,7 @@ class Solution:
         def bfs(x,y):
             if x + y == 0:
                 return 0
-            if x+y ==2:
+            if x + y ==2:
                 return 2
             return min(bfs(abs(x-1), abs(y-2)), bfs(abs(x-2), abs(y-1))) + 1
         x, y = abs(x), abs(y)
