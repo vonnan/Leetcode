481186f410f9003000cb7ccb7d3944f9fdfc722a
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-03-11 18:51:35 -0500

    Time: 174 ms (23.73%), Space: 15.5 MB (69.49%) - LeetHub

---
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sufficientSubset(self, root: Optional[TreeNode], limit: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if not root.left and not root.right:
+            return root if root.val >= limit else None
+        
+        root.left = self.sufficientSubset(root.left, limit - root.val)
+        root.right = self.sufficientSubset(root.right, limit - root.val)
+        
+        return root if root.left or root.right else None
