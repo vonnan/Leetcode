0993d46daf4046de591c16c9196725bef63787dc
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-03-21 13:02:25 -0400

    Time: 7478 ms (22.22%), Space: 415.5 MB (22.22%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
@@ -1,11 +1,12 @@
 class Solution:
     def minimumWhiteTiles(self, floor: str, c: int, L: int) -> int:
+        ct = floor.count("1")
         @lru_cache(None)
         def dp(i, k):
             if i <= 0:
                 return 0
             
-            return min(dp(i-1, k) + int(floor[i-1]), dp(i-L, k-1) if k else len(floor))
+            return min(dp(i-1, k) + int(floor[i-1]), dp(i-L, k-1) if k else ct)
         
         return dp(len(floor), c)
                 
