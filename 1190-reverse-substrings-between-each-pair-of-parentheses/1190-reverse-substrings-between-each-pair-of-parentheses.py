a300a1325a6a1ccddfef4bf896833ca085b625d3
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-03-22 00:28:08 -0400

    Time: 75 ms (5.06%), Space: 13.9 MB (33.99%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if c != ")":
+                stack.append(c)
+            else:
+                res = []
+                while stack[-1] != "(":
+                    res.append(stack.pop())
+                stack.pop()
+                stack.extend(res)
+        return "".join(stack)
