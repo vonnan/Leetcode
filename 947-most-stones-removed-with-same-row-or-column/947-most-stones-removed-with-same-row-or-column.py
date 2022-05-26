39c19baff5efa18faed74b4dcdf9942f263ebc34
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-05-26 03:43:51 -0400

    Time: 167 ms (83.39%), Space: 14.7 MB (59.82%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        UF = {}
+        def find(x):
+            if UF[x] != x:
+                UF[x] = find(UF[x])
+            return UF[x]
+        
+        def union(x,y):
+            UF.setdefault(x, x)
+            UF.setdefault(y, y)
+            UF[find(x)] = find(y)
+            
+        for r,c in stones:
+            union(r, ~c)
+            
+        return len(stones) - len(set(find(x) for x in UF))
