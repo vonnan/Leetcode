ba9f22b47044d9b84822477f5243cf7a1fe448f0
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-05-27 14:54:50 -0400

    Time: 425 ms (61.83%), Space: 32.1 MB (35.63%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        min_ = -inf
+        stack = []
+        
+        for num in nums[::-1]:
+            if num < min_:
+                return True
+            
+            while stack and num > stack[-1]:
+                min_ = stack.pop()
+            
+            stack.append(num)
+        return False
+ 
