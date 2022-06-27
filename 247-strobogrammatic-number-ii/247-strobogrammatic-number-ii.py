a114170a79afa0613bdf8af76d2b7857ab0327d3
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-06-27 14:59:20 -0400

    Time: 395 ms (16.43%), Space: 27.9 MB (27.29%) - LeetHub

---
+++ 247-strobogrammatic-number-ii/247-strobogrammatic-number-ii.py
@@ -0,0 +1,28 @@
+
+class Solution:
+    def findStrobogrammatic(self, n: int) -> List[str]:
+        dic = {"6":"9", "9": "6"}
+        same = {"0", "1", "8"}
+        if n == 1:
+            return same
+        m = n//2
+        sets = set(["1", "8", "6", "9"])
+        for _ in range( m- 1):
+            sets = set([num + c for num in sets for c in "01689"])
+        
+        def convert(num, n):
+            res = ""
+            for c in num:
+                if c in same:
+                    res = c + res
+                else:
+                    res = dic[c] + res
+            return [num + res] if n % 2 ==0 else [num + "0" + res, num + "1" + res, num + "8" + res]
+        
+        res = []
+        for num in sets:
+            res.extend(convert(num, n))
+        return res
+            
+                
+        
