e5ed85ae91af5ba414ae47d85d925e5e9ddbc945
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-06-26 21:05:57 -0400

    Time: 743 ms (79.70%), Space: 22.2 MB (5.56%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -7,14 +7,12 @@ class Solution:
        
         diff += diff
         n = len(gas)
-        
-        while diff and diff[0] <0:
-            diff.pop(0)
+        diff = diff[::-1]
         
         presum, ct = 0, 0
         while diff:
-            nxt = presum + diff.pop(0)
-            if  nxt <0:
+            nxt = presum + diff.pop()
+            if  nxt < 0:
                 presum, ct = 0, 0
             else:
                 presum = nxt
