67d7a8c4c6ed7398217338e557eaf190fd4ca5d4
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-02 19:51:54 -0400

    Time: 3018 ms (5.05%), Space: 17.1 MB (49.13%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        res = set([])
+        
+        supplies = set(supplies)
+        q = deque([])
+        
+        for r,i in zip(recipes, ingredients):
+            if set(i) & supplies == set(i):
+                res.add(r)
+                supplies.add(r)
+            else:
+                q.append((r, i))
+                
+                
+        if not q:
+            return res
+        
+        flag = True
+        while flag:
+            m = len(q)
+            for _ in range(m):
+                r, i = q.popleft()
+                if set(i) & supplies == set(i):
+                    res.add(r)
+                    supplies.add(r)
+                else:
+                    q.append((r, i))
+            if len(q) == m:
+                return res
+                
+            
+            
+        
+        
