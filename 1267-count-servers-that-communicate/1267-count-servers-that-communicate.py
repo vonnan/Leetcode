b25a281a5b5d91b46e7dc902b94de0129348047a
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-02 00:31:40 -0400

    Time: 1072 ms (15.03%), Space: 15.9 MB (23.58%) - LeetHub

---
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -0,0 +1,39 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        UF = {}
+        
+        def find(x):
+            if UF[x] != x:
+                UF[x] = find(UF[x])
+            return UF[x]
+        
+        def union(x, y):
+            UF.setdefault(x, x)
+            UF.setdefault(y, y)
+            UF[find(x)] = find(y)
+        
+        row, col = len(grid), len(grid[0])
+        
+        dic_r, dic_c = defaultdict(list), defaultdict(list)
+        sets = set([])
+        ct = 0
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c]:
+                    dic_r[r].append(c)
+                    dic_c[c].append(r)
+                   
+        
+        for r, lst in dic_r.items():
+            if len(lst) > 1:
+                for c in lst:
+                    sets.add((r, c))
+        
+        for c, lst in dic_c.items():
+            if len(lst) >1:
+                for r in lst:
+                    sets.add((r, c))
+        
+        print(ct, sets)
+        return len(sets)
+                    
