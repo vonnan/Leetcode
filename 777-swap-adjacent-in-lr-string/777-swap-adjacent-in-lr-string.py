6c2a40eaa9db329e30186ea70083fd7cc8adc32b
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-02 16:43:29 -0400

    Time: 71 ms (58.72%), Space: 14.4 MB (41.73%) - LeetHub

--- 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.py
+++ 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.py
@@ -1,38 +1,6 @@
 class Solution:
     def canTransform(self, start: str, end: str) -> bool:
-        '''
-        counter_s = Counter(start)
-        counter_e = Counter(end)
-        if counter_s["X"] != counter_e["X"] or counter_s["L"] != counter_e["L"] or counter_s["R"] != counter_e["R"]:
-            return False
+        s = [i if c == "L" else -i for i, c in enumerate(start, 1) if c in "LR"]
+        e = [i if c == "L" else -i for i, c in enumerate(end, 1) if c in "LR"]
         
-        
-        start_L, start_R = [],[]
-        
-        for i, c in enumerate(start):
-            if c == "L":
-                start_L.append(i)
-            elif c == "R":
-                start_R.append(i)
-        print(start_L, start_R)
-        
-        for i, c in enumerate(end):
-            if c == "L":
-                if i > start_L.pop(0):
-                    return False
-            elif c == "R":
-                if i < start_R.pop(0):
-                    return False
-        
-        return True
-        '''
-        s = [i if c == 'L' else -i for i, c in enumerate(start, start=1) if c in 'RL']
-        e = [i if c == 'L' else -i for i, c in enumerate(end, start=1) if c in 'RL']
-        
-        return len(s) == len(e) and all((i>0) == (j>0) and i >= j for i, j in zip(s,e))        
-    
-    
-    
-                
-            
-        
+        return len(s) == len(e) and start.count("L") == end.count("L") and all(a >= b for a,b in zip(s,e))
