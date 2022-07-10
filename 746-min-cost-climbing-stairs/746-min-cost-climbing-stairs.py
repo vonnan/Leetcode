4601bacdf36595e28d4f07bb072034f5266a40e4
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-09 20:45:13 -0400

    Time: 67 ms (84.26%), Space: 14 MB (44.40%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        prev, curr = 0, 0
+        for i, c in enumerate(cost):
+            prev, curr = curr, min(prev, curr) + c
+        return min(prev, curr)
