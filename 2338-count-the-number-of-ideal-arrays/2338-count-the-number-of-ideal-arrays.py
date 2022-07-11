5d7efdd6ab62a901aa5f06312f2e747568c32e53
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-11 16:43:42 -0400

    Time: 817 ms (77.71%), Space: 66.4 MB (6.76%) - LeetHub

--- 2338-count-the-number-of-ideal-arrays/2338-count-the-number-of-ideal-arrays.py
+++ 2338-count-the-number-of-ideal-arrays/2338-count-the-number-of-ideal-arrays.py
@@ -1,24 +1,20 @@
-
 class Solution:
     def idealArrays(self, n: int, maxValue: int) -> int:
-        # for each number, compute how many branches it can have. Then for each of those branches
-        # compute how many branches they have
-        # r = 0
-        # for k in range(1, maxValue+1):
-        #     r += maxValue // k
-        # return r
-        MOD = 1000000007
-        @cache
-        def rec(i, k):
-		    # i: current number; k: increase count
-            m = comb(n-1, k-1) % MOD  # combination if the sequence stops here
-            if k == n:  # no more chance to increase; sequence stops here
-                return m % MOD
-            for j in range(i+i, maxValue+1, i):
-                m += rec(j, k+1) % MOD
-            return m % MOD
+        mod = 10 **9 + 7
         
-        m = 0
-        for i in range(1, maxValue+1):
-            m += rec(i, 1) % MOD
-        return m % MOD
+        @lru_cache(None)
+        def dp(last, k):
+            res = math.comb(n-1, k-1) % mod
+            if k == n:
+                return res % mod
+            
+            for j in range(2*last, maxValue +1, last):
+                res += dp(j, k + 1)
+            return res
+        
+        res = 0
+        for last in range(1, maxValue+1):
+            res += dp(last, 1) % mod
+        
+        return res % mod
+        
