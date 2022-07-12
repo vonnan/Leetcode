6ea4eed729c60ecfa24adfe5b49d5f6517db1ea1
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-12 01:16:12 -0400

    Time: 86 ms (38.73%), Space: 16.2 MB (48.49%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    
+    def preorder(self, root: 'Node') -> List[int]:
+        res = []
+        def dfs(node):
+            if not node:
+                return
+            
+            res.append(node.val)
+            for x in node.children:
+                dfs(x)
+        
+        dfs(root)
+        return res
+        
+        
