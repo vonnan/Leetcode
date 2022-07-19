e19dc9d032b6ec7b4842658239d2d34b12c5a650
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-19 13:59:44 -0400

    Time: 3626 ms (25.37%), Space: 66.8 MB (41.39%) - LeetHub

--- 786-k-th-smallest-prime-fraction/786-k-th-smallest-prime-fraction.py
+++ 786-k-th-smallest-prime-fraction/786-k-th-smallest-prime-fraction.py
@@ -9,10 +9,10 @@ class Solution:
         n = len(arr)
         for i, a in enumerate(arr):
             for j in range(i + 1, n):
-                heappush(heap, (a/arr[j], i, j))
+                heappush(heap, (a/arr[j], a, arr[j]))
         
         while heap:
-            val, i, j = heappop(heap)
+            val, a, b = heappop(heap)
             k -= 1
             if not k:
-                return (arr[i], arr[j])
+                return (a,b)
