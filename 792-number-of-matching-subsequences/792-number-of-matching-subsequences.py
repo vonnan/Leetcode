20f20c8fc2042504462c852147ae3a089af0960c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-19 23:37:59 -0400

    Time: 5109 ms (6.07%), Space: 15.5 MB (75.71%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,9 +1,13 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
         res = 0
+        counter = Counter(s)
         for word in words:
             start = 0
             flag = True
+            ct = Counter(word)
+            if ct & counter != ct:
+                continue
             for c in word:
                 if c not in s[start:]:
                     flag = False
