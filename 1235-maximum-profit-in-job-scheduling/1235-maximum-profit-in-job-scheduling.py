82b2fcd295444c359b09da954a2254821578c6b8
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-22 00:55:19 -0400

    Time: 535 ms (98.32%), Space: 26.6 MB (77.18%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,19 +1,19 @@
-from bisect import bisect_left
+from bisect import bisect
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-
-        sep = sorted([(s,e,p) for s,e,p in zip(startTime, endTime, profit)])
-        start =[ s for s, _, _ in sep]
-        dp = [p for _,_, p in sep]
-        n = len(start)
-        
-        for i in range(n-2, -1, -1):
+        sep = sorted([(s,e,p) for s,e,p in zip(startTime, endTime, profit)], key = lambda x: x[1])
+        end = [e for  _,e, _ in sep]
+        dp = [p for _,_,p in sep]
+        n = len(sep)
+        for i in range(1, n):
             s,e,p = sep[i]
-            idx = bisect_left(start, e)
-            if idx != n:
-                dp[i] = max(dp[i] + dp[idx], dp[i+1])
+            idx = bisect(end, s)
+            if idx ==0:
+                dp[i] = max(dp[i-1], p)
             else:
-                dp[i] = max(dp[i], dp[i + 1])
+                dp[i] = max(dp[i-1], dp[idx -1] + p)
         
-        return dp[0]
+        return dp[-1]
                 
+                
+            
