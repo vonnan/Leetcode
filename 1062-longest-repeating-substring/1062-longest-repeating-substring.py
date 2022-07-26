be80314162bcf4f6db49963db149fe2dcc727ad0
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-26 17:52:52 -0400

    Time: 3449 ms (8.54%), Space: 235.5 MB (7.79%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.py
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -1,22 +1,15 @@
 class Solution:
     def longestRepeatingSubstring(self, s: str) -> int:
-        counter = Counter(s)
-        sets = [key for key, val in counter.items() if val == 1 ]
         n = len(s)
-        dic = defaultdict(int)
-        res = 0
-        for i,c in enumerate(s):
-            if c in sets:
-                continue
-            for j in range(i+1, n):
-                if s[j] in sets:
-                    break
-                if s[i:(j+ 1)] not in dic:
-                    dic[s[i:(j + 1)]] = i
-                else:
-                    if i > dic[s[i: (j+ 1)]]:
-                        res = max(res, j - i + 1)
-        return res
+        counter = Counter([])
+        res = 1
+        for i, c in enumerate(s):
+            pre = c
+            counter[pre] += 1
+            for j in range(i + 1, n):
+                pre += s[j]
+                counter[pre] += 1
                 
-        
-        
+        lst = [len(key) for key, val in counter.items() if val > 1]  
+        return max(lst) if lst else 0
+                
