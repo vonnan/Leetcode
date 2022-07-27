3acd6e3cf272a545cfa5f0ad282b641e76cdb0a2
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-26 23:05:05 -0400

    Time: 54 ms (60.12%), Space: 15.2 MB (87.68%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -5,31 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def flatten(self, root: TreeNode) -> None:
+    def flatten(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        
         q = deque([root])
         while root and q:
             root = q.pop()
             
             if root.right:
                 q.append(root.right)
-            
+                
             if root.left:
                 q.append(root.left)
                 root.right = root.left
-                root.left = None
+                root.left  = None
             else:
                 if q:
                     root.right = q[-1]
                     root.left = None
-                    
-        
-                
-            
-        
-        
-                    
-                
