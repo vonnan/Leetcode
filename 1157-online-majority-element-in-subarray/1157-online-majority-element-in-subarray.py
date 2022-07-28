fd2895e663fe44904296ec838a866d45795ec3b8
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-28 14:40:21 -0400

    Time: 2158 ms (41.05%), Space: 24.3 MB (48.42%) - LeetHub

--- 1157-online-majority-element-in-subarray/1157-online-majority-element-in-subarray.py
+++ 1157-online-majority-element-in-subarray/1157-online-majority-element-in-subarray.py
@@ -1,28 +1,24 @@
 from random import randint
 from bisect import bisect_left
 from bisect import bisect
-
 class MajorityChecker:
 
     def __init__(self, arr: List[int]):
         self.dic = defaultdict(list)
-        self.arr = arr
         for i, a in enumerate(arr):
             self.dic[a].append(i)
-
+        self.arr = arr
+    
     def query(self, left: int, right: int, threshold: int) -> int:
         
         for _ in range(20):
             a = self.arr[randint(left, right)]
-            lst = self.dic[a]
-            l, r = bisect_left(lst, left), bisect(lst, right)
+            l = bisect_left(self.dic[a], left)
+            r = bisect_right(self.dic[a], right)
             if r - l >= threshold:
                 return a
         return -1
-            
-            
         
-
 
 # Your MajorityChecker object will be instantiated and called as such:
 # obj = MajorityChecker(arr)
