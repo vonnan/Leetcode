5badc2265930410f7cbc39e1f27520f5961078ee
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-28 02:00:27 -0400

    Time: 1425 ms (91.67%), Space: 30.4 MB (29.17%) - LeetHub

---
+++ 1521-find-a-value-of-a-mysterious-function-closest-to-target/1521-find-a-value-of-a-mysterious-function-closest-to-target.py
@@ -0,0 +1,19 @@
+from bisect import bisect_left
+
+class Solution:
+    def closestToTarget(self, arr: List[int], target: int) -> int:
+        res, curr = set([]), set([])
+        
+        for a in arr:
+            curr = {a & b for b in curr} | {a}
+            res |= curr
+        
+        res = sorted(list(res))
+        print(res)
+        idx = bisect_left(res, target)
+        if idx ==0:
+            return res[0] - target
+        elif idx == len(res):
+            return target - res[-1]
+        else:
+            return min(target - res[idx - 1], res[idx] - target)
