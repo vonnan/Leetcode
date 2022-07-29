80850273bd16dfa7321011e40cda2a3b2ec8860f
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-07-29 00:40:54 -0400

    Time: 913 ms (83.59%), Space: 16.2 MB (46.37%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,26 +1,24 @@
 class Solution:
     def minimumEffortPath(self, A: List[List[int]]) -> int:
         row, col = len(A), len(A[0])
-        path = [(0,1), (0, -1), (1, 0), (-1, 0)]
         
         seen = set([])
         
         heap = [(0, 0, 0)]
+        path = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         
         while heap:
-            val, r,c = heappop(heap)
-            #print(val, r, c)
+            val, r, c = heappop(heap)
             if (r,c) in seen:
                 continue
-                
-            if r == row-1 and c == col - 1:
+            else:
+                seen.add((r, c))
+            if r == row -1 and c == col - 1:
                 return val
-            seen.add((r, c))
-            a = A[r][c]
+            prev = A[r][c]
+            
             for dr, dc in path:
                 nr, nc = r + dr, c + dc
                 if 0 <= nr < row and 0 <= nc < col and (nr, nc) not in seen:
-                    tmp = max(val, abs(A[nr][nc] - a))
-                    heappush(heap, (tmp, nr, nc))
-        
-        
+                    nv = max(val, abs(A[nr][nc] - prev))
+                    heappush(heap, (nv, nr, nc))
