89ec5a4135157df03340c884259bcede0fffe113
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-02 13:28:01 -0400

    Time: 3762 ms (65.26%), Space: 29 MB (67.46%) - LeetHub

--- 327-count-of-range-sum/327-count-of-range-sum.py
+++ 327-count-of-range-sum/327-count-of-range-sum.py
@@ -4,11 +4,18 @@ from bisect import bisect
 
 class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
-        s, total, res = SortedList([0]), 0, 0
-        for n in nums:
-            total +=n
-            res += s.bisect_right(total-lower) - s.bisect_left(total-upper)
-            s.add(total)
+        presum = SortedList([0])
+        prev = 0
+        res = 0
+        for num in nums:
+            
+            nxt = prev + num
+            left, right = presum.bisect_left(nxt - upper), presum.bisect(nxt- lower)
+            #print(num, nxt, nxt- upper, nxt- lower, left, right, presum)
+            res += right - left
+            presum.add(nxt)
+            prev = nxt
+        
         return res
             
             
