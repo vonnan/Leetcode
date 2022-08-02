d58bdc8785be1a4aabab63e9c082e2a31ab2ed1c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-02 00:54:43 -0400

    Time: 187 ms (43.33%), Space: 14.7 MB (83.32%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,25 +1,24 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        ct_t, ct_s, res = Counter(t), Counter(), inf
-        seen, size, l, ans = set(), len(ct_t), 0, ""
+        ct_s, ct_t, res, ans = Counter([]), Counter(t), inf, ""
+        seen = set([])
+        size, l = len(ct_t), 0
         
         for r, ch in enumerate(s):
             ct_s[ch] += 1
-            #counter of t of ch adds 1
             if ch in ct_t and ct_t[ch] == ct_s[ch]:
                 seen.add(ch)
-            #if ct_t[ch] == ct_s[ch], add ch to seen
-            
+                
             while len(seen) == size:
-                if ct_t[s[l]] == ct_s[s[l]]:
-                    #make the left side as "compact" as possible: if the leftmost character can not be removed, it the "shortest" we can get with the rightmost position as r
+                if s[l] in ct_t and ct_s[s[l]] == ct_t[s[l]]:
                     seen.remove(s[l])
-                    if res > r - l + 1:
+                    if r- l + 1 < res:
                         res = r - l + 1
-                        ans = s[l:r+1]
+                        ans = s[l: r+ 1]
                 ct_s[s[l]] -= 1
                 l += 1
-        
-        return ans
             
-    
+        return ans
+                
+                    
+                
