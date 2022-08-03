e27670ff515c2d2e722511901f8b99f203eb4c42
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-03 17:33:22 -0400

    Time: 52 ms (35.97%), Space: 13.9 MB (81.50%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,18 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         res = []
-        n = len(s)
+        
         def dfs(s, path):
-            if sum(map(len, path)) == n:
+            if not s:
                 res.append(" ".join(path))
             
             for word in wordDict:
                 m = len(word)
                 if s[:m] == word:
-                    dfs(s[m:], path + [word])
+                    dfs(s[m:], path + [s[:m]])
         
         dfs(s, [])
         return res
-                
-                
-        
