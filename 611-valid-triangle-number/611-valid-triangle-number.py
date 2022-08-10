66e2da088196f12f83e88032fc1d167f59414191
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-10 00:56:08 -0400

    Time: 3465 ms (22.64%), Space: 14.2 MB (27.06%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.py
+++ 611-valid-triangle-number/611-valid-triangle-number.py
@@ -1,14 +1,21 @@
 from bisect import bisect_left
+
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        nums.sort()
+        nums = [num for num in nums if num > 0]
+        if len(nums) < 3:
+            return 0
+        
         n = len(nums)
         res = 0
-        for i in range(n-2):
-            for j in range(i+1, n-1):
-                idx = bisect_left(nums, nums[i] + nums[j]) -1
+        nums.sort()
+        
+        for i, ni in enumerate(nums[:-2]):
+            for j, nj in enumerate(nums[i+1:-1], i + 1):
+                idx = bisect_left(nums, ni + nj) - 1
                 if idx > j:
                     res += idx - j
-        return res
                 
         
+        return res
+                
