e7fac1e56cf98245519490455bb7c2e60432955b
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-10 00:01:26 -0400

    Time: 116 ms (68.46%), Space: 15.6 MB (32.22%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.py
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.py
@@ -10,7 +10,7 @@ class Solution:
             return None
         
         mid = len(nums)//2
-        node = TreeNode(nums[mid])
-        node.left = self.sortedArrayToBST(nums[:mid])
-        node.right = self.sortedArrayToBST(nums[mid + 1:])
-        return node
+        res = TreeNode(nums[mid])
+        res.left = self.sortedArrayToBST(nums[:mid])
+        res.right = self.sortedArrayToBST(nums[mid + 1:])
+        return res
