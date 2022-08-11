fd9df4bcb1fd121d7abb7f0aacbeb101efcb20e2
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-11 12:51:02 -0400

    Time: 104 ms (6.02%), Space: 17.5 MB (14.84%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -7,12 +7,15 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         res = []
+        
         def dfs(node):
             if not node:
-                return
+                return 
+            
             dfs(node.left)
             res.append(node.val)
             dfs(node.right)
-        
+            
         dfs(root)
         return res == sorted(res) and len(res) == len(set(res))
+        
