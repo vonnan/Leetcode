82be386b2c34ca6f602191d4c11b172a3bba5292
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-11 23:53:20 -0400

    Time: 2669 ms (46.95%), Space: 26.3 MB (94.87%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
@@ -4,8 +4,14 @@ class Solution:
         left, right = 1, max(nums)
         while left < right:
             mid = (left + right)//2
-            tot = sum(ceil(num/mid) - 1 for num in nums if num > mid)
-            if tot > max_:
+            ct = 0
+            for num in nums:
+                if num > mid:
+                    ct += ceil(num/mid) - 1
+                    if ct > max_:
+                        break
+            
+            if ct > max_:
                 left = mid + 1
             else:
                 right = mid
