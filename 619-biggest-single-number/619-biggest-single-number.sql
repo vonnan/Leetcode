a64a34aae5cbbec2641174b16b807c70e1a359fc
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-11 23:20:14 -0400

    Time: 384 ms (65.99%), Space: 0B (100.00%) - LeetHub

--- 619-biggest-single-number/619-biggest-single-number.sql
+++ 619-biggest-single-number/619-biggest-single-number.sql
@@ -3,4 +3,8 @@ SELECT (SELECT num
         FROM MyNumbers
         Group By num
         HAVING count(*) = 1
+        
+        UNION ALL
+        SELECT NULL
+        
         ORDER BY num DESC Limit 1) num
