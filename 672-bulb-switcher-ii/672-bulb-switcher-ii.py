6149b119a169165b5fbc7995c9476803c2752d08
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-16 16:23:35 -0400

    Time: 51 ms (41.24%), Space: 13.9 MB (69.07%) - LeetHub

--- 672-bulb-switcher-ii/672-bulb-switcher-ii.py
+++ 672-bulb-switcher-ii/672-bulb-switcher-ii.py
@@ -1,37 +1,17 @@
 class Solution:
-    def flipLights(self, n: int, p: int) -> int:
+    def flipLights(self, n: int, m: int) -> int:
         
-        states = [1] * n
-        res = set([tuple(states)])
-        
-        for _ in range(p):
-            sets = set([])
-            for x in res:
-                new = list(x)
-                for i in range(0,n, 2):
-                    new[i] = 1- new[i]
-                sets.add(tuple(new))
-                
-                new = list(x)
-                for i in range(1, n, 2):
-                    new[i] = 1- new[i]
-                sets.add(tuple(new))
-                
-                new = list(x)
-                for i in range(n):
-                    new[i] = 1 -new[i]
-                sets.add(tuple(new))
-                
-                new = list(x)
-                for i in range(0, n, 3):
-                    new[i] = 1- new[i]
-                sets.add(tuple(new))
-            
-            res = sets
-        
-        return len(res)
-                
-                
-        
-        
-            
+        seen = set()
+        for cand in itertools.product((0, 1), repeat = 4):
+            if sum(cand) % 2 == m % 2 and sum(cand) <= m:
+                A = []
+                for i in range(min(n, 3)):
+                    light = 1
+                    light ^= cand[0]
+                    light ^= cand[1] and i % 2
+                    light ^= cand[2] and i % 2 == 0
+                    light ^= cand[3] and i % 3 == 0
+                    A.append(light)
+                seen.add(tuple(A))
+
+        return len(seen)
