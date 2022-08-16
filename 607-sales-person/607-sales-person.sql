71b69545a75e0d2a9add73b0bc3322751c4842b2
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-16 01:56:35 -0400

    Time: 2512 ms (16.17%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT s.name
-FROM Orders o JOIN Company c ON o.com_id = c.com_id and c.name = "RED"
+FROM Orders o JOIN Company c ON o.com_id = c.com_id and c.name = "Red"
 RIGHT JOIN SalesPerson s ON s.sales_id = o.sales_id
-WHERE o.sales_id is Null
+WHERE o.sales_id is NULL
