49767f30d97c38e6e9a464441ba6948f64f77048
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-17 16:08:05 -0400

    Time: 195 ms (83.47%), Space: 20.4 MB (37.22%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -4,24 +4,10 @@ class Solution:
         if target < matrix[0][0] or target > matrix[-1][-1]:
             return False
         
-        for rows in matrix:
-            if target < rows[0]:
-                return False
-            elif target > rows[-1]:
+        for r in range(row):
+            if target > matrix[r][-1]:
                 continue
             else:
-                left, right = 0, col -1
-                if target == rows[left] or target == rows[right]:
-                    return True
-                while left < right:
-                    mid = (left + right)//2
-                    m = rows[mid]
-                    if m == target:
-                        return True
-                    elif m < target:
-                        left = mid + 1
-                    else:
-                        right = mid
-                if rows[left] == target:
+                if target in matrix[r]:
                     return True
         return False
