dca4d4c8808381fecf4c824a2a6cc1c0857e25e6
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-20 03:11:36 -0400

    Time: 1019 ms (84.99%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -3,6 +3,6 @@ SELECT u.user_id buyer_id, u.join_date, COUNT(o.order_id) orders_in_2019
 FROM Users u 
 LEFT JOIN Orders o
 ON o.buyer_id = u.user_id and YEAR(o.order_date) = "2019"
-GROUP By u.user_id
-ORDER BY u.user_id
+GROUP By 1
+ORDER BY 1
 
