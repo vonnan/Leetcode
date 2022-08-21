48b648c46b89af24fc1bd9e0bab246af81c81e10
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-21 16:20:05 -0400

    Time: 3957 ms (18.75%), Space: 26.7 MB (80.36%) - LeetHub

--- 891-sum-of-subsequence-widths/891-sum-of-subsequence-widths.py
+++ 891-sum-of-subsequence-widths/891-sum-of-subsequence-widths.py
@@ -3,4 +3,4 @@ class Solution:
         nums.sort()
         mod = 10 ** 9 + 7
         n = len(nums)
-        return sum(((1<<i) - (1<<(n-1-i))) * num % mod for i, num in enumerate(nums)) % mod
+        return sum(((1<<i) - (1<<n-1-i)) * num  for i, num in enumerate(nums)) % mod
