3985c2ca4e96479f03c07798808be1e35aa9af14
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-23 00:05:17 -0400

    Time: 312 ms (95.49%), Space: 20.2 MB (53.92%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.py
+++ 436-find-right-interval/436-find-right-interval.py
@@ -1,19 +1,19 @@
 from bisect import bisect_left
+
 class Solution:
-    def findRightInterval(self, A: List[List[int]]) -> List[int]:
-        lst =sorted([[a[0], i] for i, a in enumerate(A)])
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        n = len(intervals)
+        start = sorted([(intervals[i][0], i) for i in range(n)])
         
         res = []
-        n = len(A)
-        for i, a in enumerate(A):
-            end = a[1]
-            idx = bisect_left(lst, [end])
+        
+        for i, lst in enumerate(intervals):
+            _, end = lst
+            idx = bisect_left(start, (end, -1))
             if idx == n:
                 res.append(-1)
             else:
+                res.append(start[idx][1])
                 
-                res.append(lst[idx][1])
-                
+        
         return res
-                    
-                
