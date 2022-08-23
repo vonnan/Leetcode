f54f986306be6377fa1f5f2c8da4ee60653dba5d
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-22 22:13:05 -0400

    Time: 606 ms (42.80%), Space: 17.4 MB (17.16%) - LeetHub

--- 826-most-profit-assigning-work/826-most-profit-assigning-work.py
+++ 826-most-profit-assigning-work/826-most-profit-assigning-work.py
@@ -1,25 +1,20 @@
 from bisect import bisect
+
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        lst = sorted([[d, p] for d,p in zip(difficulty, profit)])
-        
+        lst = [(0, 0)] + sorted([(d, p) for d,p in zip(difficulty, profit)])
         dic = defaultdict(int)
         max_= 0
-        dic[0] = max_
-        
         for i, x in enumerate(lst):
             d, p = x
             max_ = max(max_, p)
             dic[i] = max_
-        
+          
         res = 0
-        counter = Counter(worker)
-        for w, val in counter.items():
-            idx = bisect(lst, [w, inf]) - 1
-            res += dic[idx] * val
-        return res
+        for d in sorted(worker):
+            idx = bisect(lst, (d, inf)) - 1
             
-            
-            
+            res += dic[idx]
         
+        return res
             
