6b08f20e9137963c76d14323992853c9164268d7
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-24 16:08:53 -0400

    Time: 196 ms (72.43%), Space: 13.9 MB (96.32%) - LeetHub

---
+++ 514-freedom-trail/514-freedom-trail.py
@@ -0,0 +1,28 @@
+from bisect import bisect_left
+class Solution:
+    def findRotateSteps(self, ring: str, key: str) -> int:
+        n = len(ring)
+        dic = defaultdict(list)
+        for i, c in enumerate(ring):
+            dic[c].append(i)
+            
+        def dist(a, b):
+            d = abs(a - b)
+            return min(d, n - d)
+        
+        state = {0 : 0}
+        for c in key:
+            nxt = defaultdict(int)
+            for i in dic[c]:
+                nxt[i] = inf
+                for pos in state:
+                    nxt[i] = min(nxt[i], state[pos] + dist(i, pos))
+            
+            state = nxt
+        return min(state.values()) + len(key)
+                
+        
+              
+                
+            
+                
