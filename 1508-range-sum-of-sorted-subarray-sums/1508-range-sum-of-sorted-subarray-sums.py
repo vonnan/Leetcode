92206ddc955e30edb30afe90e2dfabc8b3258028
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-25 01:19:55 -0400

    Time: 483 ms (73.68%), Space: 41.4 MB (10.84%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
@@ -1,11 +1,14 @@
-from sortedcontainers import SortedList
 
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
-        presum = list(accumulate(nums, initial = 0))
-        SL = SortedList([])
-        for i in range(n):
-            for j in range(i + 1, n + 1):
-                SL.add(presum[j] - presum[i])
-        return sum(SL[left - 1:right]) %(10**9 + 7)
+        res, mod = [], 10 ** 9 + 7
         
+        for i in range(n):
+            tot = nums[i]
+            res.append(tot)
+            
+            for j in range(i + 1, n):
+                tot += nums[j]
+                res.append(tot)
+        
+        return sum(sorted(res)[left-1: right])%mod
