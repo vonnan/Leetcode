c3854d0f35120aa48db9b3d9c49b892edc6bdd23
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-26 11:34:11 -0400

    Time: 2546 ms (27.52%), Space: 28.3 MB (13.20%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
@@ -7,24 +7,25 @@ class Solution:
         n = len(A)
         if m == n:
             return min(A[i] - A[i-1] for i in range(1, n))
-        print(A)
+        
         left, right = 0, A[-1]
         while left < right:
             mid = (left + right + 1)//2
-            start = A[0]
+            start = 0
             ct = 1
-            for num in A[1:]:
-                if num - start >= mid:
-                    ct += 1
-                    if ct > m or A[-1] - num < mid:
-                        break
-                    start = num
-            #print(mid, ct, left, right)
+            while start < n:
+                idx = bisect_left(A, A[start] + mid)
+                if idx == start or idx == n:
+                    break
+                start = idx
+                ct += 1
+                if ct >= m:
+                    break
+            #print(mid, ct)
             if ct >= m:
                 left = mid
             else:
                 right = mid - 1
-                
         return left
             
         
