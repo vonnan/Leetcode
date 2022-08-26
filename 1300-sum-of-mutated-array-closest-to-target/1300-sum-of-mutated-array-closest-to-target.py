d7fbded98e2b81897ae234fdbb2df5791ce284b3
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-26 01:53:41 -0400

    Time: 113 ms (89.32%), Space: 15.3 MB (49.19%) - LeetHub

--- 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.py
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.py
@@ -1,11 +1,37 @@
+from bisect import bisect
+
 class Solution:
     def findBestValue(self, arr: List[int], target: int) -> int:
-        arr.sort(reverse = True)
+        arr.sort()
+        n = len(arr)
+        presum = list(accumulate(arr, initial = 0))
+        left, right = 0, arr[-1]
         
-        if sum(arr) <= target:
-            return arr[0]
+        def helper(m):
+            idx = bisect(arr, m)
+            return presum[idx] + (n - idx) * m
         
-        while arr and target > arr[-1] * len(arr):
-            target -= arr.pop()
+        if presum[-1] <= target:
+            return arr[-1]
+        res = right
         
-        return int(round((target -0.00001)/len(arr)))
+        while left <= right:
+            mid = (left + right + 1) //2
+            
+            tot = helper(mid)
+            
+            if tot == target:
+                return mid
+            elif tot > target:    
+                right = mid - 1
+            else:
+                left = mid + 1
+            
+        
+        if abs(helper(left) - target) < abs(helper(right) - target):
+            return left
+        return right
+        
+            
+        
+            
