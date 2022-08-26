087cc54444e383a100fa36c9e13f262b400fa1c2
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-26 00:07:10 -0400

    Time: 321 ms (22.02%), Space: 14 MB (10.09%) - LeetHub

--- 842-split-array-into-fibonacci-sequence/842-split-array-into-fibonacci-sequence.py
+++ 842-split-array-into-fibonacci-sequence/842-split-array-into-fibonacci-sequence.py
@@ -1,7 +1,7 @@
 class Solution:
     def splitIntoFibonacci(self, num: str) -> List[int]:
-        self.res = []
         n = len(num)
+        self.res = []
         
         def dfs(pos, path):
             if path:
@@ -16,14 +16,17 @@ class Solution:
                         return
                 
                     if pos == n:
+                        
                         self.res = path[:]
-                        print(path)
                         return
+                
+                    
                 
             for nxt in range(pos + 1, n + 1):
                 dfs(nxt, path + [num[pos : nxt]])
         
-        dfs(0, [])
+        res = dfs(0, [])
         return [int(c) for c in self.res] if self.res else []
+    
                 
                 
