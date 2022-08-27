77761e8cea2b31a24772af0cfb5120f306cfeec1
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-27 17:34:27 -0400

    Time: 500 ms (84.31%), Space: 34 MB (66.08%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -1,27 +1,23 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.snapid = 0
-        self.arr = defaultdict(dict)
         self.size = length
-        
-        
+        self.array = defaultdict(dict)
+        self.id = 0
+
     def set(self, index: int, val: int) -> None:
-        self.arr[index][self.snapid] = val
-        
+        self.array[self.id][index] = val
+
     def snap(self) -> int:
-        for index in self.arr:
-            if self.snapid in self.arr[index]:
-                self.arr[index][self.snapid + 1] = self.arr[index][self.snapid]
-        self.snapid += 1
-        return self.snapid - 1
- 
+        self.id += 1
+        return self.id - 1
+
     def get(self, index: int, snap_id: int) -> int:
-        if snap_id in self.arr[index]:
-            return self.arr[index][snap_id]
-        else:
+        while snap_id and index not in self.array[snap_id]:
+            snap_id -= 1
+        if snap_id == 0 and index not in self.array[0]:
             return 0
-        
+        return self.array[snap_id][index]
         
 
 
