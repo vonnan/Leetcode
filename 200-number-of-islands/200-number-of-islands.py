7d1083af20545387974d9c263b2054b883f30630
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-29 00:58:56 -0400

    Time: 299 ms (96.07%), Space: 16.5 MB (50.32%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,15 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        row, col = len(grid), len(grid[0])
-        path = [ (0, 1), (0, -1), (1, 0), (-1, 0)]
         
-        def dfs(r, c):
+        row, col = len(grid), len(grid[0])
+        path = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        def dfs(r,c):
             if 0 <= r < row and 0 <= c < col and grid[r][c] == "1":
-                grid[r][c] = 0
-                for dr, dc in path:
-                    dfs(r + dr, c + dc)
+                grid[r][c] = "0"
+                dfs(r, c + 1)
+                dfs(r, c - 1)
+                dfs(r + 1, c)
+                dfs(r - 1, c)
                 return 1
             return 0
         
+       
         return sum(dfs(r, c) for r in range(row) for c in range(col))
-                
