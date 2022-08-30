479b11068b04e57d9486e6ec50fd4797c59c55a5
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-30 18:47:02 -0400

    Time: 1249 ms (20.86%), Space: 50.2 MB (16.88%) - LeetHub

--- 815-bus-routes/815-bus-routes.py
+++ 815-bus-routes/815-bus-routes.py
@@ -1,28 +1,35 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        nei = defaultdict(set)
-        for i, route in enumerate(routes):
-            for stop in route:
-                nei[stop].add(i)
-                
-        q = deque([source])
-        visited_rt, visited_stop = set([]), set([source])
+        if source == target:
+            return 0
         
-        res = 0
+        graph = defaultdict(set)
+        
+        for rt, stops in enumerate(routes):
+            for stop in stops:
+                graph[stop].add(rt)
+        
+        q = deque([source])
+        
+        res = 1
+        
+        visited_rt, visited_stop = set([]), set([])
+        
         while q:
             m = len(q)
             for _ in range(m):
                 stop = q.popleft()
-                if stop == target:
-                    return res
-                for rt in nei[stop]:
+                for rt in graph[stop]:
                     if rt not in visited_rt:
                         visited_rt.add(rt)
                         for stop in routes[rt]:
-                            if stop not in visited_stop:
-                                q.append(stop)
+                            if stop == target:
+                                return res
+                            elif stop not in visited_stop:
                                 visited_stop.add(stop)
+                                q.append(stop)
             res += 1
         
         return -1
-        
+                
+            
