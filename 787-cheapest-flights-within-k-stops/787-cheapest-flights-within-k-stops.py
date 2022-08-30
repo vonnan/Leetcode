1034f9dab2a72ae673aee394cdef13d8a5f79f2c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-30 16:28:03 -0400

    Time: 107 ms (97.19%), Space: 15.4 MB (37.75%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,31 @@
+from heapq import heappush
+from heapq import heappop
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        nei = defaultdict(dict)
+        for u, v, cost in flights:
+            nei[u][v] = cost
+        print(nei)
+        
+        heap = [(0, 0, src)]
+        seen = {src: 0}
+        
+        while heap:
+            cost, stop, u = heappop(heap)
+            seen[u] = stop
+            if u == dst:
+                return cost
+            if stop == k + 1:
+                continue
+                
+            for v in nei[u]:
+                if v in seen and seen[v] <= stop + 1:
+                    continue
+                else:
+                    heappush(heap, (cost + nei[u][v], stop + 1, v))
+        
+        return -1
+                    
+            
+            
