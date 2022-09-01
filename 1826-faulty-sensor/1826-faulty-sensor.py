192c8edb1a564f4e9753b161372305e3eeab8ba6
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-01 02:18:08 -0400

    Time: 74 ms (50.00%), Space: 13.7 MB (96.30%) - LeetHub

---
+++ 1826-faulty-sensor/1826-faulty-sensor.py
@@ -0,0 +1,13 @@
+class Solution:
+    def badSensor(self, sensor1: List[int], sensor2: List[int]) -> int:
+        n = len(sensor1)
+        for i in range(n):
+            if sensor1[i] != sensor2[i]:
+                break
+        if i == n -1 or (sensor1[i:-1] == sensor2[i + 1:] and sensor2[i:-1] == sensor1[i + 1:]):
+            return -1
+        if sensor1[i:-1] == sensor2[i + 1:]:
+            return 1
+        elif sensor2[i:-1] == sensor1[i + 1:]:
+            return 2
+        
