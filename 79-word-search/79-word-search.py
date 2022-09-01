c0b55ebb0a67fed232be9b6fe19907618b8659b4
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-08-31 23:01:54 -0400

    Time: 7169 ms (59.44%), Space: 13.9 MB (50.82%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,25 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n = len(word)
+        row, col = len(board), len(board[0])
+        path = [(0,1), (0, -1), (1,0), (-1, 0)]
+        def dfs(pos, r,c,visited):
+            #print(pos, r,c,visited)
+            if pos == n :
+                return True
+            
+            for dr, dc in path:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < row and 0 <= nc < col and (nr, nc) not in visited and board[nr][nc] == word[pos]:
+                    if dfs(pos + 1, nr, nc, visited | set([(nr, nc)])):
+                        return True
+            
+            return False
+        
+        for r in range(row):
+            for c in range(col):
+                if board[r][c] == word[0]:
+                    if dfs(1,r,c,set([(r,c)])):
+                        return True
+        return False
+            
