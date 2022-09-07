6a6dff75c26f73b4b05faff688146b390684de9a
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-07 18:23:58 -0400

    Time: 46 ms (90.37%), Space: 13.9 MB (25.12%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -3,21 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-            
-        
         n = len(nums)
-        i = j = n-1
         
-        while i > 0 and nums[i] <= nums[i-1]:
+        i = j = n - 1
+        
+        while i and nums[i] <= nums[i-1]:
             i -= 1
-            
+        
         if i == 0:
             nums.reverse()
             return
         
-        while  nums[j] <= nums[i - 1]:
+        while nums[j] <= nums[i -1]:
             j -= 1
             
         nums[j], nums[i-1] = nums[i-1], nums[j]
         
-        nums[i:] = nums[i:][::-1][:]
+        nums[i:] = nums[i:][::-1]
