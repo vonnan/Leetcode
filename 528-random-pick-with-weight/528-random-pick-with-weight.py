a7c94e1f087c9b138a53baa8bb05ebe7347b489a
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-07 17:22:26 -0400

    Time: 429 ms (39.06%), Space: 18.6 MB (61.45%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -1,15 +1,16 @@
-from random import randint
 from bisect import bisect_left
+from random import randint
 
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.lst = list(accumulate(w))
-        
+        self.w = list(accumulate(w))
 
     def pickIndex(self) -> int:
-        num = randint(1, self.lst[-1])
-        return bisect_left(self.lst, num)
+        num = randint(1, self.w[-1])
+        idx = bisect_left(self.w, num)
+        return idx
+        
 
 
 # Your Solution object will be instantiated and called as such:
