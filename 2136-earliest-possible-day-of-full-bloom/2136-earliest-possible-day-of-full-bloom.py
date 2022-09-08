32d404508fe58a487afaa72b8b899d5fa22d968c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-08 02:10:51 -0400

    Time: 2796 ms (50.00%), Space: 36.5 MB (24.31%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
@@ -1,17 +1,13 @@
 class Solution:
-    def earliestFullBloom(self, PT: List[int], GT: List[int]) -> int:
-        pairs = [(g,p) for g, p in zip(GT, PT)]
-        pairs.sort()
+    def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
+        lst = sorted([(b, a) for a,b in zip(plantTime, growTime)], reverse = True)
+        print(lst)
+        plant, grow = [p for _, p in lst], [ g for g, _ in lst]
         
-        GT, PT = [g for g, _ in pairs], [p for _,p in pairs]
+        prefix = list(accumulate(plant))
         
-        prefix = [0]
-        for p in PT:
-            prefix.append(prefix[-1]+ p)
-        
-        tot = prefix[-1]
         res = prefix[-1]
-        for i, g in enumerate(GT):
-            res = max(res, tot - prefix[i] + g)
-            
+        for i, g in enumerate(grow):
+            res = max(res, prefix[i] + g)
+        
         return res
