9d2a74652f681a50381d11fcff4a9ad6b33b3c52
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-12 19:59:26 -0400

    Time: 3430 ms (25.01%), Space: 15.7 MB (78.98%) - LeetHub

--- 2184-number-of-ways-to-build-sturdy-brick-wall/2184-number-of-ways-to-build-sturdy-brick-wall.py
+++ 2184-number-of-ways-to-build-sturdy-brick-wall/2184-number-of-ways-to-build-sturdy-brick-wall.py
@@ -4,31 +4,32 @@ class Solution:
             return 0
         
         bricks.sort()
-        options = []
+        combos = []
     
         def dfs(path): 
             if path[-1] == width:
-                options.append(tuple(path[1:-1]))    
+                combos.append(tuple(path[1:-1]))    
             for b in bricks:
-                if path[-1] + b <= width:
-                    dfs(path + [path[-1] + b])
-                else:
+                if path[-1] + b > width:
                     break
+                else:
+                    dfs(path + [path[-1] + b])
+                
         path = [0]
         
         nei = defaultdict(set)
         dfs(path)
-        for i, combo_i in enumerate(options):
-            for j, combo_j in enumerate(options):
+        for i, combo_i in enumerate(combos):
+            for j, combo_j in enumerate(combos):
                 if not (set(list(combo_i)) & set(list(combo_j))):
                     nei[i].add(j)
         mod = 10**9 + 7 
         
-        dp = [1]* len(options)
+        dp = [1]* len(combos)
         
         for _ in range(height - 1):
-            dp2 = [0] * len(options)
-            for i in range(len(options)):
+            dp2 = [0] * len(combos)
+            for i in range(len(combos)):
                 if nei[i]:
                     dp2[i] = sum(dp[j] for j in nei[i])
         
