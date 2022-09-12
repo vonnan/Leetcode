6aa83575947f00519cab9731083ddd53d8dbc263
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-12 15:44:46 -0400

    Time: 907 ms (53.01%), Space: 28.3 MB (56.63%) - LeetHub

--- 2098-subsequence-of-size-k-with-the-largest-even-sum/2098-subsequence-of-size-k-with-the-largest-even-sum.py
+++ 2098-subsequence-of-size-k-with-the-largest-even-sum/2098-subsequence-of-size-k-with-the-largest-even-sum.py
@@ -1,15 +1,11 @@
 class Solution:
     def largestEvenSum(self, nums: List[int], k: int) -> int:
-        nums.sort()
+        nums.sort(reverse = 1)
         odd = [num for num in nums if num % 2]
         even =[ num for num in nums if num % 2 == 0]
-        if k == 1:
-            if not even:
-                return -1
-            return even[-1]
         
-        res = sum(nums[-k:])
-        
+        res = sum(nums[:k])
+        print(nums, res)
         if not odd or (res %2 ==0):
             return res
         
@@ -17,22 +13,22 @@ class Solution:
             if k == len(nums) or not even:
                 return -1
             
-            ct = sum(num % 2  for num in nums[-k:] )
+            ct = sum(num % 2  for num in nums[:k] )
             
             if k - ct == len(even):
                 if not even:
                     return -1
                 else:
-                    return res - even[0] + odd[-(ct + 1)]
+                    return res - even[-1] + odd[ct]
             
             else:
                 if ct == len(odd):
-                    return res - odd[-ct] + even[-(k - ct + 1)]
+                    return res - odd[-1] + even[k - ct]
                 else:
                     if k > ct:
-                        return max(res - odd[-ct] + even[-(k - ct + 1)], res - even[-(k - ct)] + odd[-(ct + 1)])
+                        return max(res - odd[ct - 1] + even[k - ct], res - even[k - ct -1] + odd[ct])
                     else:
-                        return res - odd[-ct] + even[-(k - ct + 1)]
+                        return res - odd[ct - 1] + even[k - ct]
             
         
     
