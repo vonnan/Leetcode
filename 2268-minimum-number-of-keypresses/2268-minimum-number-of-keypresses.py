e9df7454896181e549340ec43a354851ac6061d9
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-12 16:26:33 -0400

    Time: 61 ms (88.13%), Space: 14.9 MB (79.17%) - LeetHub

--- 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.py
+++ 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.py
@@ -2,17 +2,16 @@ class Solution:
     def minimumKeypresses(self, s: str) -> int:
         
         counter = Counter(s)
-        lst = sorted(list(counter.values()))
+        lst = sorted(list(counter.values()), reverse = 1)
         
         res = 0
-        if len(lst) <= 9:
-            res += sum(lst)
-        elif len(lst) <= 18:
-            res = sum(lst[-9:]) + sum(lst[:-9]) * 2
-        else:
-            res = sum(lst[-9:]) + sum(lst[-18:-9]) * 2 + sum(lst[:-18]) * 3
+        ct = 0
+        for i, val in enumerate(lst):
+            if i % 9 ==0:
+                ct += 1
+            res += val * ct
         return res
-        
+    
             
             
         
