eb40a5f28ab9f7ef88d8d180e806ac4cafffd38f
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-13 18:27:49 -0400

    Time: 1127 ms (83.77%), Space: 28.3 MB (72.17%) - LeetHub

---
+++ 2355-maximum-number-of-books-you-can-take/2355-maximum-number-of-books-you-can-take.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumBooks(self, books: List[int]) -> int:
+        stack = [(-1, 0)]
+        res = books[0]
+        for i, book in enumerate(books):
+            while stack and  books[stack[-1][0]] >= book - (i - stack[-1][0]):
+                stack.pop()
+            
+            prev_idx, prev_val = -1, 0
+            if stack:
+                prev_idx, prev_val = stack[-1]
+            h = min((i - prev_idx), book)
+            ct = prev_val + h * (book + book - h + 1)//2
+            res = max(res, ct)
+            stack.append((i, ct))
+        
+        return res
+            
+        
+        
+            
+                    
+                    
+            
