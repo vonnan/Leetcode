c82b2f0d67d4134c780d50c5d6dbe5489377b387
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-13 01:19:12 -0400

    Time: 4943 ms (63.42%), Space: 14.8 MB (8.54%) - LeetHub

--- 2168-unique-substrings-with-equal-digit-frequency/2168-unique-substrings-with-equal-digit-frequency.py
+++ 2168-unique-substrings-with-equal-digit-frequency/2168-unique-substrings-with-equal-digit-frequency.py
@@ -1,21 +1,13 @@
 class Solution:
     def equalDigitFrequency(self, s: str) -> int:
-        res = set()
+        res = set([])
+        n = len(s)
     
-        for i in range(len(s)):
-            dict1 = defaultdict(int)
-            j = i + 1
-            for c in s[i:]:
-                dict1[c] += 1
-            
-                if len(set(dict1.values())) == 1:
-                    res.add(s[i:j])
-                
-                j += 1
-            
-        return len(res)                
+        for i in range(n):
+            counter = Counter([])
+            for j, c in enumerate(s[i:], i):
+                counter[c] += 1
+                if len(set(counter.values())) == 1:
+                    res.add(s[i: j + 1])
         
-        
-        
-        
-            
+        return len(res)
