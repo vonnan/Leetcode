44f1b104ab14bee79029c81a9add99f5b91aa93b
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-12 23:04:24 -0400

    Time: 4340 ms (16.61%), Space: 13.9 MB (82.10%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -0,0 +1,22 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        dp = [0] * k
+        self.res = sum(cookies)
+        n = len(cookies)
+        if n <= k:
+            return max(cookies)
+            
+        def dfs(path, pos):
+            if pos == n:
+                self.res = min(self.res, max(path))
+                return
+                
+            for i in range(k):
+                nxt = path[:i] + [path[i] + cookies[pos]] + path[i+1:]
+                #print(i, nxt)
+                dfs(nxt, pos + 1)
+        
+        dfs(dp, 0)
+        
+        return self.res
+        
