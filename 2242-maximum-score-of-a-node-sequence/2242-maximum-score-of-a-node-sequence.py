5aca09f95ed1b536760a53a6c68da28cedc28a92
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-14 01:16:58 -0400

    Time: 3209 ms (70.96%), Space: 42.3 MB (50.55%) - LeetHub

--- 2242-maximum-score-of-a-node-sequence/2242-maximum-score-of-a-node-sequence.py
+++ 2242-maximum-score-of-a-node-sequence/2242-maximum-score-of-a-node-sequence.py
@@ -1,27 +1,31 @@
 class Solution:
     def maximumScore(self, scores: List[int], edges: List[List[int]]) -> int:
-        n = len(scores)
-        t3 = [[] for _ in range(n)]
-        gr = [[] for _ in range(n)]
-
+        res = 0
+        nei = defaultdict(list)
+        
+        for u, v in edges:
+            nei[u].append((scores[v], v))
+            nei[v].append((scores[u], u))
+            
+        for u in nei:
+            nei[u] =sorted(nei[u])[-3:]
+            
         for a, b in edges:
-            gr[a].append(b)
-            gr[b].append(a)
-
-        for v in range(n):
-            for to in gr[v]:
-                t3[v].append((scores[to], to))
-            t3[v] = sorted(t3[v], reverse=True)[:3]
-
-        mx = -1
-        for a, b in edges:
-            for sa, ta in t3[a]:
-                if ta == b: continue
-                for sb, tb, in t3[b]:
-                    if tb == a or tb == ta: continue
-                    mx = max(mx, sa + sb + scores[a] + scores[b])
-
-        return mx                
+            ct = scores[a] + scores[b]
+            for su, u in nei[a]:
+                if u == b:
+                    continue
+                else:
+                    for sv, v in nei[b]:
+                        if v== a or v == u:
+                            continue
+                        else:
+                            res = max(res, ct + su + sv)
+        
+        return res if res else -1
+                
+            
+                
             
         
         
