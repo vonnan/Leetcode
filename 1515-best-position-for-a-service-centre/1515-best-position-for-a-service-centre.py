a9a07fbf8035f37529994921dad99c062441b2f8
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-14 17:04:50 -0400

    Time: 339 ms (31.62%), Space: 14 MB (44.66%) - LeetHub

--- 1515-best-position-for-a-service-centre/1515-best-position-for-a-service-centre.py
+++ 1515-best-position-for-a-service-centre/1515-best-position-for-a-service-centre.py
@@ -1,45 +1,39 @@
 from math import sqrt
 class Solution:
-    def getMinDistSum(self, positions: List[List[int]]) -> float:
-        if len(positions)<=1: return 0 
+    def getMinDistSum(self, A: List[List[int]]) -> float:
+        X, Y = [ x for x, _ in A], [y for _, y in A]
+        n = len(A)
         
-        def calculate(x, y): 
-            ans = 0
-            for a, b in positions: 
-                ans += pow(pow(x-a, 2) + pow(y-b, 2), 0.5)
-            return ans
-        xmin, xmax = positions[0][0], positions[0][0]
-        ymin, ymax = positions[0][1], positions[0][1]
-        xcen, ycen = 0, 0
-        N = len(positions)
-        for x, y in positions: 
-            xmin = min(x, xmin)
-            xmax = max(x, xmax)
-            ymin = min(y, ymin)
-            ymax = max(y, ymax)
-            xcen += x
-            ycen += y
-        xstep = xmax - xmin 
-        ystep = ymax - ymin
-        x, y = xcen/N, ycen/N
-        curD = calculate(x, y)
-        while pow(xstep, 2) + pow(ystep, 2) > pow(10, -15): 
-            # print(x, y, xstep, ystep)
-            curD = calculate(x, y)
-            if curD>calculate(x+xstep, y+ystep): 
-                x += xstep
-                y += ystep
-            elif curD>calculate(x-xstep, y+ystep): 
-                x -= xstep
-                y += ystep
-            elif curD>calculate(x-xstep, y-ystep): 
-                x -= xstep
-                y -= ystep
-            elif curD>calculate(x+xstep, y-ystep): 
-                x += xstep
-                y -= ystep
-            else: 
-                xstep/=2
-                ystep/=2
-        return curD
-                
+        def dist(a, b):
+            return sum(sqrt(pow((x- a), 2) + pow(y - b, 2)) for x, y in A)
+        
+        x_min, x_max = min(X), max(X)
+        y_min, y_max = min(Y), max(Y)
+        x, y = sum(X)/n, sum(Y)/n
+        x_step, y_step = x_max- x_min, y_max - y_min
+        
+        curr = dist(x, y)
+        while x_step** 2 + y_step**2 > 10**(-12):
+            curr = dist(x, y)
+            if curr > dist(x + x_step, y + y_step):
+                x += x_step
+                y += y_step
+            elif curr > dist(x + x_step, y - y_step):
+                x += x_step
+                y -= y_step
+            elif curr > dist(x - x_step, y + y_step):
+                x -= x_step
+                y += y_step
+            elif curr > dist(x - x_step, y - y_step):
+                x -= x_step
+                y -= y_step
+            else:
+                x_step /= 2
+                y_step /= 2
+        
+        return curr
+        
+        
+            
+        
+                    
