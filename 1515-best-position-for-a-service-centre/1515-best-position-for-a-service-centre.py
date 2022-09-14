5b5bf3031b67bb07a1669f6a9aa1c47dc39ec8e1
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-14 15:42:20 -0400

    Time: 369 ms (29.02%), Space: 13.9 MB (45.10%) - LeetHub

--- 1515-best-position-for-a-service-centre/1515-best-position-for-a-service-centre.py
+++ 1515-best-position-for-a-service-centre/1515-best-position-for-a-service-centre.py
@@ -1,27 +1,45 @@
 from math import sqrt
 class Solution:
-    def getMinDistSum(self, A: List[List[int]]) -> float:
-        X, Y = [ x for x, _ in A], [y for _, y in A]
+    def getMinDistSum(self, positions: List[List[int]]) -> float:
+        if len(positions)<=1: return 0 
         
-        x_min, x_max = min(X), max(X)
-        y_min, y_max = min(Y), max(Y)
-        
-        def dist(a, b):
-            return sum(sqrt(pow((x- a), 2) + pow(y - b, 2)) for x, y in A)
-        
-        step = 9
-        grid = [[0]* step for _ in range(step)]
-        min_ = [inf, 0, 0]
-        
-        for _ in range(100):
-            for i, row in enumerate(grid):
-                for j, _ in enumerate(row):
-                    x = x_min + i/step*(x_max - x_min)
-                    y = y_min + j/step*(y_max - y_min)
-                    cost = [dist(x,y), x, y]
-                    min_ = min(min_, cost)
-            x_min, x_max = min_[1] - (x_max - x_min)/3, min_[1] + (x_max - x_min)/3
-            y_min, y_max = min_[2] - (y_max - y_min)/3, min_[2] + (y_max - y_min)/3
-        
-        return min_[0]
-                    
+        def calculate(x, y): 
+            ans = 0
+            for a, b in positions: 
+                ans += pow(pow(x-a, 2) + pow(y-b, 2), 0.5)
+            return ans
+        xmin, xmax = positions[0][0], positions[0][0]
+        ymin, ymax = positions[0][1], positions[0][1]
+        xcen, ycen = 0, 0
+        N = len(positions)
+        for x, y in positions: 
+            xmin = min(x, xmin)
+            xmax = max(x, xmax)
+            ymin = min(y, ymin)
+            ymax = max(y, ymax)
+            xcen += x
+            ycen += y
+        xstep = xmax - xmin 
+        ystep = ymax - ymin
+        x, y = xcen/N, ycen/N
+        curD = calculate(x, y)
+        while pow(xstep, 2) + pow(ystep, 2) > pow(10, -15): 
+            # print(x, y, xstep, ystep)
+            curD = calculate(x, y)
+            if curD>calculate(x+xstep, y+ystep): 
+                x += xstep
+                y += ystep
+            elif curD>calculate(x-xstep, y+ystep): 
+                x -= xstep
+                y += ystep
+            elif curD>calculate(x-xstep, y-ystep): 
+                x -= xstep
+                y -= ystep
+            elif curD>calculate(x+xstep, y-ystep): 
+                x += xstep
+                y -= ystep
+            else: 
+                xstep/=2
+                ystep/=2
+        return curD
+                
