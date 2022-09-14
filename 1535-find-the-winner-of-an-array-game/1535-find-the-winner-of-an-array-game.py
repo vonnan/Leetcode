c0da3aa42737988696facd86733182a6d24963af
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-14 19:15:56 -0400

    Time: 2754 ms (17.35%), Space: 25.9 MB (97.11%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -0,0 +1,29 @@
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        ct = 0
+        if k >= len(arr) -1:
+            return max(arr)
+        
+        ct = 0
+        back = 0
+        while arr:
+            if len(arr) == 1:
+                return arr[0]
+            
+            if arr[0] > arr[1]:
+                arr.pop(1)
+                ct += 1
+                
+            else:
+                arr.pop(0)
+                ct = 1
+            
+            back += 1
+            if ct == k:
+                return arr[0]
+        
+        
+            
+            
+            
+            
