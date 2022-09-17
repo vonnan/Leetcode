04f85c0da95d1ad86084050509100c63057ab9b9
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-16 23:20:44 -0400

    Time: 3926 ms (70.49%), Space: 27.8 MB (38.61%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.py
+++ 336-palindrome-pairs/336-palindrome-pairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
-        dic = {word[::-1]: i for i, word in enumerate(words)}
+        dic = {word: i for i, word in enumerate(words)}
         
         res = set([])
         
@@ -9,11 +9,11 @@ class Solution:
                 prefix = word[:j]
                 suffix = word[j:]
                 
-                if prefix in dic and (dic[prefix] != i) and suffix == suffix[::-1]:
-                    res.add((i, dic[prefix]))
+                if prefix[::-1] in dic and (dic[prefix[::-1]] != i) and suffix == suffix[::-1]:
+                    res.add((i, dic[prefix[::-1]]))
                     
-                if suffix in dic and (dic[suffix] != i) and prefix == prefix[::-1]:
-                    res.add((dic[suffix], i))
+                if suffix[::-1] in dic and (dic[suffix[::-1]] != i) and prefix == prefix[::-1]:
+                    res.add((dic[suffix[::-1]], i))
         
         return res
             
