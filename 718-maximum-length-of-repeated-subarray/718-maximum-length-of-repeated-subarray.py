11c51f6c7c9694817301309010d0de7766a9dad4
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-09-20 00:21:47 -0400

    Time: 3054 ms (86.84%), Space: 39.3 MB (50.31%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,44 +1,17 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1, nums2 = [ x+ 1 for x in nums1], [x + 1 for x in nums2]
-        sets = set(nums1) & set(nums2)
-        if not sets:
-            return 0
+        n1, n2 = len(nums1), len(nums2)
+        dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+        max_ = 0
         
-        h = 128
-        set1 = set2 = sets
-        max_ = 1
-        for i, c in enumerate(nums1):
-            if c not in sets:
-                continue
-            pre = c
-            
-            for j, d in enumerate(nums1[i+1:], i + 1):
-                if d not in sets:
-                    break
-                pre = h * pre + d
-                if pre not in set1:
-                    set1.add(pre)
-            
-        for i, c in enumerate(nums2):
-            if c not in sets:
-                continue
-            pre = c
-            for j, d in enumerate(nums2[i+1:], i + 1):
-                if d not in sets:
-                    break
-                pre = h * pre + d
-                if pre in set1:
-                    if pre > max_:
-                        max_ = pre
-                        
-                else:
-                    break
+        for i in range(n1):
+            for j in range(n2):
+                if nums1[i] == nums2[j]:
+                    dp[i+1][j+1] =  dp[i][j] + 1
+                    if dp[i+1][j+1] > max_:
+                        max_ = dp[i+1][j+1]
         
-        res = 0
-        while max_:
-            max_ = max_//h
-            res += 1
-        return res
-                
-            
+        return max_
+        
+        
+        
