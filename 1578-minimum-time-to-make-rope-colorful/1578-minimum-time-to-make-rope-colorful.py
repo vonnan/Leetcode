2cad98e453dd15f8362ba87a06f396f5334863a5
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-10-02 22:21:16 -0400

    Time: 4650 ms (5.05%), Space: 24.9 MB (74.83%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,22 @@
+from bisect import insort
+
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        
+        prev = colors[0]
+        cost =  neededTime[0]
+        lst = [cost]
+        res = 0
+        print(lst, res, cost)
+        for c,t in zip(colors[1:], neededTime[1:]):
+            if c != prev:
+                res += cost - lst[-1]
+                cost = t
+                lst = [t]
+            else:
+                cost += t
+                insort(lst, t)
+            prev = c
+        return res + cost - lst[-1]
+                    
+                
