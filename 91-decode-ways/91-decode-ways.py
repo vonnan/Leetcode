652215a8879b392948abb9f82ee5ddac312728b8
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-10-02 21:33:38 -0400

    Time: 62 ms (37.43%), Space: 13.8 MB (79.99%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,26 +1,26 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if s[0] =="0":
+        if s[0] == "0":
             return 0
         
         n = len(s)
-        
         dp = [0] * (n + 1)
         dp[0] = dp[1] = 1
         
         for i, c in enumerate(s[1:], 1):
-            
             if c == "0":
                 if s[i-1] == "0" or s[i-1] >= "3":
                     return 0
+                
                 else:
                     dp[i + 1] += dp[i-1]
+            
             else:
-                dp[i+1] += dp[i]
-                if "10" <= s[i-1:i+1] <= "26":
+                dp[i + 1] += dp[i]
+                if 10 <= int(s[i-1: i+1]) <= 26:
                     dp[i+1] += dp[i-1]
-        
         return dp[-1]
+        
                     
                     
             
