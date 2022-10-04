d35f5509f4a0abc35d2efb46018b6a0afa9a928e
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-10-04 02:26:26 -0400

    Time: 2801 ms (58.64%), Space: 32.3 MB (93.98%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -1,21 +1,15 @@
-from sortedcontainers import SortedList
 from bisect import bisect
 from bisect import insort
 
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
         nums = [a - b for a, b in zip(nums1, nums2)]
-        
-        counter = Counter()
-        
+    
         lst = []
         res = 0
         for num in nums:
             idx = bisect(lst, num + diff)
             res += idx
             insort(lst, num)
-            
-            
-            
         return res
             
