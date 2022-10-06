260f6f5f6f831c52eca34f5e95130dbe08bd2717
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-10-06 15:35:57 -0400

    Time: 50 ms, Memory: 14 MB - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,13 +1,14 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        dic = {"2":"abc", "3": "def", "4": "ghi", "5": "jkl", "6" : "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
-        
+        dic = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9":"wxyz"}
         if not digits:
             return []
         
-        sets = set([""])
+        res = set([""])
         
-        for num in digits:
-            sets = set([path + c for path in sets for c in dic[num]])
-            
-        return sets
+        for c in digits:
+            new = set([])
+            for nxt in dic[c]:
+                new |= set([word + nxt for word in res] )
+            res = new
+        return res
