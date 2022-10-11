129879c8dd843cf587817b7444d4e6fffdd8df92
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-10-11 01:45:40 -0400

    Time: 898 ms (64.86%), Space: 24.8 MB (18.64%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        LIS = [ nums[0]]
+        
+        for num in nums[1:]:
+            idx = bisect_left(LIS, num)
+            if idx == len(LIS):
+                LIS.append(num)
+                if len(LIS) >= 3:
+                    return True
+            else:
+                LIS[idx] = num
+            
+        
+        return len(LIS) >= 3
