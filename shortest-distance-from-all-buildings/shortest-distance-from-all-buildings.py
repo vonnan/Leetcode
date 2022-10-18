467acf7669c4698be9aa3eabf47725199692808c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-10-18 19:37:22 -0400

    Time: 6891 ms, Memory: 28 MB - LeetHub

--- shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.py
+++ shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.py
@@ -1,36 +1,30 @@
 class Solution:
     def shortestDistance(self, grid: List[List[int]]) -> int:
         row, col = len(grid), len(grid[0])
-        path = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        houses, res, reach  = set([]), inf, defaultdict(list)
-        
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == 1:
-                    houses.add((r,c, 0))
-                elif grid[r][c]== 0:
-                    reach[(r,c)] = []
-                    
+        houses = set([(r,c) for r in range(row) for c in range(col) if grid[r][c] == 1])
+        dic = defaultdict(list)
+        path = [(0,1), (0, -1), (1, 0), (-1, 0)]
         for house in houses:
-            
-            q = deque([house])
-            seen = set()
-            
+            r, c = house
+            q = deque([(r,c, 0)])
+            seen = set([(r, c)])
             while q:
-                r, c, dist = q.popleft()
-                
+                r,c,d = q.popleft()
                 for dr, dc in path:
                     nr, nc = r + dr, c + dc
-                    
-                    if ((nr, nc) not in seen) and (0 <= nr < row) and (0 <= nc < col) and grid[nr][nc] == 0:
+                    if 0 <= nr < row and 0 <= nc < col and ((nr, nc) not in seen) and grid[nr][nc] == 0:
+                        dic[(nr, nc)].append(d + 1)
                         seen.add((nr, nc))
-                        q.append((nr, nc, dist + 1))
-                        reach[(nr,nc)].append(dist + 1)
-                    
-       
+                        q.append((nr, nc, d + 1))
         n = len(houses)
-        for key, val in reach.items():
-            if len(val) == n:
-                res = min(res, sum(val))
-        return res if res != inf else -1
-                    
+        
+        if not dic:
+            return -1
+        
+        lst = [sum(val) for val in dic.values() if len(val) == n]
+        if not lst:
+            return -1
+        return min(lst)
+                
+        
+        
