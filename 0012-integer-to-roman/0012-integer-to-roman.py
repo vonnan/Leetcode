d48347c82d364a9b9d060f419b86fb86a4eb1487
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-10-19 20:58:04 -0400

    Time: 71 ms (75.91%), Space: 13.8 MB (80.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,28 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        dic = { 1: "I",
+                4: "IV",
+                5: "V",
+                9: "IX",
+                10: "X",
+                40: "XL",
+                50: "L",
+                90: "XC",
+                100: "C",
+                400: "CD",
+                500: "D",
+                900: "CM",
+                1000: "M"}
+        
+        res = ""
+        for key in sorted(dic.keys(), reverse = 1):
+            k = num//key
+            res +=  dic[key] * k
+            num -= key * k
+            
+        return res
+        
+            
+        
+            
+        
