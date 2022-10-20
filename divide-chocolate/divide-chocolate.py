c032156a54f512bb20671070fa982d883ece546d
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-10-20 03:26:09 -0400

    Time: 247 ms, Memory: 15.3 MB - LeetHub

--- divide-chocolate/divide-chocolate.py
+++ divide-chocolate/divide-chocolate.py
@@ -1,20 +1,28 @@
 class Solution:
-    def maximizeSweetness(self, A: List[int], k: int) -> int:
-        left, right = min(A), sum(A)//(k + 1)
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
+        
+        
+        left, right = min(sweetness), sum(sweetness)//(k + 1)
+        if len(sweetness) == k + 1:
+            return left
+        
+        res  = left
         while left < right:
-            mid = (left + right)//2
-            ct, tot = 1, 0
-            for a in A:
-                if tot + a <= mid:
-                    tot += a
+            mid = (left + right) // 2
+            ct = 1
+            tot = 0
+            for num in sweetness:
+                if num + tot <= mid:
+                    tot += num
                 else:
                     ct += 1
                     tot = 0
                     if ct > k + 1:
-                        left = mid + 1
                         break
-            if ct <= k + 1:
+            if ct > k + 1:
+                left = mid + 1
+            else:
                 right = mid
         return left
-        
-        
+                    
+            
