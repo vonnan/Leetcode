5a077c903f279a4d2b229bbce19d572bfc08c2ff
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-11-05 01:20:13 -0400

    Time: 583 ms (84.47%), Space: 39 MB (94.02%) - LeetHub

--- 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.py
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.py
@@ -1,17 +1,13 @@
 class Solution:
     def smallestCommonElement(self, mat: List[List[int]]) -> int:
+        
         row, col = len(mat), len(mat[0])
-        counter = Counter([])
-        sets = set()
+        counter = Counter()
         
         for r, rows in enumerate(mat):
             for num in rows:
                 counter[num] += 1
-                if r== row -1 and counter[num] == row:
+                if r == row -1 and counter[num] == row:
                     return num
-                
+        
         return -1
-        
-        
-        
-            
