cc98199d4d714639b79ef415d055a9ddab8d723c
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-11-22 01:48:57 -0500

    Time: 3405 ms (48.12%), Space: 28.8 MB (48.38%) - LeetHub

--- 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.py
+++ 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.py
@@ -1,35 +1,42 @@
 class Solution:
-    def beautifulPartitions(self, s: str, k: int, minLength: int) -> int:
-        n = len(s)
-        primes = ['2', '3', '5', '7']
+    def beautifulPartitions(self, s: str, k: int, min_: int) -> int:
+        res = ""
+        for c in s:
+            if c in "2357":
+                res += "1"
+            else:
+                res += "0"
+        n = len(s)  
         
-        # pruning
-        if k * minLength > n or s[0] not in primes or s[-1] in primes:
+        if k * min_ > n or res[0] != "1" or res[-1] != "0":
             return 0
         
-        # posible starting indexes of a new partition
-        ids = [0]
-        for i in range(n-1):
-            if s[i] not in primes and s[i+1] in primes:
-                ids.append(i+1)
-        m = len(ids)
-
+        lst = [0]
+        mod = 10 ** 9 + 7
+        for i, c in enumerate(res[1:], 1):
+            if c == "1" and res[i-1] == "0":
+                lst.append(i)
+        
+        m = len(lst)
+        
         @cache
-        # dp(i, kk) means number of ways to partition s[ids[i]:n] into kk partitions
+        
         def dp(i, kk):
-            
-            # kk==1: last remaining partition, needs to have length >= l
             if kk == 1:
-                return 1 if ids[i]+minLength-1 <= n-1 else 0
+                return 1 if lst[i] + min_ -1 <= n -1 else 0
+            
             res = 0
             
-            # iterate possible starting index of next partition
-            for j in range(i+1, m-kk+2):
-                if ids[j]-ids[i] >= minLength:
-                    res += dp(j, kk-1)
+            for j in range(i + 1, m - kk + 2):
+                if lst[j] - lst[i] >= min_:
+                    res += dp(j, kk -1)
             
-            return res % (10**9+7)
+            return res % mod
         
         return dp(0, k)
-                
+                    
+            
+        
+        
+        
         
