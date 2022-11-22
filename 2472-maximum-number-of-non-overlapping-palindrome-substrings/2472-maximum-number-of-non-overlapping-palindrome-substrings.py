fcbb4126861dbbc87d7003c699d907cda154588b
Nan Feng <56846412+vonnan@users.noreply.github.com>
2022-11-22 16:31:05 -0500

    Time: 117 ms (59.03%), Space: 13.9 MB (95.27%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -8,12 +8,14 @@ class Solution:
         
         n = len(s)
         dp = [0] * (n + 1)
+        
         for j in range(k, n + 1):
             dp[j] = dp[j-1]
             if Pal(j - k, j):
                 dp[j] = max(dp[j], 1 + dp[j - k])
             elif Pal(j - k - 1, j):
                 dp[j] = max(dp[j], 1 + dp[j - k - 1])
+        
         return dp[-1]
         
         
