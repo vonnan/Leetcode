171dea5539c1397208b557097ce1be451363602a
Nan Feng <56846412+vonnan@users.noreply.github.com>
2023-07-13 16:24:43 -0400

    Time: 51 ms (76.56%), Space: 16.9 MB (28.99%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return
+        
+        seen = {}
+        seen[node] = Node(node.val, [])
+        
+        q = deque([node])
+        while q:
+            nd = q.popleft()
+            
+            for nei in nd.neighbors:
+                if nei not in seen:
+                    seen[nei] = Node(nei.val, [])
+                    q.append(nei)
+                seen[nd].neighbors.append(seen[nei])
+        return seen[node]
+                
+                    
